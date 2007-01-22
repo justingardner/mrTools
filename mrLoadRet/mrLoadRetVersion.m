652188594ee56ce57d90816c812a83828c6dae12
david <david@d7859a6d-3d6a-0410-b503-b92f2c4db295>
2007-01-22 16:28:43 +0000

    Modified Files:  	checkAlignment.m computeAlignment.m  	intensityContrastCorrection.m interpVolume.m mrAlign.m  	mrAlignGUI.m refreshAlignDisplay.m ---------------------------------------------------------------------- use shiftOriginXform instead of ALIGN.origin to specify nifti origin fix coarse alignment bug Modified Files: 	mrGlobals.m mrLoadRetVersion.m ---------------------------------------------------------------------- mrLoadRetVersion returns also expected matlab version, used for warning when starting mrLoadRet or mrAlign

--- mrLoadRet/mrLoadRetVersion.m
+++ mrLoadRet/mrLoadRetVersion.m
@@ -1,3 +1,4 @@
-function ver = mrLoadRetVersion
+function [ver, expectedMatlabVersion] = mrLoadRetVersion
 
 ver = 4.5;
+expectedMatlabVersion = [7.1 7.2];  % Change this after testing Matlab upgrades
