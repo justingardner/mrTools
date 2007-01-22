0fe55d44e8cb769d2addd425a851cfee17b6caad
david <david@d7859a6d-3d6a-0410-b503-b92f2c4db295>
2007-01-22 16:32:28 +0000

    Modified Files:  	checkAlignment.m computeAlignment.m  	intensityContrastCorrection.m interpVolume.m mrAlign.m  	mrAlignGUI.m refreshAlignDisplay.m use shiftOriginXform instead of ALIGN.origin to specify nifti origin fix coarse alignment bug ---------------------------------------------------------------------- Modified Files: 	mrGlobals.m mrLoadRetVersion.m mrLoadRetVersion returns also expected matlab version, used for warning when starting mrLoadRet or mrAlign ---------------------------------------------------------------------- Modified Files:  	averageTSeries.m  	concatTSeries.m use shiftOriginXform to specify nifti origin ---------------------------------------------------------------------- Modified Files:  	mlrGuiSet.m mrLoadRetGUI.m refreshMLRDisplay.m use shiftOriginXform to specify nifti origin fix scan and slice sliders ---------------------------------------------------------------------- Modified Files:  	xformROIcoords.m use shiftOriginXform to specify nifti origin ---------------------------------------------------------------------- Modified Files: 	viewGet.m fix scan and slice sliders (round value in viewGet) ---------------------------------------------------------------------- Added Files: 	shiftOriginXform.m use shiftOriginXform to specify nifti origin ---------------------------------------------------------------------- Modified Files:  	computeDerivatives3.m convXYZsep.m convXYsep.m added shape ('full', 'same', or 'valid') parameter to all of these functions as in conv2, and set appropriate defaults.

--- mrUtilities/ImageProcessing/computeDerivatives3.m
+++ mrUtilities/ImageProcessing/computeDerivatives3.m
@@ -9,17 +9,17 @@ function [fx,fy,fz,ft] = computeDerivatives3(in1,in2)
 filter = [0.03504 0.24878 0.43234 0.24878 0.03504];
 dfilter = [0.10689 0.28461 0.0  -0.28461  -0.10689];
 
-dz1 = convXYsep(convZ(in1,dfilter),filter,filter);
+dz1 = convXYsep(convZ(in1,dfilter),filter,filter,'valid');
 tmp1 = convZ(in1,filter);
-dx1 = convXYsep(tmp1,dfilter,filter);
-dy1 = convXYsep(tmp1,filter,dfilter);
-blur1 = convXYsep(tmp1,filter,filter);
+dx1 = convXYsep(tmp1,dfilter,filter,'valid');
+dy1 = convXYsep(tmp1,filter,dfilter,'valid');
+blur1 = convXYsep(tmp1,filter,filter,'valid');
 
-dz2 = convXYsep(convZ(in2,dfilter),filter,filter);
+dz2 = convXYsep(convZ(in2,dfilter),filter,filter,'valid');
 tmp2 = convZ(in2,filter);
-dx2 = convXYsep(tmp2,dfilter,filter);
-dy2 = convXYsep(tmp2,filter,dfilter);
-blur2 = convXYsep(tmp2,filter,filter);
+dx2 = convXYsep(tmp2,dfilter,filter,'valid');
+dy2 = convXYsep(tmp2,filter,dfilter,'valid');
+blur2 = convXYsep(tmp2,filter,filter,'valid');
 
 fx=(dx1+dx2)/2;
 fy=(dy1+dy2)/2;
