3cfe5d6f1161183a93d3169728513f93545a8f51
eli <eli@d7859a6d-3d6a-0410-b503-b92f2c4db295>
2007-02-14 17:39:36 +0000

    *** empty log message ***

--- trunk/mrUtilities/File/Nifti/cbiSwapNiftiDimensions.m
+++ trunk/mrUtilities/File/Nifti/cbiSwapNiftiDimensions.m
@@ -6,7 +6,7 @@ function varargout=cbiSwapNiftiDimensions(varargin);
 % Swaps the dimensions of a Nifti file or data set.
 % Modifies the qform44 and quaternions accordingly.
 % If sform44 is non-identity, also modifies the sform.
-% Equivalent to avwswapdim except it works - including modifying qform44/sform44 matrices
+% Equivalent to avwswapdim except it (hopefully correctly) modifies qform44/sform44 matrices
 % NOTE: Only modifies the first 3 dimensions of a file - but works with data with 4 or more dimensions.
 %
 % swapvect: a 3-vector with entries +/-(1,2,3) indicating axis changes/flips, e.g.:
@@ -14,12 +14,17 @@ function varargout=cbiSwapNiftiDimensions(varargin);
 % - exchange x and z:          swapvect=[3 2 1]
 % - exchange x with flipped y: swapvect=[-2 1 3]
 %
+% NOTE: if the image is acquired in one of the cardinal directions (axial, sagittal, or coronal 
+% slice prescription) swapvect can be omitted, in which case the default is to swap the image axes 
+% to conform to a 'standard' right-handed coordinate system as follows:
+% - dimension i (fastest changing dimension) corresponds to x (increasing from left to right)
+% - dimension j (second fastest changing dimension) corresponds to y (increasing from back to front)
+% - dimension k (third fastest changing dimension) corresponds to z (increasing from bottom to top)
+% 
 % Optionally returns swap matrix
+%
   
 % File or data input?
-if (nargin<3) 
-  error('not enough inputs');
-end
 
 isfile=0;
 isarray=0;
@@ -41,7 +46,25 @@ else
   datasize=size(varargin{1});
 end
 
-swapvect=varargin{3};
+if (nargin>=3)
+  swapvect=varargin{3};
+else
+  % check that image is in cardinal planes
+  image_axes=hdr.qform44(1:3,1:3);
+  ndims_per_axis=sum(abs(image_axes)>0);
+  if (any(ndims_per_axis>1))
+    error('Image planes not in cardinal planes - image must be rotated to canonical orientation. Aborting...')
+  else
+    swapvect=[0 0 0];
+    swapmat=inv(image_axes);
+    for n=1:3
+      % row index of nonzero entry in each column == axis index with sign
+      ri=find(swapmat(:,n));
+      swapvect(n)=ri*sign(swapmat(ri,n));
+    end
+  end
+  disp(['Extracted permutation vector (swapvect): ' num2str(swapvect)])
+end
 
 if (length(swapvect)~=3)
   error('Permutation (swap) vector must have 3 dimensions!');
@@ -60,6 +83,7 @@ end
 % R = Q*inv(P)
 % and similarly, the new sform Z is given by
 % Z = W*inv(P) where W is the old sform
+% NOTE: swapMatrix == inv(P)
 
 swapMatrix=eye(4);
 id44=eye(4);
@@ -70,22 +94,25 @@ for n=1:3
   srccol=abs(srccol);
   swapMatrix(:,n)=sgn*id44(:,srccol);
   if (sgn<0)
-    % axis inversions correspond to inverting sign and adding axis dimension-1  
+    % axis inversions correspond to inverting sign and adding axis dimension-1 
+    % (because this matrix operates on array coordinates which are zero-offset integers)
+    % (i.e. voxel i==2 when flipped becomes voxel i==datasize-1-2)
     swapMatrix(srccol,4)=datasize(srccol)-1;
   end
 end
+disp(swapMatrix)
 
 % Calculate new qform & sform. Qform is needed for pixdim etc.
 if (~isfield(hdr,'qform44') | isempty(hdr.qform44))
   hdr.qform44=eye(4);
 end
 
-newqform44=hdr.qform44*inv(swapMatrix);
+newqform44=hdr.qform44*swapMatrix;
 % Set new qform and update quaternions, qoffset, and pixdim
 fliphdr=cbiSetNiftiQform( hdr, newqform44 );
 
-if (isfield(hdr,'sform44') & ~isempty(hdr.sform44))
-  newsform44=hdr.sform44*inv(swapMatrix);
+if (isfield(hdr,'sform44') & ~isequal(eye(4),hdr.sform44))
+  newsform44=hdr.sform44*swapMatrix;
   fliphdr=cbiSetNiftiSform( fliphdr, newsform44 );
 end
 
@@ -103,6 +130,8 @@ for n=1:3
   end
 end
 
+% Update flipped header
+fliphdr=cbiCreateNiftiHeader(fliphdr,flipdata);
 
 if (isarray)
   varargout{1}=flipdata;
