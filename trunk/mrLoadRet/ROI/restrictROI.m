f92f1f13a119e82f717d30c1e3c777ccc8abe28f
farshadm <farshadm@d7859a6d-3d6a-0410-b503-b92f2c4db295>
2007-02-26 19:53:57 +0000

    *** empty log message ***

--- trunk/mrLoadRet/ROI/restrictROI.m
+++ trunk/mrLoadRet/ROI/restrictROI.m
@@ -48,7 +48,8 @@ for overlayNum = 1:viewGet(view,'numberOfOverlays')
 		else
 			indices = find(data >= overlayClip(1) | data <= overlayClip(2));
 		end
-		ROIcoords = ROIcoords(:,indices);
+		% ROIcoords = ROIcoords(:,indices);
+		ROIcoords = xformROIcoords(coords(:,indices),inv(ROIxform)*overlayXform,ROIvoxelSize,overlayVoxelSize,[1,1,1]);
 	else
 		ROIcoords = [];
 	end
