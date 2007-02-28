26dd123a5fea251d01d6fa0c6af0ab768cb28e7c
farshadm <farshadm@d7859a6d-3d6a-0410-b503-b92f2c4db295>
2007-02-28 16:46:45 +0000

    restrictROI.m bug fixed (fm).

--- mrLoadRet/ROI/restrictROI.m
+++ mrLoadRet/ROI/restrictROI.m
@@ -13,7 +13,7 @@ function view = restrictROI(view,ROInum,scan)
 if ieNotDefined('ROInum')
 	ROInum = viewGet(view,'currentroinum');
 end
-if isstr(ROInum)
+if ischar(ROInum)
 	ROInum = viewGet(view,'roinum',ROInum);
 end
 
@@ -24,31 +24,32 @@ end
 ROI = viewGet(view,'roi',ROInum);
 ROIcoords = viewGet(view,'roiCoords',ROInum);
 ROIxform = viewGet(view,'roiXform',ROInum);
-ROIvoxelSize = viewGet(view,'roiVoxelSize',ROInum);
 
 for overlayNum = 1:viewGet(view,'numberOfOverlays')
 	overlayData = viewGet(view,'overlayData',scan,overlayNum);
 	overlayClip = viewGet(view,'overlayClip',overlayNum);
 	overlayXform = viewGet(view,'overlayXform',scan);
-    overlayVoxelSize = viewGet(view,'scanVoxelSize',scan);
 	if ~isempty(overlayData)
         % Transform ROI coords to overlay
-        coords = xformROIcoords(ROIcoords,inv(overlayXform)*ROIxform,overlayVoxelSize,ROIvoxelSize);
+        shiftXform = shiftOriginXform;
+        xform = inv(shiftXform) * inv(overlayXform) * ROIxform * shiftXform;
+        coords = round(xform * ROIcoords); 
 		overlaySize = size(overlayData);
 		% Find coords that are within the overlay size
 		for v = 1:3
 			indices = find((coords(v,:) > 0) & (coords(v,:) <= overlaySize(v)));
 			coords = coords(:,indices);
+            ROIcoords = ROIcoords(:,indices);
 		end
 		% Find overlay voxels that are within clip
 		indices = sub2ind(overlaySize,coords(1,:),coords(2,:),coords(3,:));
 		data = overlayData(indices);
-		if diff(overlayClip) > 0
+        if diff(overlayClip) > 0
 			indices = find(data >= overlayClip(1) & data <= overlayClip(2));
 		else
 			indices = find(data >= overlayClip(1) | data <= overlayClip(2));
-		end
-		ROIcoords = ROIcoords(:,indices);
+        end
+        ROIcoords = ROIcoords(:,indices);
 	else
 		ROIcoords = [];
 	end
