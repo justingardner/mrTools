b84f6a631ba38d428b9369c5f8ad4e9f5670f36f
justin <justin@d7859a6d-3d6a-0410-b503-b92f2c4db295>
2007-03-20 21:10:02 +0000

    added loadScan

---
+++ mrLoadRet/Analysis/EventRelated/loadScan.m
@@ -0,0 +1,63 @@
+% loadScan.m
+%
+%      usage: loadScan()
+%         by: justin gardner
+%       date: 03/20/07
+%    purpose: loads a scan into a "d" structure
+%
+function d = loadScan(view,scanNum,groupNum,sliceNum)
+
+if ~any(nargin == [1 2 3 4])
+  help('loadScan');
+  return
+end
+
+% default to loading all slices
+if ~exist('groupNum','var'),groupNum = [];end
+if ~exist('sliceNum','var'),sliceNum = [];end
+
+% set the group number
+if ~isempty(groupNum)
+  view = viewSet(view,'curGroup',groupNum);
+end
+% load parameters
+d.ver = 4.5;
+d.tr = viewGet(view,'framePeriod',scanNum);
+d.voxelSize = viewGet(view,'scanvoxelsize',scanNum);
+d.nFrames = viewGet(view,'nFrames',scanNum);
+d.dim(4) = d.nFrames;
+d.filename = viewGet(view,'tseriesfile',scanNum);
+d.filepath = viewGet(view,'tseriespathstr',scanNum);
+[d.fullpath d.expname] = fileparts(pwd);
+
+% dispay string to say what we are loading
+mrDisp(sprintf('Loading scan %i from group: %s',scanNum,viewGet(view,'groupName')));
+if isempty(sliceNum)
+  myDisp(sprintf('\n'));
+elseif length(sliceNum) == 2
+  mrDisp(sprintf(' slices=%i:%i of %i\n',sliceNum(1),sliceNum(2),viewGet(view,'nSlices',scanNum)));
+else
+  mrDisp(sprintf(' slices=%i of %i\n',sliceNum(1),viewGet(view,'nSlices',scanNum)));
+end
+% load the data
+d.data = loadTSeries(view,scanNum,sliceNum);
+	
+% Dump junk frames
+junkFrames = viewGet(view,'junkframes',scanNum);
+d.data = d.data(:,:,:,junkFrames+1:junkFrames+d.nFrames);
+% junk frames total is used by getStimvol to adjust
+% volumes according to how many volumes have been thrown out
+d.junkFrames = viewGet(view,'junkFramesTotal',scanNum);
+
+% load dicom header
+d.dicom = viewGet(view,'dicom',scanNum);
+
+% load stimfile and set traces
+d.stimfile = viewGet(view,'stimfile',scanNum);
+
+% get any concat info
+d.concatInfo = viewGet(view,'concatInfo',scanNum);
+
+% get dimensions
+d.dim = size(d.data);
+
