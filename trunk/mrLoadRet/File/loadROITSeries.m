41b5c491e5ee1797f0e445abca80a220714aec91
justin <justin@d7859a6d-3d6a-0410-b503-b92f2c4db295>
2007-03-28 01:22:35 +0000

    *** empty log message ***

--- trunk/mrLoadRet/File/loadROITSeries.m
+++ trunk/mrLoadRet/File/loadROITSeries.m
@@ -1,28 +1,38 @@
 % loadROITSeries.m
 %
-%      usage: loadROITSeries(view,scanNum,<roiname>)
+%      usage: rois = loadROITSeries(view,<roiname>,<scanNum>,<groupNum>)
 %         by: justin gardner
 %       date: 03/22/07
-%    purpose: load the time series for a roi
+%    purpose: load the time series for a roi, without roiname
+%             specified, brings up selection dialog. roiname
+%             may be a cell array of rois. scanNum and groupNum
+%             default to current scan/group
 %
-function rois = loadROITSeries(view,scanNum,roiname)
+%        e.g.:
+%
+%v = newView('Volume')
+%rois = loadROITseries(v);
+%
+function rois = loadROITSeries(view,roiname,scanNum,groupNum);
 
 rois = {};
 
 % check arguments
-if ~any(nargin == [1 2 3])
+if ~any(nargin == [1 2 3 4])
   help loadROITSeries
   return
 end
 
 % get group and scan
-groupNum = viewGet(view,'currentGroup');
-if ~exist('scanNum','var')
+if ieNotDefined('groupNum')
+  groupNum = viewGet(view,'currentGroup');
+end
+if ieNotDefined('scanNum')
   scanNum = viewGet(view,'currentScan');
 end
 
 % if there is no roi, ask the user to select
-if ~exist('roiname','var')
+if ieNotDefined('roiname')
   roiname = getPathStrDialog(viewGet(view,'roiDir'),'Choose one or more ROIs','*.mat','on');
 elseif isstr(roiname)
   %make into a cell array
@@ -34,22 +44,54 @@ end
 % load the rois in turn
 for roinum = 1:length(roiname)
   % check for file
-  if ~isfile(roiname{roinum})
+  if ~isfile(sprintf('%s.mat',stripext(roiname{roinum})))
     disp(sprintf('(loadROITSeries) Could not find roi %s',roiname{roinum}));
-  end
-  % load the roi
-  roi = load(roiname{roinum});
-  roiFieldnames = fieldnames(roi);
-  % get all the rois
-  for roinum = 1:length(roiFieldnames)
-    rois{end+1} = roi.(roiFieldnames{roinum});
-    % convert to scan coordinates
-    rois{end}.scanCoords = getROICoords(view,groupNum,scanNum,rois{end});
+  else
+    % load the roi
+    roi = load(roiname{roinum});
+    roiFieldnames = fieldnames(roi);
+    % get all the rois
+    for roinum = 1:length(roiFieldnames)
+      rois{end+1} = roi.(roiFieldnames{roinum});
+      % convert to scan coordinates
+      rois{end}.scanCoords = getROICoords(view,groupNum,scanNum,rois{end});
+      % get x y and s in array form
+      x = rois{end}.scanCoords(1,:);
+      y = rois{end}.scanCoords(2,:);
+      s = rois{end}.scanCoords(3,:);
+      % set the n
+      rois{end}.n = length(x);
+      % load the tseries, voxel-by-voxel
+      disppercent(-inf,sprintf('Loading tSeries for %s',rois{end}.name));
+      % for now we always load by block, but if memory is an issue, we can
+      % switch this if statement and load voxels indiviudally from file
+      if 0
+	% load each voxel time series indiviudally
+	for voxnum = 1:rois{end}.n
+	  rois{end}.tSeries(voxnum,:) = squeeze(loadTSeries(view,scanNum,s(voxnum),[],x(voxnum),y(voxnum)));
+	  disppercent(voxnum/rois{end}.n);
+	end
+      else
+	% load the whole time series as a block (i.e. a block including the min and max voxels)
+	% this is usually faster then going back and loading each time series individually
+	% but is more inefficient with memory
+	tSeriesBlock = squeeze(loadTSeries(view,scanNum,[min(s) max(s)],[],[min(x) max(x)],[min(y) max(y)]));
+	% now go through and pick out the voxels that we need.
+	for voxnum = 1:rois{end}.n
+	  rois{end}.tSeries(voxnum,:) = squeeze(tSeriesBlock(x(voxnum)-min(x)+1,y(voxnum)-min(y)+1,s(voxnum)-min(s)+1,:));
+	  disppercent(voxnum/rois{end}.n);
+	end
+	clear tSeriesBlock;
+      end
+      disppercent(inf);
+    end
   end
 end
 
   
-
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% convert the roi coordinates into this scans coordinates
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 function scanCoords = getROICoords(view,groupNum,scanNum,roi)
 
 % get the scan transforms
@@ -62,4 +104,3 @@ scanCoords = round(xformROIcoords(roi.coords,inv(scanXform)*roi.xform,roi.voxelS
 % return the unique ones
 scanCoords = unique(scanCoords','rows')';
 
-keyboard
