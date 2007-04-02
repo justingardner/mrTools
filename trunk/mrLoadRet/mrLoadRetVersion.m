5f21851b8a5837d7dbe0249f8d185a1d631018be
eli <eli@d7859a6d-3d6a-0410-b503-b92f2c4db295>
2007-04-02 15:29:47 +0000

    updated to matlab version 7.4.  seems to work on my intel mac running 7.4 (2007.04.02)

--- trunk/mrLoadRet/mrLoadRetVersion.m
+++ trunk/mrLoadRet/mrLoadRetVersion.m
@@ -2,4 +2,4 @@ function [ver, expectedMatlabVersion] = mrLoadRetVersion
 %	$Id$	
 
 ver = 4.5;
-expectedMatlabVersion = [7.3];  % Change this after testing Matlab upgrades
+expectedMatlabVersion = [7.4];  % Change this after testing Matlab upgrades
