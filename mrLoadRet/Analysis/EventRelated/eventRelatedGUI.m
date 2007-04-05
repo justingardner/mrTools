17858955dc35ae5890fb901a5a0fa2192e861dd1
justin <justin@d7859a6d-3d6a-0410-b503-b92f2c4db295>
2007-04-05 15:11:07 +0000

    pulled out eventRelated gui

---
+++ mrLoadRet/Analysis/EventRelated/eventRelatedGUI.m
@@ -0,0 +1,197 @@
+% eventRelatedGUI.m
+%
+%      usage: eventRelatedGUI()
+%         by: justin gardner
+%       date: 04/05/07
+%    purpose: 
+%
+function params = eventRelatedGUI(varargin)
+
+% check arguments
+if ~any(nargin == [0 1 2 3 4])
+  help eventRelatedGUI
+  return
+end
+
+% get the arguments
+eval(evalargs(varargin));
+
+% get a view
+view = newView('Volume');
+
+% get the group names
+if ieNotDefined('groupName')
+  groupNames = viewGet(view,'groupNames');
+else
+  % if passed in name, put that on top of list to make it the default
+  groupNames = putOnTopOfList(groupName,viewGet(view,'groupNames'));
+end
+
+% check for variable to just useDefaults rather than bring up gui
+if ieNotDefined('useDefault')
+  useDefault = 0;
+end
+
+% set the parameter string
+paramsInfo = {...
+    {'groupName',groupNames,'Name of group from which to do eventRelated analysis'},...
+    {'saveName','erAnal','File name to try to save as'},...
+};
+
+% Get parameter values
+if useDefault
+  params = mrParamsDefault(paramsInfo);
+else
+  params = mrParamsDialog(paramsInfo);
+end
+
+% if empty user hit cancel
+if isempty(params),return,end
+
+% get scans
+view = viewSet(view,'groupName',params.groupName);
+if useDefault
+  params.scanNum = 1:viewGet(view,'nScans');
+else
+  params.scanNum = selectScans(view);
+end
+if isempty(params.scanNum)
+  params = [];
+  return
+end
+
+% get the parameters for each scan
+params.scanParams = getEventRelatedParams(view,params,useDefault);
+if isempty(params.scanParams)
+  params = [];
+  return
+end
+
+% check parameters
+params = mrParamsReconcile([],params);
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% function to get the variable name that the user wants
+% to do the event related analysis on, puts up a gui
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function scanParams = getEventRelatedParams(view,params,useDefault);
+
+% make the output as long as the number of scans
+scanParams = cell(1,viewGet(view,'nScans',viewGet(view,'groupNum',params.groupName)));
+
+% check for stimfile, and if it is mgl/type then ask the
+% user which variable they want to do the anlysis on
+for scanNum = 1:length(params.scanNum)
+  % get scan and default description
+  scanInfo = sprintf('%i: %s',params.scanNum(scanNum),viewGet(view,'description',params.scanNum(scanNum)));
+  description = sprintf('Event related analysis of %s: %i',params.groupName,params.scanNum(scanNum));
+  % standard parameters to set
+  taskVarParams = {...
+      {'scan',scanInfo,'type=statictext','Description of scan to set parameters for (not editable)'},...
+      {'description',description,'Event related analysis of [x...x]','Description of the analysis'}...
+      {'hdrlen',25,'Length of response in seconds to calculate'}...
+      {'preprocess','','String of extra commands for preprocessing (see wiki for details)'}...
+		  };
+
+  % make sure we are running on a set with a stimfile
+  stimfile = viewGet(view,'stimfile',params.scanNum(scanNum));
+  
+  if isempty(stimfile)
+    mrMsgBox(sprintf('No associated stimfile with scan %i in group %s',params.scanNum(scanNum),params.groupName));
+    scanParams = [];
+    return
+  end
+
+  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+  % see if we have a stimfile from mgl, in which case we should
+  % ask the user what the variable name is that they want ot use for the analysis
+  if strfind(stimfile{1}.filetype,'mgl')
+
+    % check to see what style this is, if the task variable does
+    % not have a segmentTrace then it mus be an old style, in which
+    % we used channels
+    if isfield(stimfile{1}.myscreen,'traces') && ...
+	  ((iscell(stimfile{1}.task{1}) && ~isfield(stimfile{1}.task{1}{1},'segmentTrace')) ||...
+	   (~iscell(stimfile{1}.task{1}) && ~isfield(stimfile{1}.task{1},'segmentTrace')))
+      % this is the old style, get the stimtrace number
+      taskVarParams{end+1} = {'stimtrace',stimfile{1}.myscreen.stimtrace,'the trace number that contains the stimulus','incdec=[-1 1]',sprintf('minmax=[%i %i]',stimfile{1}.myscreen.stimtrace,size(stimfile{1}.myscreen.traces,1))};
+    else
+      % this is the new tyle, ask for a variable name
+      [varnames varnamesStr] = getTaskVarnames(stimfile{1}.task);
+      % if there is more than one task, then ask the user for that
+      if length(stimfile{1}.task)>1
+	taskVarParams{end+1} = {'taskNum',num2cell(1:length(stimfile{1}.task)),'The task you want to use'};
+      end
+      % if there are multiple phases, then ask for that
+      maxPhaseNum = 0;
+      for tnum = 1:length(stimfile{1}.task)
+	phaseNum{tnum} = num2cell(1:length(stimfile{1}.task{tnum}));
+	maxPhaseNum = max(maxPhaseNum,length(stimfile{1}.task{tnum}));
+      end
+      if maxPhaseNum > 1
+	if length(stimfile{1}.task) == 1
+	  taskVarParams{end+1} = {'phaseNum',phaseNum{1},'The phase of the task you want to use'};
+	else
+	  taskVarParams{end+1} = {'phaseNum',phaseNum,'The phase of the task you want to use','contingent=taskNum'};
+	end
+      end
+      % set up to get the variable name from the user
+      taskVarParams{end+1} ={'varname',varnames{1},sprintf('Analysis variables: %s',varnamesStr)};
+    end
+  end
+  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    
+  % give the option to use the same variable for all
+  if (scanNum == 1) && (length(params.scanNum)>1)
+    taskVarParams{end+1} = {'sameForAll',1,'type=checkbox','Use the same variable name for all analyses'};
+  end
+    
+  %%%%%%%%%%%%%%%%%%%%%%%
+  % now we have all the dialog information, ask the user to set parameters
+  if useDefault
+    scanParams{params.scanNum(scanNum)} = mrParamsDefault(taskVarParams);
+  else
+    scanParams{params.scanNum(scanNum)} = mrParamsDialog(taskVarParams);
+  end
+  % user hit cancel
+  if isempty(scanParams{params.scanNum(scanNum)})
+    scanParams = [];
+    return
+  end
+  %%%%%%%%%%%%%%%%%%%%%%%
+    
+  % check if the varname is a cell array, then convert to a cell array
+  % instead of a string this is so that the user can specify a variable
+  % name like {{'varname'}}
+  if (isfield(scanParams{params.scanNum(scanNum)},'varname') &&...
+      isstr(scanParams{params.scanNum(scanNum)}.varname) && ...
+      (length(scanParams{params.scanNum(scanNum)}.varname) > 1) && ...
+      (scanParams{params.scanNum(scanNum)}.varname(1) == '{'))
+    scanParams{params.scanNum(scanNum)}.varname = eval(scanParams{params.scanNum(scanNum)}.varname);
+  end
+
+  % if we set stimtrace for backwards compatibility make an alias
+  % to this and call it varname
+  if isfield(scanParams{params.scanNum(scanNum)},'stimtrace')
+    scanParams{params.scanNum(scanNum)}.varname = scanParams{params.scanNum(scanNum)}.stimtrace;
+  end
+  
+  % if sameForAll is set, copy all parameters into all scans and break out of loop
+  if isfield(scanParams{params.scanNum(scanNum)},'sameForAll') && ...
+	scanParams{params.scanNum(scanNum)}.sameForAll
+    for i = 2:length(params.scanNum)
+      % set the other scans params to the same as this one
+      scanParams{params.scanNum(i)} = scanParams{params.scanNum(1)};
+      % change the description field appropriately for this scan num
+      description = scanParams{params.scanNum(1)}.description;
+      groupNameLoc = strfind(description,params.groupName);
+      if ~isempty(groupNameLoc)
+	description = sprintf('%s%s: %i',description(1:groupNameLoc(1)),params.groupName,params.scanNum(i));
+      end
+      scanParams{params.scanNum(i)}.description = description;
+    end
+    break
+  end
+  taskVarParams = {};
+end
+
