848d06ef3353aea7a09abc47123510a3d3b0be5f
justin <justin@d7859a6d-3d6a-0410-b503-b92f2c4db295>
2007-04-12 23:07:56 +0000

    yes to all

--- mrUtilities/MatlabUtilities/askuser.m
+++ mrUtilities/MatlabUtilities/askuser.m
@@ -5,23 +5,29 @@
 %       date: 02/08/06
 %    purpose: ask the user something
 %
-function r = askuser(question)
+function r = askuser(question,toall)
 
 % check arguments
-if ~any(nargin == [1])
+if ~any(nargin == [1 2])
   help askuser
   return
 end
-
+if ieNotDefined('toall'),toall = 0;,end
 r = [];
 while isempty(r)
   % ask the question
-  r = input(sprintf('%s (y/n)? ',question),'s');
+  if toall
+    r = input(sprintf('%s (y/n or a for Yes to all)? ',question),'s');
+  else
+    r = input(sprintf('%s (y/n)? ',question),'s');
+  end
   % make sure we got a valid answer
   if (lower(r) == 'n')
     r = 0;
   elseif (lower(r) == 'y')
     r = 1;
+  elseif (lower(r) == 'a') & toall
+    r = inf;
   else
     r =[];
   end
