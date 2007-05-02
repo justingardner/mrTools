7514a78ed97451520f90f9319cdfd5a909f50dd6
justin <justin@d7859a6d-3d6a-0410-b503-b92f2c4db295>
2007-05-02 16:04:01 +0000

    scanInfo displays in mrParamsDialog option

--- mrLoadRet/File/scanInfo.m
+++ mrLoadRet/File/scanInfo.m
@@ -1,18 +1,20 @@
 % scanInfo.m
 %
-%      usage: scanInfo(scanNum,groupNum)
+%      usage: scanInfo(scanNum,groupNum,<displayInDialog>)
 %         by: justin gardner
 %       date: 11/08/06
 %    purpose: print out information about scans in group
 %       e.g.: groupInfo(1);
 %             groupInfo('Raw');
-function retval = scanInfo(scanNum,groupNum)
+function retval = scanInfo(scanNum,groupNum,displayInDialog)
 
 % check arguments
-if ~any(nargin == [2])
+if ~any(nargin == [2 3])
   help scanInfo
   return
 end
+
+if ~exist('displayInDialog','var'),displayInDialog = 0;end
 
 view = newView('Volume');
 
@@ -48,26 +50,45 @@ scanHdr = viewGet(view,'niftiHdr',scanNum,groupNum);
 scanDims = viewGet(view,'scanDims',scanNum,groupNum);
 
 % display info
-disp(sprintf('%s',description));
-disp(sprintf('Filename: %s GroupName: %s',filename,groupName));
-for i = 1:length(originalFilename)
-  disp(sprintf('Original Filename: %s Group: %s',originalFilename{i},originalGroupname{i}));
-end
-for i = 1:length(stimFilename)
-  disp(sprintf('StimFilename: %s',stimFilename{i}));
-end
+if displayInDialog
+  paramsInfo = {{'description',description,'editable=0','Scan description'},...
+		{'Filename',filename,'editable=0','Name of file'},...
+		{'GroupName',groupName,'editable=0','Name of group'}};
+  for i = 1:length(originalFilename)
+    paramsInfo{end+1} = {sprintf('Original%i',i) sprintf('%s: %s',originalGroupname{i},originalFilename{i}) 'editable=0' 'Name of original group and filename that this scan came from'};
+  end
+  for i = 1:length(stimFilename)
+    paramsInfo{end+1} = {sprintf('Stimfile%i',i) stimFilename{i} 'editable=0' 'Name of stim file'};
+  end
+  paramsInfo{end+1} = {'voxelSize',scanVoxelSize,'editable=0','Voxel dimensions in mm'};
+  paramsInfo{end+1} =  {'dims',scanDims,'editable=0','Dimensions of scan'};
+  paramsInfo{end+1} =  {'TR',tr,'editable=0','TR'};
+  paramsInfo{end+1} =  {'numVolumes',totalFrames,'editable=0','Number of volumes'};
+  paramsInfo{end+1} = {'qform',scanHdr.qform44,'editable=0','Qform matrix specifies the transformation to the scanner coordinate frame'};
+  paramsInfo{end+1} = {'sform',scanHdr.sform44,'editable=0','Sform matrix is set by mrAlign and usually specifies the transformation to the volume anatomy'};
+  mrParamsDialog(paramsInfo,'Scan info');
+else
+  disp(sprintf('%s',description));
+  disp(sprintf('Filename: %s GroupName: %s',filename,groupName));
+  for i = 1:length(originalFilename)
+    disp(sprintf('Original Filename: %s Group: %s',originalFilename{i},originalGroupname{i}));
+  end
+  for i = 1:length(stimFilename)
+    disp(sprintf('StimFilename: %s',stimFilename{i}));
+  end
   
-disp(sprintf('voxelSize=[%0.1f %0.1f %0.1f] TR=%0.4f Dims: [%i %i %i] Volumes=%i',scanVoxelSize(1),scanVoxelSize(2),scanVoxelSize(3),tr,scanDims(1),scanDims(2),scanDims(3),totalFrames));
+  disp(sprintf('voxelSize=[%0.1f %0.1f %0.1f] TR=%0.4f Dims: [%i %i %i] Volumes=%i',scanVoxelSize(1),scanVoxelSize(2),scanVoxelSize(3),tr,scanDims(1),scanDims(2),scanDims(3),totalFrames));
 
-% display qform and sform
-disp(sprintf('++++++++++++++++++++++++++ qform ++++++++++++++++++++++++++'));
-for rownum = 1:4
-  disp(sprintf('%f\t%f\t%f\t%f',scanHdr.qform44(rownum,1),scanHdr.qform44(rownum,2),scanHdr.qform44(rownum,3),scanHdr.qform44(rownum,4)));
-end
+  % display qform and sform
+  disp(sprintf('++++++++++++++++++++++++++ qform ++++++++++++++++++++++++++'));
+  for rownum = 1:4
+    disp(sprintf('%f\t%f\t%f\t%f',scanHdr.qform44(rownum,1),scanHdr.qform44(rownum,2),scanHdr.qform44(rownum,3),scanHdr.qform44(rownum,4)));
+  end
 
-disp(sprintf('++++++++++++++++++++++++++ sform ++++++++++++++++++++++++++'));
-for rownum = 1:4
-  disp(sprintf('%f\t%f\t%f\t%f',scanHdr.sform44(rownum,1),scanHdr.sform44(rownum,2),scanHdr.sform44(rownum,3),scanHdr.sform44(rownum,4)));
+  disp(sprintf('++++++++++++++++++++++++++ sform ++++++++++++++++++++++++++'));
+  for rownum = 1:4
+    disp(sprintf('%f\t%f\t%f\t%f',scanHdr.sform44(rownum,1),scanHdr.sform44(rownum,2),scanHdr.sform44(rownum,3),scanHdr.sform44(rownum,4)));
+  end
 end
 
 
