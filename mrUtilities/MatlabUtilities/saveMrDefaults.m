02ce61ae0f421372f30385837a6960586025c962
david <david@d7859a6d-3d6a-0410-b503-b92f2c4db295>
2007-05-08 13:52:08 +0000

    loadMrDefaults.m saveMrDefaults.m moved from mrLoadRet to mrUtilities

---
+++ mrUtilities/MatlabUtilities/saveMrDefaults.m
@@ -0,0 +1,33 @@
+% saveMrDefaults
+%
+%      usage: saveMrDefaults()
+%         by: justin gardner
+%       date: 03/17/07
+%    purpose: save default positions
+%
+function retval = saveMrDefaults()
+
+% check arguments
+if ~any(nargin == [0])
+  help saveMrDefaults
+  return
+end
+
+% get globals
+mrGlobals;
+
+% save figloc
+if isfield(MLR,'figloc')
+  figloc = MLR.figloc;
+else
+  figloc = [];
+end
+
+if isfield(MLR,'prefs')
+  prefs = MLR.prefs;
+else
+  prefs = [];
+end
+
+
+save '~/.mrDefaults.mat' figloc prefs -V6;
