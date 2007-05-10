82c4283e4552864ac36575b46b27a5a9ed81e8ac
farshadm <farshadm@d7859a6d-3d6a-0410-b503-b92f2c4db295>
2007-05-10 23:28:46 +0000

    eventRelatedMultiple.m updated and added to GUI

--- mrLoadRet/Analysis/EventRelated/eventRelatedMultiple.m
+++ mrLoadRet/Analysis/EventRelated/eventRelatedMultiple.m
@@ -5,13 +5,15 @@
 %       date: 02/05/07
 %    purpose: eventrelated for multiple files
 %
-function view = eventRelatedMultiple(view,params)
+function [view d] = eventRelatedMultiple(view,params)
 
 % check arguments
 if ~any(nargin == [1 2])
   help(mfilename)
   return
 end
+
+d = [];
 
 % First get parameters
 if ieNotDefined('params')
@@ -44,12 +46,16 @@ if nhdr<=0
     return
 end
 
+set(viewGet(view,'figNum'),'Pointer','watch');drawnow;
 for i=params.includeScans
     % load the scan
     d = loadScan(view,i);
     d.scms = scms;
     d.volumes = volumes{i};
+    % do any called for preprocessing
+    d = eventRelatedPreProcess(d,params.scanParams{i}.preprocess);    
     % compute the hemodynamic responses
+    d.scanNum = i;
     d = calcleastsqestimate(d);
     ehdr = ehdr+d.ehdr;
     meanintensity = meanintensity+d.meanintensity;
@@ -66,6 +72,9 @@ for i=params.includeScans
     d.ehdr = ehdr;
     d.scm = scms{i};
     d.volumes = volumes{i};
+    % do any called for preprocessing
+    d = eventRelatedPreProcess(d,params.scanParams{i}.preprocess);    
+    % compute the residual error    
     d = calcVariances(d);
     unexplainedVariance = unexplainedVariance+d.unexplainedVariance;
     totalVariance = totalVariance+d.totalVariance;
@@ -79,10 +88,10 @@ d.meanintensity = meanintensity;
 
 % create the r2 overlay
 dateString = datestr(now);
-r2.name = 'co';
-r2.function = 'corAnal';
+r2.name = 'r2';
 r2.groupName = params.groupName;
-r2.reconcileFunction = 'eventRelatedReconcileParams';
+r2.function = 'eventRelated';
+r2.reconcileFunction = 'mrParamsReconcile';
 r2.data = cell(1,viewGet(view,'nScans'));
 for i=params.includeScans(1)
     r2.data{i}=d.r2;
@@ -91,9 +100,17 @@ r2.date = dateString;
 r2.params = params;
 r2.range = [0 1];
 r2.clip = [0 1];
-r2.colormap = hot(256);
+r2.params = cell(1,viewGet(view,'nScans'));
+for i=params.includeScans
+    r2.params{i} = params.scanParams{i};
+end;
+% colormap is made with a little bit less on the dark end
+r2.colormap = hot(312);
+r2.colormap = r2.colormap(end-255:end,:);
 r2.alpha = 1;
+r2.colormapType = 'setRangeToMax';
 r2.interrogator = 'eventRelatedPlot';
+
 
 % install analysis
 erAnal.name = 'erAnal';  % This can be reset by editAnalysisGUI
@@ -108,7 +125,7 @@ erAnal.curOverlay = 1;
 erAnal.date = dateString;
 erAnal.ehdr = ehdr./repmat(meanintensity, [1,1,1,size(ehdr,4)])*100;
 erAnal.nhdr = nhdr;
-erAnal.d = d;
+erAnal.d = {d};
 
 view = viewSet(view,'newAnalysis',erAnal);
 
@@ -116,29 +133,8 @@ view = viewSet(view,'newAnalysis',erAnal);
 if ~isfield(params, 'suppress_save')
     saveAnalysis(view,erAnal.name);
 end
+set(viewGet(view,'figNum'),'Pointer','arrow');drawnow
 
-
-%%%%%%%%%%%%%%%%%%%
-% loadScan
-%%%%%%%%%%%%%%%%%%%
-function d = loadScan(view,scanNum)
-
-% load parameters
-d.tr = viewGet(view,'framePeriod',scanNum);
-d.voxelSize = viewGet(view,'scanvoxelsize',scanNum);
-d.dim = viewGet(view,'scandims',scanNum);
-d.nFrames = viewGet(view,'nFrames',scanNum);
-d.dim(4) = d.nFrames;
-d.scanNum = scanNum;
-% Load data
-mrDisp(sprintf('Loading scan %i from %s\n',scanNum,viewGet(view,'groupName')));
-d.data = loadTSeries(view,scanNum,'all');
-% Dump junk frames
-junkFrames = viewGet(view,'junkframes',scanNum);
-d.data = d.data(:,:,:,junkFrames+1:junkFrames+d.nFrames);
-% load dicom header
-d.dicom = viewGet(view,'dicom',scanNum);
-d.baseline = ones(d.dim);
 
 %%%%%%%%%%%%%%%%%%%
 % getFullDesign
@@ -146,118 +142,43 @@ d.baseline = ones(d.dim);
 function [scms, volumes, nhdr] = getFullDesign(view, params)
 scms=[];
 volumes = [];
-nhdr = 0;
+nhdr = [];
+accumLength = 1;
+
+d.stimfile = cell(1, max(params.includeScans));
+d.concatInfo.runTransition = zeros(max(params.includeScans), 2);
+d.tr = [];
+
 for i=params.includeScans
     stimfile = viewGet(view,'stimfile',i);
     if length(stimfile) == 1
-      stimfile = stimfile{1};
-      switch stimfile.filetype,
-          case 'traces',
-            % get acquisition times. 2 means volume acq (we don't know slice acq)
-            stimfile.acq = [0 2*(diff(d.traces(1,:))==1)];
-          case 'eventtimes',
-          otherwise,
-            mrMsgBox(sprintf('Unknown of invalid stimfile type %s',stimfile.filetype));
-      end
-    else
-        disp(i);
-        error('invalid number of stimulus files');
-    end
-    d.filetype = stimfile.filetype;
-    stimfile.tr = viewGet(view,'framePeriod',i);
-    d = getStimVol(stimfile);
+        d.stimfile{i} = stimfile{1};
+    end;
+    nFrames = viewGet(view,'nFrames',i);
+    d.concatInfo.runTransition(i,:) = accumLength+[0 nFrames-1];
+    accumLength = accumLength+nFrames;
+    if isempty(d.tr)
+        d.tr = viewGet(view,'framePeriod',i);
+    elseif d.tr ~= viewGet(view,'framePeriod',i)
+        disp('Incompatible TRs');
+        retrun;
+    end;
     d.dim = viewGet(view,'scandims',i);
-    d.nFrames = viewGet(view,'nFrames',i);
-    d.dim(4) = d.nFrames;
-    d = makescm(d,ceil(params.hdrlen/d.tr));
-    scms{i}=d.scm;
-    volumes{i}=d.volumes;
-    if nhdr==0
-        nhdr=d.nhdr;
-    end
-    if nhdr~=d.nhdr
-        nhdr = -1;
-        return
-    end
+    volumes{i}=1:nFrames;
+end;
+% get the stim volumes, if empty then abort
+d = getStimvol(d);
+if isempty(d.stimvol),mrWarnDlg('No stim volumes found');return,end
+nhdr=length(d.stimvol);
+d.dim(4) = accumLength;
+% make a stimulation convolution matrix
+d = makescm(d,ceil(params.hdrlen/d.tr));
+for i=params.includeScans
+    if d.concatInfo.runTransition(i,1)>0
+        scms{i}=d.scm(d.concatInfo.runTransition(i,1):d.concatInfo.runTransition(i,2),:);
+    end;
 end;
 
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% getstimvol.m
-%
-%      usage: d = getStimVol(d)
-%         by: justin gardner
-%       date: 12/21/05
-%    purpose: gets the stimulus vols from the traces
-%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-function d = getStimVol(d,keepstim)
-
-% check arguments
-if ~any(nargin == [1 2])
-  help getstimvol
-  return
-end
-
-if exist('keepstim','var')~=1,keepstim = []; end;
-switch d.filetype,
-    case 'traces',
-        d = getStimVolFromTraces(d.myscreen);
-    case 'eventtimes',
-        d = getStimVolFromEventTimes(d.mylog, d.tr);
-end;
-
-% get the first volume
-d = getfirstvol(d);
-
-% check if we need to only keep some vols
-if ~isempty(keepstim)
-  if (min(keepstim) < 1) || (max(keepstim) > length(d.stimvol))
-    mrDisp(sprintf('UHOH: Keepstim out of range. Ignoring'));
-  else
-    % go through and only keep the stim values asked for
-    stimvol = [];
-    for i = 1:length(keepstim)
-      stimvol{i} = d.stimvol{keepstim(i)};
-    end
-    d.stimvol = stimvol;
-  end
-end
-
-
-function d = getStimVolFromTraces(d)
-% get the stim times
-stimraw = d.traces(d.stimtrace,:);
-stimraw(stimraw < 0) = 0;
-stimtimes = find([0 (diff(stimraw)~=0)]);
-
-% get the image number
-acqnum = cumsum(d.acq>1);
-
-% set the beginning acqnum to 1, so that
-% any event that happens before the first
-% acquistion pulse is assumed to happen
-% during the first acquisition pulse.
-acqnum(1:first(find(acqnum == 1))) = 1;
-
-% sort into stimuli
-nhdr = max(stimraw);
-for i = 1:nhdr
-  d.stimtimes{i} = stimtimes(stimraw(stimtimes) == i);
-  d.pulselens(i) = i;
-  d.stimvol{i} = acqnum(d.stimtimes{i});
-end
-
-
-function d = getStimVolFromEventTimes(d, tr)
-% sort into stimuli
-nhdr = length(d.stimtimes_s);
-for i = 1:nhdr
-  d.stimtimes{i} = d.stimtimes_s{i};
-  d.pulselens(i) = i;
-  d.stimvol{i} = round(d.stimtimes_s{i} / tr);
-end
-d.tr = tr;
 
 %%%%%%%%%%%%%%%%%%%
 % calcVariances
