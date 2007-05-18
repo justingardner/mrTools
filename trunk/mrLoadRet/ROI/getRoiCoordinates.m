cde9286bb5dae0cecf0b555b920ad738c76cffe9
justin <justin@d7859a6d-3d6a-0410-b503-b92f2c4db295>
2007-05-18 20:22:44 +0000

    getROiCoordinates can get base coordinates too

--- trunk/mrLoadRet/ROI/getRoiCoordinates.m
+++ trunk/mrLoadRet/ROI/getRoiCoordinates.m
@@ -4,7 +4,10 @@
 %         by: justin gardner
 %       date: 04/02/07
 %    purpose: get roi coordinates in scan coordinates
-%
+%             if scanNum is 0, then will compute in base
+%             coordinates. 
+%             if roinum is a structure, works on the structure
+%             rather than the roinum
 function scanCoords = getRoiCoordinates(view,roiNum,scanNum,groupNum)
 
 % check arguments
@@ -22,14 +25,25 @@ if ieNotDefined('scanNum')
 end
 
 % get the scan transforms
-scanXform = viewGet(view,'scanXform',scanNum,groupNum);
-scanVoxelSize = viewGet(view,'scanVoxelSize',scanNum,groupNum);
+if scanNum
+  scanXform = viewGet(view,'scanXform',scanNum,groupNum);
+  scanVoxelSize = viewGet(view,'scanVoxelSize',scanNum,groupNum);
+else
+  view = viewSet(view,'curGroup',groupNum);
+  scanXform = viewGet(view,'baseXform');
+  scanVoxelSize = viewGet(view,'baseVoxelSize');
+end  
 
 % get the roi transforms
-roiXform = viewGet(view,'roiXform',roiNum);
-roiVoxelSize = viewGet(view,'roiVoxelSize',roiNum);
-roiCoords = viewGet(view,'roiCoords',roiNum);
-
+if isstruct(roiNum)
+  roiXform = roiNum.xform;
+  roiVoxelSize = roiNum.voxelSize;
+  roiCoords = roiNum.coords;
+else
+  roiXform = viewGet(view,'roiXform',roiNum);
+  roiVoxelSize = viewGet(view,'roiVoxelSize',roiNum);
+  roiCoords = viewGet(view,'roiCoords',roiNum);
+end
 % Use xformROI to supersample the coordinates
 scanCoords = round(xformROIcoords(roiCoords,inv(scanXform)*roiXform,roiVoxelSize,scanVoxelSize));
 
