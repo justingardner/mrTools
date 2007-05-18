76e1b66a5ba44877c52dbe7ed431ae22cf597dbb
justin <justin@d7859a6d-3d6a-0410-b503-b92f2c4db295>
2007-05-18 20:23:05 +0000

    minor change to get rid of warning with 'String'

--- trunk/mrUtilities/MatlabUtilities/evalargs.m
+++ trunk/mrUtilities/MatlabUtilities/evalargs.m
@@ -29,7 +29,11 @@ for i = 1:length(args)
   % evaluate anything that has an equal sign in it
   if isstr(args{i}) && ~isempty(strfind(args{i},'='))
     % if the argument is a numeric, than just set it
-    if (exist(args{i}(strfind(args{i},'=')+1:end)) ~= 2) && ~isempty(str2num(args{i}(strfind(args{i},'=')+1:end)))
+    % also if the argument happens to be string, don't
+    % even do str2num on that since it gives an annoyinw warning
+    if ((exist(args{i}(strfind(args{i},'=')+1:end)) ~= 2) && ...
+	~strcmp(args{i}(strfind(args{i},'=')+1:end),'string') && ...
+	~isempty(str2num(args{i}(strfind(args{i},'=')+1:end))))
       evalstr = sprintf('%s%s;',evalstr,args{i});
     % same for a quoted string
     elseif args{i}(strfind(args{i},'=')+1)==''''
