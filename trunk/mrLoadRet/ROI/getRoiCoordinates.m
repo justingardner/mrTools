f25576de4effd5a3d44c8a8a6c297a0152bad2fd
justin <justin@d7859a6d-3d6a-0410-b503-b92f2c4db295>
2007-05-21 22:44:55 +0000

    loadROITSeries calls getRoiCoordinates for coordinates saveAnalysis got changed to handle defaultMerge

--- trunk/mrLoadRet/ROI/getRoiCoordinates.m
+++ trunk/mrLoadRet/ROI/getRoiCoordinates.m
@@ -44,6 +44,7 @@ else
   roiVoxelSize = viewGet(view,'roiVoxelSize',roiNum);
   roiCoords = viewGet(view,'roiCoords',roiNum);
 end
+
 % Use xformROI to supersample the coordinates
 scanCoords = round(xformROIcoords(roiCoords,inv(scanXform)*roiXform,roiVoxelSize,scanVoxelSize));
 
@@ -52,3 +53,15 @@ scanCoords = unique(scanCoords','rows')';
 if ~isempty(scanCoords)
   scanCoords = scanCoords(1:3,:);
 end
+
+% check scan dimensions
+scanDims = viewGet(view,'dims',scanNum,groupNum);
+
+% make sure we are inside scan dimensions
+xCheck = (scanCoords(1,:) >= 1) & (scanCoords(1,:) <= scanDims(1));
+yCheck = (scanCoords(2,:) >= 1) & (scanCoords(2,:) <= scanDims(2));
+sCheck = (scanCoords(3,:) >= 1) & (scanCoords(3,:) <= scanDims(3));
+
+% only return ones that are in bounds
+scanCoords = scanCoords(:,find(xCheck & yCheck & sCheck));
+
