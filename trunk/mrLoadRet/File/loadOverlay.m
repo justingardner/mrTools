90a5b969a718109434d64fb0d7c1c623d8939e73
david <david@d7859a6d-3d6a-0410-b503-b92f2c4db295>
2007-05-23 20:03:07 +0000

    loadAnalysis.m loadOverlay.m Works if filename argument is passed with or without '.mat'

--- trunk/mrLoadRet/File/loadOverlay.m
+++ trunk/mrLoadRet/File/loadOverlay.m
@@ -51,10 +51,11 @@ else
 	if iscell(filename)
 		pathStr = cell(size(filename));
 		for p=1:length(pathStr)
-			pathStr{p} = fullfile(startPathStr,[filename{p},'.mat']);
+			pathStr{p} = fullfile(startPathStr,filename{p});
+      pathStr{p} = [stripext(pathStr{p}),'.mat'];
 		end
 	else
-		pathStr = {fullfile(startPathStr,[filename,'.mat'])};
+		pathStr = {[stripext(fullfile(startPathStr,filename)),'.mat']};
 	end
 end
 
@@ -67,6 +68,7 @@ end
 if ~iscell(pathStr)
     pathStr = {pathStr};
 end
+
 
 % Loop through the filenames, load each file, and add each of them as a new
 % overlay.
