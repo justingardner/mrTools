b59dad16109034814b2ca310795e27e562931443
david <david@d7859a6d-3d6a-0410-b503-b92f2c4db295>
2007-05-23 16:42:59 +0000

    corAnal.m using defaults for some of the overlay and analysis fields isanalysis.m isoverlay.m Added optional fields with default values. These functions now return either 0 or the modified structure with optional fields added and default values set.
    viewSet.m
    Changed calls to isanalysis and isoverlay to use returned (possibly modified) structure with optional fields set.

--- mrLoadRet/View/isoverlay.m
+++ mrLoadRet/View/isoverlay.m
@@ -15,12 +15,33 @@ if ~isstruct(overlay)
 	return
 end
 
-requiredFields = {'alpha','clip','colormap','data','date','function',...
-	'groupName','interrogator','name','params','range','reconcileFunction'};
+requiredFields = {'function','groupName','name','params','range'};
 for f = 1:length(requiredFields)
 	fieldName = requiredFields{f};
 	if ~isfield(overlay,fieldName)
 		% mrWarnDlg(['Invalid overlay, missing field: ',fieldName]);
 		val = 0;
 	end
+end
+
+% Optional fields and defaults
+optionalFields = {'date',dateString;
+  'type',overlay.name;
+  'alpha',1;
+  'clip',overlay.range;
+  'colormap',jet(256);
+  'interrogator','mrDefaultInterrogator';
+  'reconcileFunction','defaultReconcileFunction';
+  'mergeFunction','defaultMergeFunction';
+  'data',[]};
+for f = 1:length(optionalFields)
+	fieldName = optionalFields{f,1};
+  default = optionalFields{f,2};
+  if ~isfield(analysis,fieldName)
+    analysis.fieldName = default;
+  end
+end
+
+if val ==1
+  val = orderfields(overlay);
 end
