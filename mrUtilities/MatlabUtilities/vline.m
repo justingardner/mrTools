b6b3e91aca052e26782a6676af8e7be3d2764950
justin <justin@d7859a6d-3d6a-0410-b503-b92f2c4db295>
2007-05-24 14:09:59 +0000

    returns handles

--- mrUtilities/MatlabUtilities/vline.m
+++ mrUtilities/MatlabUtilities/vline.m
@@ -5,19 +5,19 @@
 %    date: 01/21/99
 % purpose: draws a vertical line on the current axis
 %
-function vline(hpos,linetype)
+function h = vline(hpos,linetype)
 
 ax = axis;
-
+h = [];
 if (nargin == 1)
   for i = 1:length(hpos)
     hold on
-    plot([hpos(i) hpos(i)],[ax(3) ax(4)],'k:');
+    h(i) = plot([hpos(i) hpos(i)],[ax(3) ax(4)],'k:');
   end
 elseif (nargin == 2)
   for i = 1:length(hpos)
     hold on
-    plot([hpos(i) hpos(i)],[ax(3) ax(4)],linetype);
+    h(i) = plot([hpos(i) hpos(i)],[ax(3) ax(4)],linetype);
   end
 else
   help vline
