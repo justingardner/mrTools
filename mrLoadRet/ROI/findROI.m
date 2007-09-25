bf52223db5ffcb541799e931ddb738355a1d6e9e
justin <justin@d7859a6d-3d6a-0410-b503-b92f2c4db295>
2007-09-25 16:16:36 +0000

    *** empty log message ***

---
+++ mrLoadRet/ROI/findROI.m
@@ -0,0 +1,50 @@
+% findROI.m
+%
+%        $Id$
+%      usage: findROI()
+%         by: justin gardner
+%       date: 09/25/07
+%    purpose: looks for an ROI in the base anatomy (is called form mrLoadRetGUI)
+%
+function retval = findROI(view)
+
+retval = [];
+
+% check arguments
+if ~any(nargin == [1])
+  help findROI
+  return
+end
+
+if ~isempty(viewGet(v,'baseCoordMap'))
+  disp(sprintf('(findROI) Base anatomy cannot be flat to look for an ROI'));
+  return
+end
+% get the roi
+roiNum = viewGet(view,'currentROI');
+if isempty(roiNum),return,end
+
+% get the current scan number
+scanNum = viewGet(view,'curScan');
+% get roi coordinates
+coords = getROICoordinates(view,roiNum,scanNum);
+
+if isempty(coords)
+  msgbox(sprintf('ROI %s has no voxels in the current anatomy',viewGet(view,'roiName',roiNum)));
+  return
+end
+
+% get current slice
+curSlice = viewGet(view,'curSlice');
+
+% go find the ROI
+sliceIndex = viewGet(view,'baseSliceIndex');
+
+% find the closest slice
+distanceToCurrentSlice = abs(coords(sliceIndex,:)-curSlice);
+closestSlice = coords(sliceIndex,first(find(min(distanceToCurrentSlice)==distanceToCurrentSlice)));
+
+% set the slice
+mlrGuiSet(view.viewNum,'slice',closestSlice);
+refreshMLRDisplay(view.viewNum);
+
