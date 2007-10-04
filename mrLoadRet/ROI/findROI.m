617c9404fb9f843cba7ccfa486a7c49de27a17ad
justin <justin@d7859a6d-3d6a-0410-b503-b92f2c4db295>
2007-10-04 21:17:58 +0000

    changes made necessary by tweaking of ROI code

--- mrLoadRet/ROI/findROI.m
+++ mrLoadRet/ROI/findROI.m
@@ -25,23 +25,30 @@ roiNum = viewGet(view,'currentROI');
 if isempty(roiNum),return,end
 
 % get roi coordinates from the cache
-roiCache = viewGet(view,'ROICache',viewGet(view,'currentROI'));
-if isempty(roiCache)
+roi = viewGet(view,'ROICache',viewGet(view,'currentROI'));
+if isempty(roi)
   disp(sprintf('(findROI) ROI cache is emtpy (probably you are not viewing ROIs)'));
   return
 end
 
-if isempty(roiCache.s)
+baseNum = viewGet(view,'currentBase');
+baseName = fixBadChars(viewGet(view,'baseName',baseNum));
+sliceIndex = viewGet(view,'baseSliceIndex',baseNum);
+if ((~isfield(roi,baseName)) || ...
+    (length(roi.(baseName)) < sliceIndex) || ...
+    (isempty(roi.(baseName){sliceIndex})))
   msgbox(sprintf('ROI %s has no voxels in the current anatomy',viewGet(view,'roiName',roiNum)));
   return
+else
+  s = roi.(baseName){sliceIndex}.s;
 end
 
 % get current slice
 curSlice = viewGet(view,'curSlice');
 
 % find the closest slice
-distanceToCurrentSlice = abs(roiCache.s-curSlice);
-closestSlice = roiCache.s(first(find(min(distanceToCurrentSlice)==distanceToCurrentSlice)));
+distanceToCurrentSlice = abs(s-curSlice);
+closestSlice = s(first(find(min(distanceToCurrentSlice)==distanceToCurrentSlice)));
 
 % set the slice
 mlrGuiSet(view.viewNum,'slice',closestSlice);
