08b29211aae5a321c9eae09d571c09f9628a3731
eli <eli@d7859a6d-3d6a-0410-b503-b92f2c4db295>
2007-10-10 21:29:29 +0000

    installs the flatpatch.off as a base anatomy using loadFlatOFF

--- mrLoadRet/Analysis/makeFlat.m
+++ mrLoadRet/Analysis/makeFlat.m
@@ -35,12 +35,15 @@ params.startCoord = xformROIcoords([x;y;s;1], inv(baseSform)*scanXform, baseVoxe
 paramsInfo = {};
 if ~isempty(baseCoordMap)
   % we can get all of the file names from the baseCoordMap
-  paramsInfo{end+1} = {'surfDir',baseCoordMap.flatDir,'editable=0','Directory from which this flat map was originally created'};
+  paramsInfo{end+1} = {'flatDir',baseCoordMap.flatDir,'editable=0','Directory from which this flat map was originally created'};
+  paramsInfo{end+1} = {'whichHemi', baseCoordMap.whichHemi, 'editable=0'};
   paramsInfo{end+1} = {'flatFileName',baseCoordMap.flatFileName,'editable=0','Name of original off file from which this flat map was created'};
   paramsInfo{end+1} = {'innerFileName',baseCoordMap.innerFileName,'editable=0','Name of inner mesh (aka gray matter mesh) from which this flat map was created'};
   paramsInfo{end+1} = {'outerFileName',baseCoordMap.outerFileName,'editable=0','Name of outer mesh (aka white matter mesh) from which this flat map was created'};
   paramsInfo{end+1} = {'curvFileName',baseCoordMap.curvFileName,'editable=0','Name of curvature file from which this flat map was created'};
   paramsInfo{end+1} = {'anatFileName',baseCoordMap.anatFileName,'editable=0','Name of anatomy file from which the xform for this flat map was taken'};
+  paramsInfo{end+1} = {'calcCurvFlag', 0, 'editable=0'};
+
 else
   % we will have to build a list of file names...
   % Open dialog box to have user choose the file
@@ -72,8 +75,8 @@ else
   end
 
   % contents of the current directory
-  [params.surfDir params.innerFileName] = fileparts(innerFileName);
-  dirContents = dir(params.surfDir);
+  [params.flatDir params.innerFileName] = fileparts(innerFileName);
+  dirContents = dir(params.flatDir);
 
   % guess the GM surface
   params.outerFileName = {};
@@ -105,7 +108,7 @@ end
 
 % parse the parameters
 paramsInfo = {};
-paramsInfo{end+1} = {'surfDir', params.surfDir, 'editable=0', 'directory path for the inner .off surface file'};
+paramsInfo{end+1} = {'flatDir', params.flatDir, 'editable=0', 'directory path for the inner .off surface file'};
 paramsInfo{end+1} = {'whichHemi', params.whichHemi, 'editable=0', 'the hemisphere that the patch comes from -- not really important'};
 paramsInfo{end+1} = {'innerFileName', strcat(params.innerFileName, '.off'), 'name of the surface at the white/gray boundary (i.e., the inner surface'};
 paramsInfo{end+1} = {'outerFileName', params.outerFileName, 'name of the surface at the gray/pial boundary (i.e., the outer surface)'};
@@ -131,10 +134,10 @@ if isempty(params)
 end
 
 % check for directory
-if ~isdir(params.surfDir)
-  params.surfDir = uigetdir(mrGetPref('volumeDirectory'),'Find anatomy directory');
+if ~isdir(params.flatDir)
+  params.flatDir = uigetdir(mrGetPref('volumeDirectory'),'Find anatomy directory');
   % user hits cancel
-  if params.surfDir == 0
+  if params.flatDir == 0
     return
   end
 end
@@ -146,19 +149,39 @@ end
 % find the vertex closests to the starting point
 params.startVertex = dsearchn(surf.inner.vtcs, params.startCoord);
 
-params.patchFileName = sprintf('%s_Patch%i.off', stripext(params.innerFileName), params.startVertex);
-params.flatFileName = sprintf('%s_Flat%i.off', stripext(params.innerFileName), params.startVertex);
+% set the name of the patch to cut
+params.patchFileName = sprintf('%s_Patch_%i_%i_%i_Rad%i.off', ...
+                               stripext(params.innerFileName), ...
+                               params.startCoord(1), params.startCoord(2), params.startCoord(3), ...
+                               params.patchRadius);
 
+% set the name of the new flat file
+params.flatFileName = sprintf('%s_Flat_%i_%i_%i_Rad%i.off', ...
+                              stripext(params.innerFileName), ...
+                              params.startCoord(1), params.startCoord(2), params.startCoord(3), ...
+                              params.patchRadius);
+
+% cut and flatten the surface
 myCutAndFlatten(params)
 
+% make it into a MLR4 base anatomy
+base = loadFlatOFF(params);
 
+% install it
+disp(sprintf('(makeFlat) installing new flat base anatomy: %s', params.patchFileName));
+if ~isempty(base)
+  viewNum = 1;                          % dunno how to figure out the right view num
+  viewSet(viewNum, 'newbase', base);
+  refreshMLRDisplay(viewNum);
+end
 
 return;
 
 function myCutAndFlatten(params)
 
 % set lib path
-setenv('DYLD_LIBRARY_PATH', '/Users/eli/src/TFI/sw/lib/');
+mylibs = getenv('DYLD_LIBRARY_PATH');
+setenv('DYLD_LIBRARY_PATH', sprintf('%s:/Users/eli/src/TFI/sw/lib/', mylibs));
 
 % check for the SurfRelax program called 'surfcut'
 [statusCut,result] = system('surfcut');
@@ -177,16 +200,16 @@ else
     % cut the patch from the 3d mesh
     system(sprintf('surfcut -vertex %i -distance %i %s %s', ...
                    params.startVertex, params.patchRadius+distanceInc, ... 
-                   fullfile(params.surfDir, params.innerFileName), ...
-                   fullfile(params.surfDir, params.patchFileName)));
+                   fullfile(params.flatDir, params.innerFileName), ...
+                   fullfile(params.flatDir, params.patchFileName)));
     disppercent(inf);
     
     % flatten the patch
     disppercent(-inf, sprintf('(makeFlat) Flattening surface'));
     [degenFlag result] = system(sprintf('FlattenSurface.tcl %s %s %s', ...
-                                     fullfile(params.surfDir, params.innerFileName), ...
-                                     fullfile(params.surfDir, params.patchFileName), ...
-                                     fullfile(params.surfDir, params.flatFileName)));
+                                     fullfile(params.flatDir, params.innerFileName), ...
+                                     fullfile(params.flatDir, params.patchFileName), ...
+                                     fullfile(params.flatDir, params.flatFileName)));
     disppercent(inf);
 
     % if FlattenSurface failed, most likely b/c surfcut made a bad patch
@@ -198,101 +221,9 @@ else
   end
 
   % remove the 3d patch b/c it isn't need for anything
-  system(sprintf('rm -rf %s', fullfile(params.surfDir, params.patchFileName)));
+  system(sprintf('rm -rf %s', fullfile(params.flatDir, params.patchFileName)));
   
 end
-
-
-% loadSurfOFF.m
-%
-%      usage: surf = loadSurfOFF(surffile)
-%         by: eli merriam
-%       date: 09/25/07
-%    purpose: Loading an OFF binary surface into Matlab
-%
-% This code was ripped directly from Jonas Larsson's tfiReadOFF code
-% the main difference is that this code returns a neat structure,
-% rather than a bunch of variables
-%
-% INPUT ARGUMENTS:
-% surffile - file name of surface in OFF binary format.
-%
-% OUTPUT ARGUMENTS:
-% vtcs: Nvtcs x 3 matrix of vertex coordinates
-% tris: Ntris x 3 matrix of triangle vertex indexes
-%       NB!!! Vertex indexes are 1-offset for Matlab compatibility.
-%       In the OFF surface vertex indexes are 0-offset.
-%
-% The surf structure consists of the following variables
-% vtcs,tris,Nvtcs,Ntris,Nedges (nParent,nPatch,patch2parent)
-%
-function surf = loadSurfOFF(surffile)
-
-% check arguments
-if ~any(nargin == [1])
-  help loadSurfOFF
-  return
-end
-
-fid = fopen(surffile, 'r', 'ieee-be');
-fl = fgetl(fid);
-if (~strcmp(fl,'OFF BINARY'))
-  if (strcmp(fl,'#PATCH'))
-
-    % get the surface parent name
-    fl = fgetl(fid);
-    surf.parentSurfaceName = sscanf(fl,'#parent_surface=%s\n');
-    
-    % parent surface dimensions
-    fl = fgetl(fid); 
-    surf.nParent = sscanf(fl,'#parent_dimensions=%i %i %i\n');
-
-    % patch surface dimensions
-    fl = fgetl(fid); 
-    surf.nPatch = sscanf(fl,'#patch_dimensions=%i %i %i\n');
-
-    % parent vertex indexes tag
-    fl = fgetl(fid); 
-    
-    % read patch info
-    [a,c] = fscanf(fid,'#%i %i\n',[2 surf.nPatch(1)]);
-    if (c ~= surf.nPatch(1)*2)
-      disp(c)
-      error('error reading file')
-    end
-    
-    % matlab 1-offset
-    surf.patch2parent = a'+1; 
-    while (~strcmp(fl,'OFF BINARY'))
-      fl= fgetl(fid);
-    end
-  else
-    disp('WARNING!! Magic number (OFF BINARY) not found - this file may not be in the correct format!');
-  end
-end
-
-[Ninfo, count] = fread(fid, 3, 'int32');
-if (count~=3) error('error reading file!'); end
-surf.Nvtcs  = Ninfo(1);
-surf.Ntris  = Ninfo(2);
-surf.Nedges = Ninfo(3);
-
-[surf.vtcs, count] = fread(fid, surf.Nvtcs*3, 'float32');
-if (count ~= 3*surf.Nvtcs) error('error reading file!'); end
-
-[surf.tris, count] =  fread(fid, surf.Ntris*5, 'int32');
-if (count ~= 5*surf.Ntris) error('error reading file!'); end
-
-surf.vtcs = reshape(surf.vtcs, 3, surf.Nvtcs);
-surf.tris = reshape(surf.tris, 5, surf.Ntris);
-
-surf.vtcs = surf.vtcs';
-
-%% ATTN ATTN ATTN %%
-%% for some reason, I needed to add 2 to the vtcs to make them line up with the anatomy files.
-% surf.vtcs = [surf.vtcs(:,2) surf.vtcs(:,1) surf.vtcs(:,3)]; % swaping x and y
-surf.vtcs = surf.vtcs + 2;              % adding '2' here, but not sure why -epm
-
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -303,17 +234,17 @@ function [surf, params] = loadSurfHandler(params)
 % now load the rest of the surfaces
 
 % load the white matter surface
-surf.inner = loadSurfOFF(fullfile(params.surfDir, params.innerFileName));
+surf.inner = loadSurfOFF(fullfile(params.flatDir, params.innerFileName));
 
 % load the gray matter surface
-surf.outer = loadSurfOFF(fullfile(params.surfDir, params.outerFileName));
+surf.outer = loadSurfOFF(fullfile(params.flatDir, params.outerFileName));
 
 % % read in the curvature file
-% [surf.curv, hdr] = tfiReadVFF(fullfile(params.surfDir, params.curvFileName));
+% [surf.curv, hdr] = tfiReadVFF(fullfile(params.flatDir, params.curvFileName));
 % surf.curv = surf.curv';           % needs to be transposed;
 
 % read in the anatomy file
-[surf.anat.data  surf.anat.hdr] = cbiReadNifti(fullfile(params.surfDir, params.anatFileName));
+[surf.anat.data  surf.anat.hdr] = cbiReadNifti(fullfile(params.flatDir, params.anatFileName));
 
 % Extract permutation matrix to keep track of slice orientation.
 % This logic which is admittedly arcane is duplicated in mrAlignGUI. If you
