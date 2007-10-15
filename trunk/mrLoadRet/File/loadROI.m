67d609da47ba84babd0352266ba0cc8d5ef062d6
justin <justin@d7859a6d-3d6a-0410-b503-b92f2c4db295>
2007-10-15 16:00:44 +0000

    added path so that you can load from volume directory

--- trunk/mrLoadRet/File/loadROI.m
+++ trunk/mrLoadRet/File/loadROI.m
@@ -1,6 +1,6 @@
-function view = loadROI(view,filename,select)
+function view = loadROI(view,filename,select,startPathStr)
 %
-% view = loadROI(view,[filename],[select])
+% view = loadROI(view,[filename],[select],[startPath])
 %
 % Loads an ROI and adds it to view.ROIs.
 %
@@ -33,8 +33,10 @@ if ieNotDefined('select')
     select = 1;
 end
 
-% Path to overlays
-startPathStr = viewGet(view,'roiDir');
+% Path to roi
+if ieNotDefined('startPathStr')
+  startPathStr = viewGet(view,'roiDir');
+end
 
 % Complete pathStr
 if ieNotDefined('filename')
