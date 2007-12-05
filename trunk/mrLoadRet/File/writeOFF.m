b156c7e49f370f239c41a3bb08beb3266a8f1432
eli <eli@d7859a6d-3d6a-0410-b503-b92f2c4db295>
2007-12-05 21:54:37 +0000

    *** empty log message ***

--- trunk/mrLoadRet/File/writeOFF.m
+++ trunk/mrLoadRet/File/writeOFF.m
@@ -1,12 +1,18 @@
 % writeOFF.m
 %
-%      usage: writeOFF()
+%       $Id$	
+%      usage: writeOFF(surf, outName)
 %         by: eli merriam
 %       date: 10/25/07
 %    purpose: 
 %
 function retval = writeOFF(surf, outName)
+
 % write the OFF format file
+
+% undo what loadSurfOFF did in loading surface into matlab
+surf.tris = cat(1, repmat(3,1,length(surf.tris)), surf.tris, repmat(0,1,length(surf.tris)));
+surf.vtcs = surf.vtcs - 2;
 
 fid = fopen(outName, 'w', 'ieee-be');
 
