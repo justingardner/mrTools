77ebad109ffcf5ab14577f73e9b5637808d04056
justin <justin@d7859a6d-3d6a-0410-b503-b92f2c4db295>
2007-12-19 12:16:25 +0000

    rationalizing file names and surface info

--- mrLoadRet/File/baseInfo.m
+++ mrLoadRet/File/baseInfo.m
@@ -23,16 +23,18 @@ baseVolPermutation = viewGet(view,'baseVolPermutation');
 baseVoxelSize = viewGet(view,'baseVoxelSize');
 baseName = viewGet(view,'baseName');
 baseCoordMap = viewGet(view,'baseCoordMap');
+baseType = viewGet(view,'baseType');
 
 % set parameters
 paramsInfo = {{'baseName',baseName,'editable=0','The name of the base anatomy'},...
     {'voxelSize',baseVoxelSize,'editable=0','Voxel dimensions in mm'},...
     {'baseDims',baseDims,'editable=0','Dimensions of base anatomy'},...
     {'qform',baseQform,'editable=0','Qform matrix specifies the transformation to the scanner coordinate frame'},...
-    {'sform',baseSform,'editable=0','Sform matrix is set by mrAlign and usually specifies the transformation to base coordinate system'}};
+    {'sform',baseSform,'editable=0','Sform matrix is set by mrAlign and usually specifies the transformation to base coordinate system'},...
+    {'baseType',baseType,'editable=0','Type of base. 0 = inplane. 1 = flat. 2 = surface'}};
 
 % add baseCoordMap info for flat files
-if ~isempty(baseCoordMap)
+if baseType == 1
   paramsInfo{end+1} = {'flatDir',baseCoordMap.flatDir,'editable=0','Directory from which this flat map was originally created'};
   paramsInfo{end+1} = {'flatFileName',baseCoordMap.flatFileName,'editable=0','Name of original off file from which this flat map was created'};
   paramsInfo{end+1} = {'innerFileName',baseCoordMap.innerFileName,'editable=0','Name of inner mesh (aka gray matter mesh) from which this flat map was created'};
@@ -42,6 +44,15 @@ if ~isempty(baseCoordMap)
   paramsInfo{end+1} = {'viewFlatOnSurface',[],'type=pushbutton','buttonString=View flat on surface','callback',@viewFlatOnSurface,'passParams=1','Click to view flat on the surface meshes'};
 end
 
+% add info for surfaces
+if baseType == 2
+  paramsInfo{end+1} = {'inner',baseCoordMap.innerFileName,'editable=0','Inner surface'};
+  paramsInfo{end+1} = {'innerCoords',baseCoordMap.innerCoordsFileName,'editable=0','Inner surface coordinates'};
+  paramsInfo{end+1} = {'outer',baseCoordMap.outerFileName,'editable=0','Outer surface'};
+  paramsInfo{end+1} = {'outerCoords',baseCoordMap.outerCoordsFileName,'editable=0','Outer surface coordinates'};
+  paramsInfo{end+1} = {'curv',baseCoordMap.curvFileName,'editable=0','Name of curvature file from which this surface was created'};
+  paramsInfo{end+1} = {'anatomy',baseCoordMap.anatFileName,'editable=0','Name of anatomy file from which the xform for this surface was taken'};
+end
 
 % bring up dialog
 mrParamsDialog(paramsInfo,'Base anatomy information');
