10a41851b44b8d37cd0d364d6d1671523c648074
justin <justin@d7859a6d-3d6a-0410-b503-b92f2c4db295>
2007-12-19 16:48:54 +0000

    base gamma added

--- trunk/mrLoadRet/View/isbase.m
+++ trunk/mrLoadRet/View/isbase.m
@@ -26,11 +26,12 @@ if (nargout == 2)
 		    'rotate',0;
 		    'curSlice',[];
 		    'sliceOrientation',[],;
-		    'type',[]};
+		    'type',[],;
+		    'gamma',1};
 else
   % Return 0 if the overlay structure is missing any fields required or
   % optional (since w/out changing the base structure it is invalid).
-  requiredFields = {'clip','coordMap','curSlice','data','hdr','name','permutationMatrix','range','rotate','sliceOrientation','type'};
+  requiredFields = {'clip','coordMap','curSlice','data','hdr','name','permutationMatrix','range','rotate','sliceOrientation','type','gamma'};
   optionalFields = {};
 end
 
