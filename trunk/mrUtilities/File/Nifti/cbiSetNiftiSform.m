ae9840cdd903370a3ae163eaa72ce6318525697f
shani <shani@d7859a6d-3d6a-0410-b503-b92f2c4db295>
2008-02-18 18:17:12 +0000

    Made it check for sform_code = 0 or sform_code = []

--- trunk/mrUtilities/File/Nifti/cbiSetNiftiSform.m
+++ trunk/mrUtilities/File/Nifti/cbiSetNiftiSform.m
@@ -33,7 +33,7 @@ hdr.srow_z=hdr.sform44(3,:);
 
 % only reset sform_code if had been 0 (eg unset)
 % otherwise don't change it (because may be =3, not =1)
-if ~hdr.sform_code
+if isequal(hdr.sform_code,0) || isempty(hdr.sform_code)
   hdr.sform_code=1;
 end
 
