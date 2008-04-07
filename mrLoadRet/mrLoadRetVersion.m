5d1aa3b367d2ffab106172b14aa3462300c56e0d
eli <eli@d7859a6d-3d6a-0410-b503-b92f2c4db295>
2008-04-07 19:28:39 +0000

    seems to work fine in 7.6 after a week of testing...

--- mrLoadRet/mrLoadRetVersion.m
+++ mrLoadRet/mrLoadRetVersion.m
@@ -2,4 +2,4 @@ function [ver, expectedMatlabVersion] = mrLoadRetVersion
 %     $Id$	
 
 ver = 4.5;
-expectedMatlabVersion = [7.2 7.3 7.4 7.5];  % Change this after testing Matlab upgrades
+expectedMatlabVersion = [7.2 7.3 7.4 7.5 7.6];  % Change this after testing Matlab upgrades
