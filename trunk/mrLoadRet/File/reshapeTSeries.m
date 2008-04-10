603408cc94d94cfaa3eb91e7ca025fccaa34b19a
eli <eli@d7859a6d-3d6a-0410-b503-b92f2c4db295>
2008-04-10 17:03:09 +0000

    *** empty log message ***

--- trunk/mrLoadRet/File/reshapeTSeries.m
+++ trunk/mrLoadRet/File/reshapeTSeries.m
@@ -3,16 +3,17 @@
 %      usage: tseries = reshapeTSeries(tseries)
 %         by: eli merriam
 %       date: 03/22/07
-%    purpose: 
+%    purpose: input is an (X,Y,T) slice, output is a reshaped (X*Y,T) matrix
 %	$Id$	
 %
+
 function tseries = reshapeTSeries(tseries)
 
-% % check arguments
-% if ~any(nargin == [0])
-%   help reshapeTSeries
-%   return
-% end
+% check arguments
+if ~any(nargin == [1])
+  help reshapeTSeries
+  return
+end
 
 % Reformat to nFrames x nVoxels
 tseries = squeeze(tseries);
