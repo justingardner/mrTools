533731a166764b08352657a97258c285973dc1b5
justin <justin@d7859a6d-3d6a-0410-b503-b92f2c4db295>
2008-05-08 20:29:23 +0000

    added flag for non-warning just print

--- mrUtilities/MatlabUtilities/oneTimeWarning.m
+++ mrUtilities/MatlabUtilities/oneTimeWarning.m
@@ -1,7 +1,7 @@
 % oneTimeWarning.m
 %
 %        $Id$
-%      usage: oneTimeWarning(warningName,warningText)
+%      usage: oneTimeWarning(warningName,warningText,<justPrint>)
 %         by: justin gardner
 %       date: 02/26/08
 %    purpose: Brought over from Shani's viewGet function. This
@@ -13,11 +13,16 @@
 %             The first time this happens it prints out the
 %             warning, after that it won't do anything.
 %
+%             If the warning is actually just a message, and
+%             not a warning (i.e. you just want it printed
+%             as is to the matlab buffer), set the justPrint
+%             argument to 1
 %
-function oneTimeWarning(fieldCheck,warnText)
+
+function oneTimeWarning(fieldCheck,warnText,justPrint)
 
 % check arguments
-if ~any(nargin == [2])
+if ~any(nargin == [2 3])
   help oneTimeWarning
   return
 end
@@ -27,7 +32,11 @@ verbose = mrGetPref('verbose');
 fieldCheck = fixBadChars(fieldCheck);
 if ~isfield(gMLRWarning,fieldCheck)
   gMLRWarning.(fieldCheck) = 1;
-  mrWarnDlg(warnText)
+  if ieNotDefined('justPrint')
+    mrWarnDlg(warnText)
+  else
+    disp(warnText);
+  end
 end
 
 
