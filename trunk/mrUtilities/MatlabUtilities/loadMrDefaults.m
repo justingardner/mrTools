3cbfddd1512ac1f993bfd277f59a884385b2e10c
justin <justin@d7859a6d-3d6a-0410-b503-b92f2c4db295>
2008-05-28 20:47:10 +0000

    changes to handle default preferences

--- trunk/mrUtilities/MatlabUtilities/loadMrDefaults.m
+++ trunk/mrUtilities/MatlabUtilities/loadMrDefaults.m
@@ -27,43 +27,21 @@ else
     mrDefaults.figloc = [];
 end
 
-% If any of the defaults are unset, them set them
-if ~isfield(mrDefaults.prefs,'interpMethod') | isempty(mrDefaults.prefs.interpMethod)
-    mrDefaults.prefs.interpMethod = 'nearest';
+% set the default values if there are any known fields that have not been set
+[prefNames prefDefaults] = mrGetPref;
+for i = 1:length(prefNames)
+  if ~isfield(mrDefaults.prefs,prefNames{i}) | isempty(mrDefaults.prefs.(prefNames{i}))
+    % if we have a cell array, that means a list of different possibilities, default
+    % to the top of the list
+    if iscell(prefDefaults{i})
+      mrDefaults.prefs.(prefNames{i}) = prefDefaults{i}{1};
+    % otherwise as long as the default is not empty, use it.
+    elseif ~isempty(prefDefaults{i})
+      mrDefaults.prefs.(prefNames{i}) = prefDefaults{i};
+    end
+  end
 end
-if ~isfield(mrDefaults.prefs,'overwritePolicy') | isempty(mrDefaults.prefs.overwritePolicy)
-    mrDefaults.prefs.overwritePolicy = 'ask';
-end
-if ~isfield(mrDefaults.prefs,'site') | isempty(mrDefaults.prefs.site)
-    mrDefaults.prefs.site = 'NYU';
-end
-if ~isfield(mrDefaults.prefs,'verbose') | isempty(mrDefaults.prefs.verbose)
-    mrDefaults.prefs.verbose = 'Yes';
-end
-if ~isfield(mrDefaults.prefs,'maxBlocksize') | isempty(mrDefaults.prefs.maxBlocksize)
-    mrDefaults.prefs.maxBlocksize = 250000000;
-end
-if ~isfield(mrDefaults.prefs,'volumeDirectory') | isempty(mrDefaults.prefs.volumeDirectory)
-    mrDefaults.prefs.volumeDirectory = pwd;
-end
-if ~isfield(mrDefaults.prefs,'niftiFileExtension') | isempty(mrDefaults.prefs.niftiFileExtension)
-    mrDefaults.prefs.niftiFileExtension = '.img';
-end
-if ~isfield(mrDefaults.prefs,'roiCacheSize') | isempty(mrDefaults.prefs.roiCacheSize)
-  mrDefaults.prefs.roiCacheSize = 50;
-% this is added since when we went to single roi caches
-% we need to have a larger roi cache size--this will convert
-% everyone who had the default 3 to 50
-elseif (mrDefaults.prefs.roiCacheSize == 3)
-  mrDefaults.prefs.roiCacheSize = 50;
-end  
-if ~isfield(mrDefaults.prefs,'overlayCacheSize') | isempty(mrDefaults.prefs.overlayCacheSize)
-  mrDefaults.prefs.overlayCacheSize = 20;
-end
-if ~isfield(mrDefaults.prefs,'baseCacheSize') | isempty(mrDefaults.prefs.baseCacheSize)
-  mrDefaults.prefs.baseCacheSize = 20;
-end
-
+  
 % check for any figloc that are strange
 if ~isempty(mrDefaults.figloc)
   figlocNames = fieldnames(mrDefaults.figloc);
--- trunk/mrUtilities/MatlabUtilities/mrGetPref.m
+++ trunk/mrUtilities/MatlabUtilities/mrGetPref.m
@@ -26,16 +26,24 @@ function [value prefDefaults] = mrGetPref(pref)
 
 % with no arguments, return a list of possible preferences
 prefNames = {'interpMethod','overwritePolicy','verbose','niftiFileExtension','roiPolygonMethod','systemInterrogatros','selectedROIColor','site','maxBlocksize','volumeDirectory','roiCacheSize','baseCacheSize','overlayCacheSize','defaultInterrogators','importROIPath'};
+
+% set the defaults for preference we have defaults for
+prefDefaults{length(prefNames)} = [];
+prefDefaults{find(strcmp('interpMethod',prefNames))} = {'nearest','linear','spline','cubic'};
+prefDefaults{find(strcmp('overwritePolicy',prefNames))} = {'Ask','Merge','Rename','Overwrite'};
+prefDefaults{find(strcmp('verbose',prefNames))} = {'Yes','No'};
+prefDefaults{find(strcmp('niftiFileExtension',prefNames))} = {'.img','.nii'};
+prefDefaults{find(strcmp('roiPolygonMethod',prefNames))} = {'roipoly','getpts','getptsNoDoubleClick'};
+prefDefaults{find(strcmp('selectedROIColor',prefNames))} = color2RGB;
+prefDefaults{find(strcmp('selectedROIColor',prefNames))}{end+1} = 'none';
+prefDefaults{find(strcmp('site',prefNames))} = 'NYU';
+prefDefaults{find(strcmp('maxBlocksize',prefNames))} = 250000000;
+prefDefaults{find(strcmp('volumeDirectory',prefNames))} = '';
+prefDefaults{find(strcmp('roiCacheSize',prefNames))} = 100;
+prefDefaults{find(strcmp('baseCacheSize',prefNames))} = 50;
+prefDefaults{find(strcmp('overlayCacheSize',prefNames))} = 50;
+
 if nargin == 0
-  % set the defaults for preference we have defaults for
-  prefDefaults{length(prefNames)} = [];
-  prefDefaults{find(strcmp('interpMethod',prefNames))} = {'nearest','linear','spline','cubic'};
-  prefDefaults{find(strcmp('overwritePolicy',prefNames))} = {'Ask','Merge','Rename','Overwrite'};
-  prefDefaults{find(strcmp('verbose',prefNames))} = {'Yes','No'};
-  prefDefaults{find(strcmp('niftiFileExtension',prefNames))} = {'.img','.nii'};
-  prefDefaults{find(strcmp('roiPolygonMethod',prefNames))} = {'roipoly','getpts','getptsNoDoubleClick'};
-  prefDefaults{find(strcmp('selectedROIColor',prefNames))} = color2RGB;
-  prefDefaults{find(strcmp('selectedROIColor',prefNames))}{end+1} = 'none';
   % return arguments
   value = prefNames;
   return
@@ -57,7 +65,17 @@ end
 if isfield(mrDEFAULTS.prefs,pref)
     value = getfield(mrDEFAULTS.prefs,pref);
 else
+  % not set yet, take the top most possibility in the default
+  % list, otherwise return empty
+  if ~isempty(prefNum) && ~isempty(prefDefaults{prefNum})
+    if iscell(prefDefaults{prefNum})
+      value = prefDefaults{prefNum}{1};
+    else
+      value = prefDefaults{prefNum};
+    end
+  else
     value = [];
+  end
 end
 
 % default value for selectedROIColor
@@ -67,7 +85,9 @@ end
 
 % deal with interrogators
 if strcmp(pref,'systemInterrogators')
-  value = {'timecoursePlot','makeFlat','searchForVoxel'};
+  if isempty(value)
+    value = {'timecoursePlot','makeFlat','searchForVoxel'};
+  end
 end
 if strcmp(pref,'defaultInterrogators')
   if isempty(value)
