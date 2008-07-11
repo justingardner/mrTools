16c17e02a1bd786c5e1306d752b6feae5c787ea1
justin <justin@d7859a6d-3d6a-0410-b503-b92f2c4db295>
2008-07-11 14:32:30 +0000

    askuser now checks verbose setting and brings up a questdlg if verbose is set to yes

--- trunk/mrUtilities/MatlabUtilities/askuser.m
+++ trunk/mrUtilities/MatlabUtilities/askuser.m
@@ -1,9 +1,11 @@
 % askuser.m
 %
-%      usage: askuser(question)
+%      usage: askuser(question,<toall>)
 %         by: justin gardner
 %       date: 02/08/06
-%    purpose: ask the user something
+%    purpose: ask the user a yes/no question. Question is a string with the question. This
+%             function will return 1 for yes and 0 for no. If toall is set to 1, then
+%             'Yes to all' will be an option, which if selected will return inf
 %
 function r = askuser(question,toall)
 
@@ -12,14 +14,32 @@ if ~any(nargin == [1 2])
   help askuser
   return
 end
+
+verbose = mrGetPref('verbose');
+if strcmp(verbose,'Yes'),verbose = 1;else,verbose = 0;end
+
 if ieNotDefined('toall'),toall = 0;,end
 r = [];
 while isempty(r)
   % ask the question
   if toall
-    r = input(sprintf('%s (y/n or a for Yes to all)? ',question),'s');
+    % ask the question (with option for all)
+    if ~verbose
+      % not verbose, use text question
+      r = input(sprintf('%s (y/n or a for Yes to all)? ',question),'s');
+    else
+      % verbose, use dialog
+      r = questdlg(sprintf('%s?',question),'','Yes','No','All','Yes');
+      r = lower(r(1));
+    end
+  % ask question (without option for all)
   else
-    r = input(sprintf('%s (y/n)? ',question),'s');
+    if ~verbose
+      r = input(sprintf('%s (y/n)? ',question),'s');
+    else
+      r = questdlg(sprintf('%s?',question),'','Yes','No','Yes');
+      r = lower(r(1));
+    end
   end
   % make sure we got a valid answer
   if (lower(r) == 'n')
