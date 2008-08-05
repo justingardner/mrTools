498414a2daebcd29be1d22c920759982458df9f4
justin <justin@d7859a6d-3d6a-0410-b503-b92f2c4db295>
2008-08-05 22:23:56 +0000

    ability to add a number so that you can see more than the last directory

--- trunk/mrUtilities/MatlabUtilities/getLastDir.m
+++ trunk/mrUtilities/MatlabUtilities/getLastDir.m
@@ -1,26 +1,31 @@
 % getLastDir(pathStr)
 %
-%      usage: getLastDir(pathStr)
+%      usage: getLastDir(pathStr,<levels>)
 %         by: justin gardner
 %       date: 04/05/07
-%    purpose: 
+%    purpose: Gets the last dir from a path. If levels is set then
+%             it returns more directory levels (default for levels is 1).
 %
-function lastDir = getLastDir(pathStr)
+function lastDir = getLastDir(pathStr,levels)
 
 % check arguments
-if ~any(nargin == [1])
+if ~any(nargin == [1 2])
   help getLastDir
   return
 end
 
-% remove trailling fileseparator if it is there
+if ieNotDefined('levels'),levels = 1;end
+if levels == 0,lastDir = '';return;end
+
+% remove trailing fileseparator if it is there
 if length(pathStr) && (pathStr(end) == filesep)
   pathStr = pathStr(1:end-1);
 end
 
-% and return last dir
+% get last dir
 [pathStr lastDir ext] = fileparts(pathStr);
 
 % paste back on extension
 lastDir = [lastDir ext];
 
+lastDir = fullfile(getLastDir(pathStr,levels-1),lastDir);
