88dbb214a81755e159104d82b52cd2a7664d1e9e
justin <justin@d7859a6d-3d6a-0410-b503-b92f2c4db295>
2008-08-05 22:23:25 +0000

    some filename checking

--- trunk/mrUtilities/File/FreeSurfer/mlrImportFreeSurfer.m
+++ trunk/mrUtilities/File/FreeSurfer/mlrImportFreeSurfer.m
@@ -1,28 +1,28 @@
-% importFStoSR.m
+% mlrImportFreeSurfer.m
 %
 %        $Id$
-%      usage: importFStoSR(varargin)
+%      usage: mlrImportFreeSurfer(varargin)
 %         by: eli merriam
 %       date: 07/11/07
 %    purpose: 
 %
-function retval = importFStoSR(varargin)
+function retval = mlrImportFreeSurfer(varargin)
 
 % check arguments
 % if ~any(nargin == [0])
-%   help importFStoSR
+%   help mlrImportFreeSurfer
 %   return
 % end
 
 if ~exist('mrParamsDialog')
-    disp(sprintf('(mglDoRetinotopy) You must have mrTools in your path to run this'));
-    return
+  disp(sprintf('(mlrImportFreeSurfer) You must have mrTools in your path to run this'));
+  return
 end
 
 % evaluate the arguments
 eval(evalargs(varargin));
 
-if ieNotDefined('baseName'), baseName = [];end
+if ieNotDefined('baseName'), baseName = getLastDir(pwd);end
 
 wmFile    = 'smoothwm';
 gmFile    = 'pial';
@@ -31,21 +31,21 @@ curvFile  = 'curv';
 anatFile  = 'T1.mgz';
 hemi      = {'lh', 'rh'};
 hemiNames = {'left', 'right'};
-
+outDir    = fullfile(pwd,'surfRelax');
 
 paramsInfo = {...
-    {'fsDir',    fullfile(getenv('SUBJECTS_DIR'), baseName),    'directory where the freeSurfer files live'}, ...
-    {'outDir',   pwd,      'directory that OFF surfaces will be writeen to'}, ...
-    {'wmFile',   wmFile,   'name of the surface defining the white/gray boundry'}, ...
-    {'gmFile',   gmFile,   'name of the surface defining the gray/pial boundry'}, ...
+    {'freeSurferDir',fullfile(getenv('SUBJECTS_DIR'), baseName),'directory where the freeSurfer files live'}, ...
+    {'outDir', outDir,'directory that OFF surfaces will be written to'}, ...
+    {'wmFile', wmFile, 'name of the surface defining the white/gray boundry'}, ...
+    {'gmFile', gmFile, 'name of the surface defining the gray/pial boundry'}, ...
     {'curvFile', curvFile, 'name of the file specifing the curvature'}, ...
-    {'infFile',  infFile,  'name of the inflated surface'}, ...
+    {'infFile', infFile, 'name of the inflated surface'}, ...
     {'anatFile', anatFile, 'name of the base anatomy file'}, ...
-    {'baseName', baseName,       'subject initials'}, ...
+    {'baseName', baseName, 'subject initials'}, ...
              };
 
 % get the parameters from the user
-params = mrParamsDialog(paramsInfo);
+params = mrParamsDialog(paramsInfo,'mlrImportFreeSurfer',1.5);
 
 % return if use rr hit cancel
 if isempty(params)
@@ -53,39 +53,65 @@ if isempty(params)
 end
 
 % import the white and gray matter surface, as well as the inflated surface and curvature
-disp(sprintf('(importFStoSR) Converting FreeSurfer surfaces to OFF format'))
+disp(sprintf('(mlrImportFreeSurfer) Converting FreeSurfer surfaces to OFF format'))
 for i = 1:length(hemi)
-    fs2off(fullfile(params.fsDir, 'surf', strcat(hemi{i}, '.', params.wmFile)), ...
-           fullfile(params.outDir, strcat(params.baseName, '_', hemiNames{i}, '_WM.off')));
+  % convert inner surface
+  surfFile = fullfile(params.freeSurferDir, 'surf', strcat(hemi{i}, '.', params.wmFile));
+  outFile = fullfile(params.outDir, strcat(params.baseName, '_', hemiNames{i}, '_WM.off'));
+  if isfile(surfFile)
+    freeSurfer2off(surfFile, outFile);
+  else
+    disp(sprintf('(mlrImportFreeSurfer) Could not find inner (white matter) surface %s',getLastDir(surfFile,2)));
+  end
+  
+  % convert outer surface
+  surfFile = fullfile(params.freeSurferDir, 'surf', strcat(hemi{i}, '.', params.gmFile));
+  outFile = fullfile(params.outDir, strcat(params.baseName, '_', hemiNames{i}, '_GM.off'));
+  if isfile(surfFile)
+    freeSurfer2off(surfFile,outFile);
+  else
+    disp(sprintf('(mlrImportFreeSurfer) Could not find outer (pial/gray matter) surface %s',getLastDir(surfFile,2)));
+  end
 
-    fs2off(fullfile(params.fsDir, 'surf', strcat(hemi{i}, '.', params.gmFile)), ...
-           fullfile(params.outDir, strcat(params.baseName, '_', hemiNames{i}, '_GM.off')));
+  % convert inflated surface
+  surfFile = fullfile(params.freeSurferDir, 'surf', strcat(hemi{i}, '.', params.infFile));
+  outFile = fullfile(params.outDir, strcat(params.baseName, '_', hemiNames{i}, '_Inf.off'));
+  if isfile(surfFile)
+    freeSurfer2off(surfFile,outFile);
+  else
+    disp(sprintf('(mlrImportFreeSurfer) Could not find inflated surface %s',getLastDir(surfFile,2)));
+  end
 
-    fs2off(fullfile(params.fsDir, 'surf', strcat(hemi{i}, '.', params.infFile)), ...
-           fullfile(params.outDir, strcat(params.baseName, '_', hemiNames{i}, '_Inf.off')));
-    
-    [curv, fnum] = freesurfer_read_curv(fullfile(params.fsDir, 'surf', strcat(hemi{i}, '.', params.curvFile)));
-    eval(sprintf('save %s %s -V6;', ...
-                 fullfile(params.outDir, strcat(params.baseName, '_', hemiNames{i}, '_Curv.mat')), 'curv'));
+%  curvFile = fullfile(params.freeSurferDir, 'surf', strcat(hemi{i}, '.', params.curvFile));
+%  if isfile(curvFile)
+%    [curv, fnum] = freesurfer_read_curv();
+%  eval(sprintf('save %s %s -V6;', fullfile(params.outDir, strcat(params.baseName, '_', hemiNames{i}, '_Curv.mat')), 'curv'));
+%  else
+%    disp(sprintf('(mlrImportFreeSurfer) Could not find curvature file %s',getLastDir(curvFile)));
+%  end
 end
 
+% convert the volume to mlr volume
+anatFile = fullfile(params.freeSurferDir, 'mri', params.anatFile);
+outFile = fullfile(params.outDir, strcat(params.baseName, '_', 'mprage_pp.hdr'));
+if isfile(anatFile)
+  disp(sprintf('(mlrImportFreeSurfer) Converting the volume anatomy to nifti format'))
+  setenv('LD_LIBRARY_PATH', '/usr/pubsw/packages/tiffjpegglut/current/lib:/opt/local/lib:/usr/local/lib:/opt/local/lib')
+  system(sprintf('mri_convert --out_type nifti1 --out_orientation RAS --cropsize 176 256 256 %s %s',anatFile,outFile));
 
-disp(sprintf('(importFStoSR) Converting the volume anatomy to nifti format'))
-setenv('LD_LIBRARY_PATH', '/usr/pubsw/packages/tiffjpegglut/current/lib:/opt/local/lib:/usr/local/lib:/opt/local/lib')
-system(sprintf('mri_convert --out_type nifti1 --out_orientation RAS --cropsize 176 256 256 %s %s', ...
-               fullfile(params.fsDir, 'mri', params.anatFile), ...
-               fullfile(params.outDir, strcat(params.baseName, '_', 'mprage_pp.hdr'))))
-
-h = cbiReadNiftiHeader(fullfile(params.outDir, strcat(params.baseName, '_', 'mprage_pp.hdr')));
-h.qform44(1,4) = -(h.dim(2)-1)/2;
-h = cbiSetNiftiQform(h, h.qform44);
-h = cbiSetNiftiSform(h, h.qform44);
-cbiWriteNiftiHeader(h, fullfile(params.outDir, strcat(params.baseName, '_', 'mprage_pp.hdr')));
+  h = cbiReadNiftiHeader(fullfile(params.outDir, strcat(params.baseName, '_', 'mprage_pp.hdr')));
+  h.qform44(1,4) = -(h.dim(2)-1)/2;
+  h = cbiSetNiftiQform(h, h.qform44);
+  h = cbiSetNiftiSform(h, h.qform44);
+  cbiWriteNiftiHeader(h, fullfile(params.outDir, strcat(params.baseName, '_', 'mprage_pp.hdr')));
+else
+  disp(sprintf('(mlrImportFreeSurfer) Could not find volume %s',getLastDir(anatFile,2)));
+end
 
 
 return;
 
-disp(sprintf('(importFStoSR) Constructing the gray graph'))
+disp(sprintf('(mlrImportFreeSurfer) Constructing the gray graph'))
 setenv('DYLD_LIBRARY_PATH', '/Users/eli/src/TFI/sw/lib/')
 for i = 1:length(hemi)
     system(sprintf('surf2graph -verbose -gimage %s -gmin 40 -gmax 120 -expand -1 -gsurface %s -gray %s -savelabels %s %s', ...
@@ -99,7 +125,7 @@ end
 
 
 return;
-% disp(sprintf('(importFStoSR) Fixing nifti header so that the volume is centered'))
+% disp(sprintf('(mlrImportFreeSurfer) Fixing nifti header so that the volume is centered'))
 % h = cbiReadNiftiHeader(sprintf(fullfile(params.outDir, strcat(params.baseName, '_', 'mprage_pp.hdr'))));
 % h.qform44(1,4) = (h.dim(2)-1)/2;
 % h.qoffset_x = -1*(h.dim(2)-1)/2;
