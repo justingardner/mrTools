6a5ce749ac2b148965d18aaea1b54ad2bb04b4ac
justin <justin@d7859a6d-3d6a-0410-b503-b92f2c4db295>
2008-08-10 18:25:25 +0000

    minor, handle .hdr extension as well as .img

--- trunk/mrLoadRet/File/baseInfo.m
+++ trunk/mrLoadRet/File/baseInfo.m
@@ -48,10 +48,10 @@ paramsInfo = {{'baseName',baseName,'editable=0','The name of the base anatomy'},
 
 % add baseCoordMap info for flat files
 if baseType == 1
-  paramsInfo{end+1} = {'flatDir',baseCoordMap.flatDir,'editable=0','Directory from which this flat map was originally created'};
+  paramsInfo{end+1} = {'path',baseCoordMap.path,'editable=0','Directory from which this flat map was originally created'};
   paramsInfo{end+1} = {'flatFileName',baseCoordMap.flatFileName,'editable=0','Name of original off file from which this flat map was created'};
-  paramsInfo{end+1} = {'innerFileName',baseCoordMap.innerFileName,'editable=0','Name of inner mesh (aka gray matter mesh) from which this flat map was created'};
-  paramsInfo{end+1} = {'outerFileName',baseCoordMap.outerFileName,'editable=0','Name of outer mesh (aka white matter mesh) from which this flat map was created'};
+  paramsInfo{end+1} = {'innerCoordsFileName',baseCoordMap.innerCoordsFileName,'editable=0','Name of inner mesh (aka gray matter mesh) from which this flat map was created'};
+  paramsInfo{end+1} = {'outerCoordsFileName',baseCoordMap.outerCoordsFileName,'editable=0','Name of outer mesh (aka white matter mesh) from which this flat map was created'};
   paramsInfo{end+1} = {'curvFileName',baseCoordMap.curvFileName,'editable=0','Name of curvature file from which this flat map was created'};
   paramsInfo{end+1} = {'anatFileName',baseCoordMap.anatFileName,'editable=0','Name of anatomy file from which the xform for this flat map was taken'};
   paramsInfo{end+1} = {'viewFlatOnSurface',[],'type=pushbutton','buttonString=View flat on surface','callback',@viewFlatOnSurface,'passParams=1','Click to view flat on the surface meshes'};
@@ -59,10 +59,10 @@ end
 
 % add info for surfaces
 if baseType == 2
-  paramsInfo{end+1} = {'inner',baseCoordMap.innerFileName,'editable=0','Inner surface'};
-  paramsInfo{end+1} = {'innerCoords',baseCoordMap.innerCoordsFileName,'editable=0','Inner surface coordinates'};
-  paramsInfo{end+1} = {'outer',baseCoordMap.outerFileName,'editable=0','Outer surface'};
-  paramsInfo{end+1} = {'outerCoords',baseCoordMap.outerCoordsFileName,'editable=0','Outer surface coordinates'};
+  paramsInfo{end+1} = {'innerSurfaceFileName',baseCoordMap.innerSurfaceFileName,'editable=0','Inner surface'};
+  paramsInfo{end+1} = {'innerCoordsFileName',baseCoordMap.innerCoordsFileName,'editable=0','Inner surface coordinates'};
+  paramsInfo{end+1} = {'outerSurfaceFileName',baseCoordMap.outerSurfaceFileName,'editable=0','Outer surface'};
+  paramsInfo{end+1} = {'outerCoordsFileName',baseCoordMap.outerCoordsFileName,'editable=0','Outer surface coordinates'};
   paramsInfo{end+1} = {'curv',baseCoordMap.curvFileName,'editable=0','Name of curvature file from which this surface was created'};
   paramsInfo{end+1} = {'anatomy',baseCoordMap.anatFileName,'editable=0','Name of anatomy file from which the xform for this surface was taken'};
 end
@@ -76,14 +76,14 @@ mrParamsDialog(paramsInfo,'Base anatomy information');
 function params = viewFlatOnSurface(params,varargin);
 
 thispwd = pwd;
-if isdir(params.flatDir)
-  cd(params.flatDir);
+if isdir(params.path)
+  cd(params.path);
 else
-  mrWarnDlg(sprintf('Directory %s does not exist, please find the anatomy folder',params.flatDir));
+  mrWarnDlg(sprintf('Directory %s does not exist, please find the anatomy folder',params.path));
   pathStr = uigetdir(mrGetPref('volumeDirectory','Find anatomy directory'));
   if pathStr == 0,return,end
   cd(pathStr);
 end
 
-mrFlatViewer(params.flatFileName,params.outerFileName,params.innerFileName,params.curvFileName,params.anatFileName);
+mrFlatViewer(params.flatFileName,params.outerCoordsFileName,params.innerCoordsFileName,params.curvFileName,params.anatFileName);
 cd(thispwd);
