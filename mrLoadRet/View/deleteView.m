5dd692d155bd797cf57857c4bfb7742c9958d39f
justin <justin@d7859a6d-3d6a-0410-b503-b92f2c4db295>
2009-01-19 19:18:32 +0000

    Prints warning if you try and delete an already deleted view. Also clears the view variable that has been passed in in the calling space when it is deleted.

--- mrLoadRet/View/deleteView.m
+++ mrLoadRet/View/deleteView.m
@@ -7,19 +7,51 @@ function deleteView(view)
 %
 % djh, 6/2004
 
+if nargin ~= 1
+  help deleteView;
+  return
+end
+
+% get mrGlobals
 mrGlobals
 
+% flag to clear input variable
+clearViewInCallerSpace = 0;
+
+% passed in number, then it is a viewNum, not a view
 if isnumeric(view)
-	viewNum = view;
+  if ~isempty(viewGet([],'view',view))
+    viewNum = view;
+  else
+    % not a valid viewNum, print error message and return
+    if isempty(view)
+      mrWarnDlg(sprintf('(%s) Empty view.',mfilename));
+    else
+      mrWarnDlg(sprintf('(%s) View number %i does not exist.',mfilename,view));
+    end
+    return
+  end
+% make sure it is a view
 elseif isview(view)
-	viewNum = view.viewNum;
+  viewNum = view.viewNum;
+  % if the input name is a variable name, flag it to be cleared int
+  % the calling space
+  if ~isempty(inputname(1))
+    clearViewInCallerSpace = 1;
+  end
 else
-	return;
+  return;
 end
 
 % Delete it
 MLR.views{viewNum} = [];
 MLR.caches{viewNum} = [];
+
+% clear it in the calling space
+if clearViewInCallerSpace
+  assignin('caller',inputname(1),[]);
+end
+
 % check to see if there are any valid views, if not, clear MLR
 for i = 1:length(MLR.views)
   if ~isempty(MLR.views{i}),return,end
