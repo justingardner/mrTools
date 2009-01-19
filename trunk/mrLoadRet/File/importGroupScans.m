0869f0b2d59f7d07916fcfb68dd9874b0cbbbed4
justin <justin@d7859a6d-3d6a-0410-b503-b92f2c4db295>
2009-01-19 19:17:11 +0000

    clean up to use viewGet/viewSet

--- trunk/mrLoadRet/File/importGroupScans.m
+++ trunk/mrLoadRet/File/importGroupScans.m
@@ -14,12 +14,14 @@ if ~any(nargin == [0])
 end
 
 % new view
-mrGlobals;
-v = newView;
+toView = newView;
 
 % go find the group that user wants to load here
-pathStr = uigetdir(MLR.homeDir,'Select session you want to import from');
-if (pathStr==0),return,end
+pathStr = uigetdir(viewGet(toView,'homeDir'),'Select session you want to import from');
+if (pathStr==0)
+  deleteView(toView);
+  return
+end
 
 % now look for that sessions mrSession
 mrSessionPath = fullfile(pathStr,'mrSession.mat');
@@ -28,106 +30,121 @@ if ~isfile(mrSessionPath)
   disp(sprintf('                   Make sure you clicked on the directory'));
   disp(sprintf('                   with the mrSession.mat file (not the group'))
   disp(sprintf('                   directory you wanted to import)'))
-  return
-end
-mrSession = load(mrSessionPath);
-if ~isfield(mrSession,'session') || ~isfield(mrSession,'groups')
-  mrWarnDlg(sprintf('(importGroupScans) Unknown format for mrSession in %s',fileparts(pathStr)));
+  deleteView(toView);
   return
 end
 
+% check for MLR 4 session
+mrSession = load(mrSessionPath);
+if ~isfield(mrSession,'session') || ~isfield(mrSession,'groups')
+  mrWarnDlg(sprintf('(importGroupScans) Unknown format for mrSession in %s',fileparts(pathStr)));
+  deleteView(toView);
+  return
+end
+clear mrSession
+
+% get info from to group
+toHomeDir = viewGet(toView,'homeDir');
+toGroupNames = viewGet(toView,'groupNames');
+
+% now we are going to have to clear the MLR
+% variable so that we can get info on the from groups
+% we will then set back to the old MLR. Note that
+% while we have switch the MLR session we cannot
+% get info from the toView
+fromView = switchSession(pathStr);
+fromView = newView;
+
 % get the groups in the import session
-for gNum = 1:length(mrSession.groups)
-  fromGroups{gNum} = sprintf('%s:%s (%i scans)',getLastDir(pathStr),mrSession.groups(gNum).name,length(mrSession.groups(gNum).scanParams));;
+for gNum = 1:viewGet(fromView,'numGroups')
+  fromGroups{gNum} = sprintf('%s:%s (%i scans)',getLastDir(pathStr),viewGet(fromView,'groupName',gNum),viewGet(fromView,'numScans',gNum));
 end
 
 % get from which and to which group we are doing
 paramsInfo = {...
     {'fromGroup',fromGroups,'The group to import from'},...
-    {'toGroup',viewGet(v,'groupNames'),'The group to import into'},...
+    {'toGroup',toGroupNames,'The group to import into'},...
     {'linkFiles',1,'type=checkbox','Link rather than copy the files. This will make a soft link rather than copying the files which saves disk space.'}};
     
 params = mrParamsDialog(paramsInfo);
-if isempty(params),return,end
+if isempty(params)
+  switchSession;
+  deleteView(toView);
+  return
+end
 
 % now set up some variables
 fromGroupNum = find(strcmp(params.fromGroup,fromGroups));
-fromGroup = mrSession.groups(fromGroupNum).name;
-toGroupNum = viewGet(v,'groupNum',params.toGroup);
+fromGroup = viewGet(fromView,'groupName',fromGroupNum);
 toGroup = params.toGroup;
 fromDir = fullfile(fullfile(pathStr,fromGroup),'TSeries');
 if ~isdir(fromDir)
   mrWarnDlg(sprintf('(importGroupScans) Could not find directory %s',fromDir));
+  switchSession;
+  deleteView(toView);
   return
 end
-toDir = fullfile(fullfile(viewGet(v,'homeDir'),toGroup),'TSeries');
+toDir = fullfile(fullfile(toHomeDir,toGroup),'TSeries');
 if ~isdir(toDir)
   mrWarnDlg(sprintf('(importGroupScans) Could not find directory %s',toDir));
+  % switch back to old one
+  switchSession;
+  deleteView(toView);
   return
 end
 fromName = getLastDir(pathStr);
 
 % set the group
-v = viewSet(v,'currentGroup',toGroupNum);
-
-% now we are going to have to clear the MLR
-% variable so that we can get info on the from groups
-% we will then set back to the old MLR
-oldMLR = MLR;
-clear global MLR;
-currentDir = pwd;
-cd(pathStr);
-fromView = newView;
 fromView = viewSet(fromView,'curGroup',fromGroupNum);
 
 % choose the scans to import
 selectedScans = selectScans(fromView,'Choose scans to import');
-if isempty(selectedScans),return,end
+if isempty(selectedScans)
+  switchSession;
+  deleteView(toView);
+  return
+end
+
+% get the scan and aux paramters for the chosen scans
 for i = 1:length(selectedScans)
   fromScanParams(i) = viewGet(fromView,'scanParams',selectedScans(i));
   fromAuxParams(i) = viewGet(fromView,'auxParams',selectedScans(i));
 end
 
+% get the stimfiles for the selected scans
 for scanNum = 1:length(selectedScans)
   stimFileName{scanNum} = viewGet(fromView,'stimFileName',selectedScans(scanNum));
 end
 
-% now switch back to old one
-cd(currentDir);
-clear global MLR;
-global MLR;
-MLR = oldMLR;
+% now switch back to old MLR session
+switchSession;
+
+% set the group
+toView = viewSet(toView,'currentGroup',toGroup);
 
 % now cycle over all scans in group
 disppercent(-inf,'Copying group scans');
 r = 0;
 for scanNum = 1:length(fromScanParams)
-  % make sure there is a file where we think there should be
+  % get the fromFilename
   fromFilename = fullfile(fromDir,fromScanParams(scanNum).fileName);
-  toFilename = fullfile(toDir,sprintf('%s_%s.img',stripext(fromScanParams(scanNum).fileName),fromName));
-  if ~isfile(fromFilename)
-    disp(sprintf('(importGroupScans) Could not find %s',fromFilename));
-    return
-  end
-  % write it to our group, making sure to ask if it already exists
-  success = copyNiftiFile(fromFilename,toFilename,params.linkFiles);
-  if ~success,continue,end
-  % add where this scan is from into description
+  % get the scan params for this scan
   toScanParams = fromScanParams(scanNum);
   toScanParams.description = sprintf('%s:%s',fromName,fromScanParams(scanNum).description);
-  toScanParams.fileName = getLastDir(toFilename);
   % and remove any reference to originalFileName/GroupName
   toScanParams.originalFileName = [];
   toScanParams.originalGroupName = [];
+  % clear filename, so that it gets a new unique filename
+  toScanParams.fileName = [];
   % and now add the scan to our group
-  v = viewSet(v,'newScan',toScanParams);
-  toScanNum = viewGet(v,'nScans');
+  saveNewTSeries(toView,fromFilename,toScanParams,[],params.linkFiles);
+  toScanNum = viewGet(toView,'nScans');
   % copy the stimFile over
   toStimFileNames = {};
   for stimFileNum = 1:length(stimFileName{scanNum})
     % get the from and to stim file names
     fromStimFileName = stimFileName{scanNum}{stimFileNum};
-    toStimFileName = fullfile(viewGet(v,'EtcDir'),getLastDir(stimFileName{scanNum}{stimFileNum}));
+    toStimFileName = fullfile(viewGet(toView,'EtcDir'),getLastDir(stimFileName{scanNum}{stimFileNum}));
     % if it doesn't exist already, then copy it over
     if isfile(toStimFileName)
       disp(sprintf('(importGroupScans) Stimfile %s already exists',toStimFileName));
@@ -138,12 +155,40 @@ for scanNum = 1:length(fromScanParams)
     toStimFileNames{stimFileNum} = getLastDir(toStimFileName);
   end
   % and set the stimfiles in the view
-  v = viewSet(v,'stimFileName',toStimFileNames,toScanNum);
+  toView = viewSet(toView,'stimFileName',toStimFileNames,toScanNum);
   disppercent(scanNum/length(fromScanParams));
 end
 disppercent(inf);
 
+deleteView(toView);
+saveSession;
 
-deleteView(v);
+
+%%%%%%%%%%%%%%%%%%%%%%%
+%%   switchSession   %%
+%%%%%%%%%%%%%%%%%%%%%%%
+function v = switchSession(pathStr)
+
+% switch to the MLR session found at "pathStr"
+if (nargin == 1)
+  % switch the path and globals
+  mrGlobals;
+  global oldMLR;
+  oldMLR = MLR;
+  clear global MLR;
+  global oldPWD
+  oldPWD = pwd;
+  cd(pathStr);
+  v = newView;
+% switch back to old one
+else
+  % switch back to old one
+  global oldPWD
+  cd(oldPWD);
+  clear global MLR;
+  global MLR;
+  global oldMLR;
+  MLR = oldMLR;
+end
 
 
