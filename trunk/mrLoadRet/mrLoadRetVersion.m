83b2631f2487cfe08697f5b07570b688dffa5172
eli <eli@d7859a6d-3d6a-0410-b503-b92f2c4db295>
2009-05-07 19:21:23 +0000

    *** empty log message ***

--- trunk/mrLoadRet/mrLoadRetVersion.m
+++ trunk/mrLoadRet/mrLoadRetVersion.m
@@ -2,4 +2,4 @@ function [ver, expectedMatlabVersion] = mrLoadRetVersion
 %     $Id$	
 
 ver = 4.5;
-expectedMatlabVersion = [7.2 7.3 7.4 7.5 7.6 7.7];  % Change this after testing Matlab upgrades
+expectedMatlabVersion = [7.2 7.3 7.4 7.5 7.6 7.7 7.8];  % Change this after testing Matlab upgrades
