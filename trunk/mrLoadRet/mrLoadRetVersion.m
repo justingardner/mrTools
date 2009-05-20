c58bfd64b960311b6537358050a52519f2838c64
justin <justin@d7859a6d-3d6a-0410-b503-b92f2c4db295>
2009-05-20 03:21:44 +0000

    testing svn repository

--- trunk/mrLoadRet/mrLoadRetVersion.m
+++ trunk/mrLoadRet/mrLoadRetVersion.m
@@ -4,4 +4,3 @@ function [ver, expectedMatlabVersion] = mrLoadRetVersion
 ver = 4.5;
 expectedMatlabVersion = [7.2 7.3 7.4 7.5 7.6 7.7 7.8];  % Change this after testing Matlab upgrades
 
-% test comment, just testing svn repository will delete later
