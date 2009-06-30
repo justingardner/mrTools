5f7087284838550eac38d01ea569fcdeea5ac200
valerio <valerio@d7859a6d-3d6a-0410-b503-b92f2c4db295>
2009-06-30 17:25:49 +0000

    Get rid of the test.

--- branches/valerio_surface_roi/mrLoadRet/mrLoadRetVersion.m
+++
@@ -1,6 +0,0 @@
-function [ver, expectedMatlabVersion] = mrLoadRetVersion
-%     $Id$	
-
-ver = 4.5;
-expectedMatlabVersion = [7.2 7.3 7.4 7.5 7.6 7.7 7.8];  % Change this after testing Matlab upgrades
-
