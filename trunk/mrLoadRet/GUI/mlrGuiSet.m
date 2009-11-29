adcaf20a64969ebd27680b6f0fb82933fdac492f
justin <justin@d7859a6d-3d6a-0410-b503-b92f2c4db295>
2009-11-29 03:36:11 +0000

    Adding changes made by Luke Robot Hospadaruk (Taosheng Liu's tech) to allow drawing ROIs on surfaces. Note that Luke used another downloaded version of dijkstra's algorithm because he needed one that also retured info about the shortest path, rather than just the path length. This version is called dijkstrap.cpp and should be compiled with fibheap.cpp using -largeArrayDims. These files are in the mrLoadRet/ROI directory. (I've compiled for mexmaci and mexmaci64). If they are not compiled the user will get an error message telling them to compile.
    Also, fixed a small bug with the 3d free rotation of surfaces in which it was using an old view variable -- which would have caused it to lose any changes in between 3d free rotations.

--- trunk/mrLoadRet/GUI/mlrGuiSet.m
+++ trunk/mrLoadRet/GUI/mlrGuiSet.m
@@ -103,9 +103,15 @@ switch lower(field)
     set(handles.convertCorticalDepthRoiMenuItem,'Enable','on');
   end
   if value == 2
-    set(handles.createRoiMenu,'Enable','off');
-    set(handles.addRoiMenu,'Enable','off');
-    set(handles.removeRoiMenu,'Enable','off');
+    set(handles.createRoiMenu,'Enable','on');
+      set(handles.createLineMenuItem,'Enable','off');
+      set(handles.createRectangleMenuItem,'Enable','off');
+      set(handles.addLineMenuItem,'Enable','off');
+      set(handles.addRectangleMenuItem,'Enable','off');
+      set(handles.removeLineMenuItem,'Enable','off');
+      set(handles.removeRectangleMenuItem,'Enable','off');
+    set(handles.addRoiMenu,'Enable','on');
+    set(handles.removeRoiMenu,'Enable','on');
     set(handles.rotateSlider,'SliderStep',[15 45]./360);
     set(handles.baseTiltSlider,'SliderStep',[15 45]./360);
     set(handles.baseTiltSlider,'Visible','on');
@@ -121,6 +127,12 @@ switch lower(field)
     set(handles.baseTilt,'Visible','off');
     set(handles.createRoiMenu,'Enable','on');
     set(handles.addRoiMenu,'Enable','on');
+      set(handles.createLineMenuItem,'Enable','on');
+      set(handles.createRectangleMenuItem,'Enable','on');
+      set(handles.addLineMenuItem,'Enable','on');
+      set(handles.addRectangleMenuItem,'Enable','on');
+      set(handles.removeLineMenuItem,'Enable','on');
+      set(handles.removeRectangleMenuItem,'Enable','on');
     set(handles.removeRoiMenu,'Enable','on');
     set(handles.rotateSlider,'SliderStep',[1 45]./360);
   end		  
