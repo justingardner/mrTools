853630da839e97cec628d29a85c80c5a74f86940
justin <justin@d7859a6d-3d6a-0410-b503-b92f2c4db295>
2009-12-09 08:29:49 +0000

    Cleaning up code a little bit -- making all procpar reading happen in fid2xform rather than in both places

--- trunk/mrUtilities/File/Varian/fid2xform.m
+++ trunk/mrUtilities/File/Varian/fid2xform.m
@@ -42,17 +42,13 @@ end
 dim = [procpar.ni procpar.nv length(procpar.pss)];
 
 % check to see if this is a sense reconstruction and what the sense acceleration factor is
-senseFactor = [];
-if isfield(procpar,'petable') && ~isempty(procpar.petable{1}) && (length(procpar.petable{1}>1))
-  % petable name that ends in r means a sense protocol
-  if isequal(procpar.petable{1}(end),'r')
-    % check for the sense factor, (hopefully this number won't be greater than 9!!)
-    senseFactor = str2num(procpar.petable{1}(end-1));
-    % fix dimensions
-    dim(1) = dim(1)*senseFactor;
-    dim(2) = dim(2)*senseFactor;
-    % print message
-    if verbose>=0, disp(sprintf('(fid2xform) Found a sense factor of %i. Dims are now [%i %i]',senseFactor,dim(1),dim(2)-navechoes));end
+if procpar.accfactor > 1
+  % fix dimensions
+  dim(1) = dim(1)*procpar.accfactor
+  dim(2) = dim(2)*procpar.accfactor;
+  % print message
+  if verbose>=0
+    disp(sprintf('(fid2xform) Found a sense factor of %i. Dims are now [%i %i]',procpar.accfactor,dim(1),dim(2)-navechoes));
   end
 end
 
@@ -127,6 +123,21 @@ if verbose > 0
   disp(sprintf('(fid2xform) offset to origin: [%0.2f %0.2f %0.2f]',originOffset(1),originOffset(2),originOffset(3)));
 end
 
+% work out what the tr is, this is just for setting in the info output field and
+% is used by fid2niftihdr
+% get the volume TR (framePeriod) for EPI 
+tr = procpar.tr;
+% if we run mutliple shots, volume TR = slice TR * shots 
+if isfield(procpar,'navechoes')
+  tr = tr * procpar.numshots;
+end
+% if we run slice at not interleaved way, 
+%  volume TR = slice TR * shots * slice number
+if isfield(procpar,'intlv') && strcmp(procpar.intlv, 'n')
+  tr = tr * length(procpar.pss);
+end
+
+
 % pack up some useful information that we have learned about the scan
 % into an output structure that can be used by other programs.
 info.dim = dim;
@@ -138,12 +149,13 @@ info.pss = procpar.pss;
 info.psi = procpar.psi;
 info.phi = procpar.phi;
 info.theta = procpar.theta;
-info.senseFactor = senseFactor;
+info.accFactor = procpar.accfactor;
 if isfield(procpar,'cntr')
   info.dim(4) = length(procpar.cntr)-1;
 else
   info.dim(4) = 1;
 end
+info.tr = tr;
 
 %%%%%%%%%%%%%%%%%%%%%%%%%
 %%   euler2rotmatrix   %%
