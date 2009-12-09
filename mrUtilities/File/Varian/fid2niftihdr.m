a1db542a23c98285202dff355e46abeaa5e4a7bd
justin <justin@d7859a6d-3d6a-0410-b503-b92f2c4db295>
2009-12-09 08:29:49 +0000

    Cleaning up code a little bit -- making all procpar reading happen in fid2xform rather than in both places

--- mrUtilities/File/Varian/fid2niftihdr.m
+++ mrUtilities/File/Varian/fid2niftihdr.m
@@ -20,69 +20,24 @@ end
 % set fid extension
 fidname = setext(fidname,'fid',0);
 
-% read procpar
-procpar = readprocpar(fidname);
-if isempty(procpar)
-  disp(sprintf('(fid2niftihdr) Could not find procpar in %s',fidname))
-  return
-end
-
 % create an empty header
 hdr = cbiCreateNiftiHeader;
 
-% get the number of navechoes
-if isfield(procpar,'navechoes')
-  navechoes = procpar.navechoes;
-else
-  navechoes = 0;
-end
-
-% get the dimensions of the scan
-dim = [procpar.ni procpar.nv length(procpar.pss)];
-
-% check to see if this is a sense reconstruction and what the sense acceleration factor is
-senseFactor = [];
-if isfield(procpar,'petable') && ~isempty(procpar.petable{1}) && (length(procpar.petable{1}>1))
-  % petable name that ends in r means a sense protocol
-  if isequal(procpar.petable{1}(end),'r')
-    % check for the sense factor, (hopefully this number won't be greater than 9!!)
-    senseFactor = str2num(procpar.petable{1}(end-1));
-    % fix dimensions
-    dim(1) = dim(1)*senseFactor;
-    dim(2) = dim(2)*senseFactor;
-    % print message
-    if verbose>=0, disp(sprintf('(fid2niftihdr) Found a sense factor of %i. Dims are now [%i %i]',senseFactor,dim(1),dim(2)-navechoes));end
-  end
-end
-
-% remove navigator echoes from k-space
-dim(2) = dim(2) - navechoes;
-
-% now set dimensions in header
-hdr.dim(1:4) = [3 dim];
-
 % get the qform
-qform44 = fid2xform(fidname);
+[qform44 info] = fid2xform(fidname);
 
 % set the qform
 hdr = cbiSetNiftiQform(hdr,qform44);
 
-% set voxel dimensions (note that this is done after cbiSetNiftiQform, since that
-% function decides that the voxel spacing and the voxel size should be the same).
-hdr.pixdim(2:4) = [10*procpar.lro/dim(1) 10*procpar.lpe/dim(2) procpar.thk];
+% now set dimensions in header
+nDims = length(info.dim);
+hdr.dim(1:nDims+1) = [nDims info.dim];
 
-% get the volume TR (framePeriod) for EPI 
-tr = procpar.tr;
-% if we run mutliple shots, volume TR = slice TR * shots 
-if isfield(procpar,'navechoes')
-  tr = tr * procpar.numshots;
-end
-% if we run slice at not interleaved way, 
-%  volume TR = slice TR * shots * slice number
-if isfield(procpar,'intlv') && strcmp(procpar.intlv, 'n')
-  tr = tr * length(procpar.pss);
-end
-hdr.pixdim(hdr.dim(1)+2) = tr*1000;
+% set voxel dimensions 
+hdr.pixdim(2:4) = info.voxsize(1:3);
+
+% set the tr
+hdr.pixdim(5) = info.tr*1000;
 
 
 
