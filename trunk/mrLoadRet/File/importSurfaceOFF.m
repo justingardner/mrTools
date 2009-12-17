4f42d65c0ac79643dcde81bda9342699bc27c0f6
eli <eli@d7859a6d-3d6a-0410-b503-b92f2c4db295>
2009-12-17 17:39:22 +0000

    Added an option to import both right and left hemisphere surfaces. If bothHemiFlag is set to 1, then the function loads the inner and outer surfaces for both hemispheres, concatenates them together, and creates a single baseAnat with both hemispheres. Having both hemispheres loaded up in MLR is convenient for data viewing and it makes extending/restricting ROI's easier when the ROI exists in both hemispheres. Still need to add a hook in MLR GUI to make bothHemiFlag an option.

--- trunk/mrLoadRet/File/importSurfaceOFF.m
+++ trunk/mrLoadRet/File/importSurfaceOFF.m
@@ -3,16 +3,25 @@
 %      usage: v = importSurfaceOFF
 %         by: justin gardner
 %       date: 10/24/07
-%    purpose: 
+%    purpose: Import a pair of inner and outer cortical surfaces in OFF format
 %
-function base = importSurfaceOFF(pathStr)
+%    base = importSurfaceOFF('/path/to/surfaces/subject_left_WM.off', bothHemiFlag);
+%    viewSet(getMLRView, 'newbase', base);
+%
+function base = importSurfaceOFF(pathStr,bothHemiFlag)
 
 % check arguments
-if ~any(nargin == [0 1])
+if ~any(nargin == [0 1 2])
   help importSurfaceOFF
   return
 end
 base = [];
+if ieNotDefined('bothHemiFlag'), 
+  bothHemiFlag = 0;
+  disp(sprintf('Only loading surfaces for one hemisphere'))
+else
+  disp(sprintf('Loading surfaces for both hemispheres'))
+end
 
 if ieNotDefined('pathStr')
   % Open dialog box to have user choose the file
@@ -29,38 +38,90 @@ if ieNotDefined('pathStr'),return,end
 [filepath filename] = fileparts(pathStr);
 thispwd = pwd;
 if ~isempty(filepath),cd(filepath);end
-params = mrSurfViewer(filename);
+params1 = mrSurfViewer(filename);
 
 % Aborted
-if isempty(params),cd(thispwd);return,end
+if isempty(params1),cd(thispwd);return,end
 
 % Create the base
-base.hdr = cbiReadNiftiHeader(params.anatomy);
+base.hdr = cbiReadNiftiHeader(params1.anatomy);
 base.name = filename;
 base.permutationMatrix = getPermutationMatrix(base.hdr);
-base.data(1,:,1) = loadVFF(params.curv);
 base.range = [-1.5 1.5];
 base.clip = [-1.5 1.5];
 
 % get the vol2mag and vol2tal fields from the volume anatomy
 % do it using a subfunction so don't confuse the two 'base' structure variables
-anatomyMatFile = sprintf('%s.mat',stripext(params.anatomy));
+anatomyMatFile = sprintf('%s.mat',stripext(params1.anatomy));
 base.vol2tal = getBaseField(anatomyMatFile,'vol2tal');
 base.vol2mag = getBaseField(anatomyMatFile,'vol2mag');
 
-% load the inner and outer coords
-innerSurface = loadSurfOFF(params.innerSurface);
-outerSurface = loadSurfOFF(params.outerSurface);
-if strcmp(params.innerCoords,'Same as surface')
-  inner = innerSurface;
-else
-  inner = loadSurfOFF(params.innerCoords);
-end
-if strcmp(params.outerCoords,'Same as surface')
-  outer = outerSurface;
-else
-  outer = loadSurfOFF(params.outerCoords);
-end
+% load both hemispheres
+if bothHemiFlag
+  % get the params for the other hemisphere
+  % was a left hemisphere passed?
+  leftFlag = strfind(lower(params1.innerSurface), 'left');
+  if leftFlag
+    prefix = params1.innerSurface(1:leftFlag-1);
+    postfix = params1.innerSurface(leftFlag+4:end);
+    rightFilename = sprintf('%sright%s', prefix, postfix);
+    params2 = mrSurfViewer(rightFilename);  
+  end
+  % or was it a right hemispehre
+  rightFlag = strfind(lower(params1.innerSurface), 'right');
+  if rightFlag
+    prefix = params1.innerSurface(1:rightFlag-1);
+    postfix = params1.innerSurface(rightFlag+5:end);
+    leftFilename = sprintf('%sleft%s', prefix, postfix);
+    params2 = mrSurfViewer(leftFilename);  
+  end
+  % and add the second curvature
+  data1(1,:,1) = loadVFF(params1.curv);
+  data2(1,:,1) = loadVFF(params2.curv);
+  base.data(1,:,1) = cat(2, data1, data2);
+  % load both inner surfaces
+  innerSurface1 = loadSurfOFF(params1.innerSurface);
+  innerSurface2 = loadSurfOFF(params2.innerSurface);
+  innerSurface = combineSurfaces(innerSurface1, innerSurface2);
+  % load both outer surfaces
+  outerSurface1 = loadSurfOFF(params1.outerSurface);
+  outerSurface2 = loadSurfOFF(params2.outerSurface);
+  outerSurface = combineSurfaces(outerSurface1, outerSurface2);
+  % load the inner coords   
+  if strcmp(params1.innerCoords,'Same as surface')
+    inner = innerSurface;
+  else
+    inner1 = loadSurfOFF(params1.innerCoords);
+    inner2 = loadSurfOFF(params2.innerCoords);
+    inner = combineSurfaces(inner1,inner2);
+  end
+  if strcmp(params1.outerCoords,'Same as surface')
+    outer = outerSurface;
+  else
+    outer1 = loadSurfOFF(params1.outerCoords);
+    outer2 = loadSurfOFF(params2.outerCoords);
+    outer = combineSurfaces(inner1,inner2);
+  end
+
+else                                    
+  % or else a single hemisphere...
+  % load the curvature
+  base.data(1,:,1) = loadVFF(params1.curv);
+  % load inner surfaces
+  innerSurface = loadSurfOFF(params1.innerSurface);
+  % load outer surface
+  outerSurface = loadSurfOFF(params1.outerSurface);
+  if strcmp(params1.innerCoords,'Same as surface')
+    inner = innerSurface;
+  else
+    inner = loadSurfOFF(params1.innerCoords);
+  end
+  if strcmp(params1.outerCoords,'Same as surface')
+    outer = outerSurface;
+  else
+    outer = loadSurfOFF(params1.outerCoords);
+  end
+end  
 
 % now convert all surfaces to array coordinates
 innerSurface = xformSurfaceWorld2Array(innerSurface,base.hdr);
@@ -70,19 +131,19 @@ outer = xformSurfaceWorld2Array(outer,base.hdr);
 
 % save names
 base.coordMap.path = filepath;
-base.coordMap.innerSurfaceFileName = params.innerSurface;
-base.coordMap.innerCoordsFileName = params.innerCoords;
-base.coordMap.outerSurfaceFileName = params.outerSurface;
-base.coordMap.outerCoordsFileName = params.outerCoords;
-base.coordMap.curvFileName = params.curv;
-base.coordMap.anatFileName = params.anatomy;
+base.coordMap.innerSurfaceFileName = params1.innerSurface;
+base.coordMap.innerCoordsFileName = params1.innerCoords;
+base.coordMap.outerSurfaceFileName = params1.outerSurface;
+base.coordMap.outerCoordsFileName = params1.outerCoords;
+base.coordMap.curvFileName = params1.curv;
+base.coordMap.anatFileName = params1.anatomy;
 % save coords
 base.coordMap.innerCoords(1,:,1,1)  = inner.vtcs(:,1);
 base.coordMap.innerCoords(1,:,1,2)  = inner.vtcs(:,2);
 base.coordMap.innerCoords(1,:,1,3)  = inner.vtcs(:,3);
 base.coordMap.innerVtcs = innerSurface.vtcs;
 base.coordMap.tris = innerSurface.tris;
-base.coordMap.outer = params.outerSurface;
+base.coordMap.outer = params1.outerSurface;
 base.coordMap.outerCoords(1,:,1,1)  = outer.vtcs(:,1);
 base.coordMap.outerCoords(1,:,1,2)  = outer.vtcs(:,2);
 base.coordMap.outerCoords(1,:,1,3)  = outer.vtcs(:,3);
@@ -93,7 +154,6 @@ base.type = 2;
 
 cd(thispwd);
 
-
 function val = getBaseField(matFilename,fieldname)
 if ~exist(matFilename,'file') % if the anatomy doesn't have these fields set
   val = [];
@@ -103,4 +163,10 @@ else % if the volume anatomy has the fields, use them
 end
 
 
-  
+function surf = combineSurfaces(surf1, surf2);
+surf.filename = sprintf('%_%s', surf1.filename, surf2.filename);
+surf.Nvtcs = surf1.Nvtcs + surf2.Nvtcs;
+surf.Ntris = surf1.Ntris + surf2.Ntris;
+surf.Nedges = 0;
+surf.vtcs = [surf1.vtcs; surf2.vtcs];
+surf.tris = [surf1.tris; surf2.tris+surf1.Nvtcs];
