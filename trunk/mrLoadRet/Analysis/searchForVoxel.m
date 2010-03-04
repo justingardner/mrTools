82064a45899573d7ce09f3f8ca6274ce5dc96be9
justin <justin@d7859a6d-3d6a-0410-b503-b92f2c4db295>
2010-03-04 13:12:15 +0000

    Continuous mode search for voxel, if you have a 2D or 3D base anatom loaded then it will put up a figure so that you can move your mouse over any point on your surface and it will continuously show where each point comes from

--- trunk/mrLoadRet/Analysis/searchForVoxel.m
+++ trunk/mrLoadRet/Analysis/searchForVoxel.m
@@ -11,10 +11,22 @@ function retval = searchForVoxel(v,overlayNum,scan,x,y,s,roi,varargin)
 % get the view number
 viewNum = viewGet(v,'viewNum');
 
+% get base types
+for iBase = 1:viewGet(v,'numBase')
+  baseType(iBase) = viewGet(v,'baseType',iBase);
+end
+
+% look for a non surface or flat map base
+startBaseNum = first(find(baseType==0));
+if ~isempty(startBaseNum)
+  baseName = viewGet(v,'baseName',startBaseNum);
+else
+  baseName = viewGet(v,'baseName');
+end
+
 % get parameters
 paramsInfo = {};
 scanDims = viewGet(v,'scanDims');
-baseName = viewGet(v,'baseName');
 sliceOrientations = {'default','axial','coronal','sagittal'};
 baseNames = putOnTopOfList(baseName,viewGet(v,'baseNames'));
 paramsInfo{end+1} = {'baseName',baseNames,'Choose which base to find voxel on'};
@@ -24,7 +36,19 @@ paramsInfo{end+1} = {'y',y,'incdec=[-1 1]','round=1',sprintf('minmax=[1 %i]',sca
 paramsInfo{end+1} = {'s',s,'incdec=[-1 1]','round=1',sprintf('minmax=[1 %i]',scanDims(3)),'Choose S coordinate to look for'};
 paramsInfo{end+1} = {'color',color2RGB,'Choose color to display voxel in'};
 paramsInfo{end+1} = {'maxSearchRadius',5,'type=numeric','If there is no exact match, then will display the closest voxel within this search radius. Set smaller to force only display of more exact matches. Set higher if you want to allow matches that are farther away.'};
-params = mrParamsDialog(paramsInfo,'Look for voxel');
+paramsInfo{end+1} = {'continuousMode',~isempty(startBaseNum),'type=checkbox','Opens another window with your anatomy that will be continuously update with the position as you move your mouse over points in the viewer','callback',@continuousModeCallback,'passParams=1','callbackArg',v,'editable',isempty(startBaseNum)};
+
+% start continuous mode
+if ~isempty(startBaseNum)
+  continuousModeCallback(v,mrParamsDefault(paramsInfo));
+end
+
+% put up dialog box
+params = mrParamsDialog(paramsInfo,'Look for voxel','callback',@searchForVoxelCallback,'modal=1');
+
+% shut down the continuousModeMouseMove function
+continuousModeEnd;
+
 if isempty(params)
   % if user hit cancel, then refresh
   % display to remove any marked voxel and return
@@ -106,3 +130,233 @@ else
 end
 
 hold off
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%    continuousModeCallback    %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function retval = continuousModeCallback(v,params)
+
+retval = [];
+
+if params.continuousMode
+  continuousModeStart(v,params);
+else
+  continuousModeEnd;
+end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%   continuousModeEnd   %%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function continuousModeEnd(x,y,z)
+
+global gCMode;
+
+if isfield(gCMode,'continuousMode') && gCMode.continuousMode
+  v = viewGet([],'view',gCMode.viewNum);
+  if isempty(v),return,end
+  fignum = viewGet(v,'fignum');
+  set(fignum,'WindowButtonMotionFcn',gCMode.oldMouseMoveFcn);
+  set(fignum,'WindowButtonDownFcn',gCMode.oldMouseDownFcn);
+  close(gCMode.graphFig);
+  gCMode.continuousMode = 0;
+end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%   continuousModeStart   %%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function continuousModeStart(v,params)
+
+% get some info that will be used by continuousModeMouseMove
+global gCMode;
+if ~isfield(gCMode,'continuousMode') || ~gCMode.continuousMode
+  gCMode.continuousMode = 1;
+  gCMode.fignum = viewGet(v,'fignum');
+  g = guidata(gCMode.fignum);
+  gCMode.axesNum = g.axis;
+  gCMode.viewNum = viewGet(v,'viewNum');;
+  
+  % set the parameters
+  updateContinuousModeParams(params);
+
+  % set the main viewer to use our mouse move function
+  gCMode.oldMouseMoveFcn = get(gCMode.fignum,'WindowButtonMotionFcn');
+  gCMode.oldMouseDownFcn = get(gCMode.fignum,'WindowButtonDownFcn');
+  set(gCMode.fignum,'WindowButtonMotionFcn',@continuousModeMouseMove);
+  set(gCMode.fignum,'WindowButtonDownFcn',@continuousModeEnd);
+  
+  % open the figure for displaying
+  gCMode.graphFig = selectGraphWin;
+  subplot(1,3,1)
+  gCMode.a1 = gca;
+  axis(gCMode.a1,'off');
+  subplot(1,3,2)
+  gCMode.a2 = gca;
+  axis(gCMode.a2,'off');
+  subplot(1,3,3)
+  gCMode.a3 = gca;
+  axis(gCMode.a3,'off');
+  colormap(gray);
+end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%   updateContinuousModeParams   %%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function updateContinuousModeParams(params)
+
+global gCMode;
+
+if isfield(gCMode,'viewNum')
+  v = viewGet([],'view',gCMode.viewNum);
+
+  gCMode.baseNum = viewGet(v,'baseNum',params.baseName);
+  gCMode.base2base = viewGet(v,'base2base',gCMode.baseNum);
+  gCMode.baseType = viewGet(v,'baseType',gCMode.baseNum);
+  gCMode.color = color2RGB(params.color);
+  gCMode.baseOrientation = find(strcmp(params.baseOrientation,{'default','axial','coronal','sagittal'}));
+  gCMode.base = viewGet(v,'baseData',gCMode.baseNum);
+  gCMode.baseDims = size(gCMode.base);
+end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%    continousModeMouseMove    %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function continuousModeMouseMove(fignum,x);
+
+
+global gCMode;
+if ~isfield(gCMode,'continuousMode') || (gCMode.continuousMode==0)
+  return
+end
+if gCMode.baseType ~= 0
+  cla(gCMode.a1);
+  axis(gCMode.a1,'off');
+  title(sprintf('Continuous mode not implemented for surface/flat maps'),'Parent',gCMode.a1);
+  cla(gCMode.a2);
+  axis(gCMode.a2,'off');
+  cla(gCMode.a3);
+  axis(gCMode.a3,'off');
+  return
+end
+% get the mouse coordinates
+[x y s xBase yBase sBase] = getMouseCoords;
+
+% convert to the base units
+if ~isnan(xBase)
+  baseCoord = gCMode.base2base * [xBase yBase sBase 1]';
+  if (baseCoord(1) >= 1) && (baseCoord(1) <= gCMode.baseDims(1)) && (baseCoord(2) >= 1) && (baseCoord(2) <= gCMode.baseDims(2)) && (baseCoord(3) >= 1) && (baseCoord(3) <= gCMode.baseDims(3))
+    if gCMode.baseOrientation == 1
+      cla(gCMode.a1,'reset');
+      imagesc(gCMode.base(:,:,round(baseCoord(3))),'Parent',gCMode.a1)
+      hold(gCMode.a1,'on');
+      plot(baseCoord(2),baseCoord(1),'.','MarkerEdgeColor',gCMode.color,'Parent',gCMode.a1);
+      title(sprintf('BaseCoord: %i %i %i',round(baseCoord(1)),round(baseCoord(2)),round(baseCoord(3))),'Parent',gCMode.a1);
+      cla(gCMode.a2,'reset');
+      imagesc(squeeze(gCMode.base(:,round(baseCoord(2)),:)),'Parent',gCMode.a2);
+      hold(gCMode.a2,'on');
+      plot(baseCoord(3),baseCoord(1),'.','MarkerEdgeColor',gCMode.color,'Parent',gCMode.a2);
+      cla(gCMode.a3,'reset');
+      imagesc(squeeze(gCMode.base(round(baseCoord(1)),:,:)),'Parent',gCMode.a3);
+      hold(gCMode.a3,'on');
+      plot(baseCoord(3),baseCoord(2),'.','MarkerEdgeColor',gCMode.color,'Parent',gCMode.a3);
+    end
+  end
+end
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% get current mouse position in image coordinates
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function [x y s xBase yBase sBase xTal yTal zTal] = getMouseCoords;
+
+global gCMode;
+fignum = gCMode.fignum;
+axesNum = gCMode.axesNum;
+v = viewGet([],'view',gCMode.viewNum);
+
+% no bases
+if viewGet(v,'numBase') == 0
+  x = nan;y= nan;s = nan;xBase = nan;yBase = nan;sBase = nan;xTal = nan;yTal = nan;zTal = nan;
+  return
+end
+baseType = viewGet(v,'baseType');
+
+% get location of pointer
+pointerLoc = get(axesNum,'CurrentPoint');
+
+if baseType <= 1
+  mouseY = round(pointerLoc(1,1));
+  mouseX = round(pointerLoc(1,2));
+
+  % get base coordinates
+  baseCoords = viewGet(v,'cursliceBaseCoords');
+  % convert mouse to baseCoords
+  if (mouseX>0) && (mouseX<=size(baseCoords,1)) && (mouseY>0) && (mouseY<=size(baseCoords,2))
+    xBase = baseCoords(mouseX,mouseY,1);
+    yBase = baseCoords(mouseX,mouseY,2);
+    sBase = baseCoords(mouseX,mouseY,3);
+  else
+    x = nan;y = nan; s = nan;
+    xBase = nan;yBase = nan; sBase = nan;
+    xTal = nan; yTal = nan; zTal = nan;
+    return
+  end
+else
+  % handle getting coordinates for surface
+  baseSurface = viewGet(v,'baseSurface');
+  baseDims = viewGet(v,'baseSurfaceDims');
+  pos = [];xBase = nan; yBase = nan; sBase = nan;
+  % check mouse bounding box coords against baseDims
+  % for a quick check to see if we are in the volume
+  if all(pointerLoc(1,:) >= 0)
+    % then use select3d which is slooow, but accurate
+    hobj = get(axesNum,'Children');
+    % make sure we are using the correct object (should be the 3D
+    % brain). Use end here because with searchForVoxel we plot a
+    % point on the image and the brain object always seems to be
+    % last. But if this is not always the case, then we may need
+    % to do a little more work here to find the correct object
+    [pos v vi] = select3d(hobj(end));
+    % convert the index to the coordinates
+    if ~isempty(pos)
+      baseCoordMap = viewGet(v,'baseCoordMap');
+      pos = round(squeeze(baseCoordMap.coords(1,vi,1,:)));
+      xBase = pos(1);yBase = pos(2);sBase = pos(3);
+    end
+  end      
+end
+
+% transform from base coordinates to talairach coordinates, if the base has a talairach transform defined
+base2tal = viewGet(v,'base2tal'); % keyboard
+if(~isempty(base2tal))
+  talCoords = round(base2tal * [xBase yBase sBase 1]');
+  xTal = talCoords(1); yTal = talCoords(2); zTal = talCoords(3);
+else
+  xTal = nan; yTal = nan; zTal = nan;
+end
+
+
+% transform from base coordinates into scan coordinates
+base2scan = viewGet(v,'base2scan');
+if isempty(base2scan), x = nan; y=nan; s=nan; return,  end
+transformed = round(base2scan*[xBase yBase sBase 1]');
+
+x = transformed(1);
+y = transformed(2);
+s = transformed(3);
+
+% get the scan dims to make sure we haven't jumped off end
+% of scan
+scanDims = viewGet(v,'scanDims',viewGet(v,'curScan'));
+if ((x < 1) || (x > scanDims(1)) || ...
+        (y < 1) || (y > scanDims(2)) || ...
+        (s < 1) || (s > scanDims(3)))
+    x = nan;y = nan;s = nan;
+end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%   searchForVoxelCallback   %%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function params = searchForVoxelCallback(params)
+
+updateContinuousModeParams(params)
