e1a0e8087ca3d9149120e7d988cd9430ee846fb1
julien <julien@d7859a6d-3d6a-0410-b503-b92f2c4db295>
2010-11-13 21:15:07 +0000

    newGraphWin.m, selectGraphWin.m, closeGraphWin.m: handles several graph windows at the same time, in case MR preference graphWindow is set to 'Make new', default is to replace the current window ('Replace')

--- mrLoadRet/GUI/closeGraphWin.m
+++ mrLoadRet/GUI/closeGraphWin.m
@@ -1,22 +1,35 @@
-function closeGraphWin
+function closeGraphWin(h)
 %
-% closeGraphWin
+% closeGraphWin([h])
 %
-% Closes the current graphWin.  Sets the global GRAPHWIN=0
+%  $Id$
+%
+% Closes the current graphWin, or graphWin with handle h, that's part of MLR graph figures.  Sets global variable MLR
 %
 % djh, 3/3/98
 
 mrGlobals
 
-h = MLR.graphFigure;
-if h
-  if (h == get(0,'CurrentFigure'))
-    MLR.graphFigure = []; 
-    % save the position
-    mrSetFigLoc('graphFigure',get(h,'Position'));
+if ~isempty(MLR.graphFigure)
+  if ieNotDefined('h')
+    h = selectGraphWin(1);
   end
-end 
+  % save the position
+  if ismember(h,MLR.graphFigure)
+    mrSetFigLoc(['graphFigure' int2str(h)],get(h,'Position'));
+    MLR.graphFigure = setdiff(MLR.graphFigure,h); 
+    delete(h)
+    return;
+  end
+end
 
-delete(get(0,'CurrentFigure'));
+% Don't do that unless the handle to the main GUI handle is made invisible:
+ 
+% %if there are no graph figures in MLR, just delete the figure
+% if isempty(h)
+%   h = gcf;               
+% end
+%   delete(h)
+% end
 
 
