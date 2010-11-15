d15a3d00d259216460c9714331fe47a2e7fb50fa
julien <julien@d7859a6d-3d6a-0410-b503-b92f2c4db295>
2010-11-15 13:35:38 +0000

    Put folders ColorMaps, Interrogators and CombineOverlayFunctions in folder Plugins

--- branches/julien/mrLoadRet/Analysis/combineOverlays.m
+++ branches/julien/mrLoadRet/Analysis/combineOverlays.m
@@ -13,7 +13,7 @@ combinationModeMenu = {'Apply function to all overlays','Apply function to each 
 %default params
 %get names of combine Functions in combineFunctions directory
 functionsDirectory = which('mrLoadRetGUI');
-functionsDirectory = [functionsDirectory(1:strfind(functionsDirectory,'GUI/mrLoadRetGUI.m')-1) 'combineOverlayFunctions/'];
+functionsDirectory = [functionsDirectory(1:strfind(functionsDirectory,'GUI/mrLoadRetGUI.m')-1) 'Plugins/CombineOverlayFunctions/'];
 combineFunctionFiles =  dir([functionsDirectory '*.m']);
 for iFile=1:length(combineFunctionFiles)
    combineFunctions{iFile} = stripext(combineFunctionFiles(iFile).name);
