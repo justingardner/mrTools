ca86634989bd10374a4ecd4ec699af67f08a2c80
julien <julien@d7859a6d-3d6a-0410-b503-b92f2c4db295>
2010-12-18 17:47:10 +0000

    mrParamsCopyFields.m: only copies parameters with corresponding paramInfo entry

--- mrUtilities/MatlabUtilities/mrParamsCopyFields.m
+++ mrUtilities/MatlabUtilities/mrParamsCopyFields.m
@@ -3,33 +3,38 @@
 %      usage: params1 = mrParamsCopyFields(params1,params2)
 %         by: julien besle 
 %       date: 04/12/2010
-%    purpose: copies (and replaces) all the fields from one params structure to another
+%    purpose: copies (and replaces) fields from one params structure to another
+%              only fields with a corresponding paramInfo cell are copied
 %              $Id$
 
 function params2 = mrParamsCopyFields(params1,params2)
 
-fieldNames=fieldnames(params1);
-for iField = 1:length(fieldNames)
-  for iParams = 1:length(params1.paramInfo)
-    if strcmp(params1.paramInfo{iParams}{1},fieldNames{iField})
-      thisParamInfo = params1.paramInfo{iParams};
+if ieNotDefined('params2')
+  params2=[];
+end
+
+if ~isfield(params1,'paramInfo')
+  mrWarnDlg('(mrParamsCopyFields) Structure to copy is not a parameter structure. Use copyFields instead');
+  return;
+end
+
+if ~isfield(params2,'paramInfo')
+  params2.paramInfo = [];
+end
+
+for iParam = 1:length(params1.paramInfo)
+  params2.(params1.paramInfo{iParam}{1})=params1.(params1.paramInfo{iParam}{1});
+  %find the paraminfo cell in struct2
+  for jParam = 1:length(params2.paramInfo)
+    foundParam = 0;
+    if strcmp(params2.paramInfo{jParam}{1},params1.paramInfo{iParam}{1})
+      params2.paramInfo{jParam}=params1.paramInfo{iParam};
+      foundParam=1;
       break;
     end
   end
-  if isfield(params2,fieldNames{iField}) && isfield(params2,'paramInfo')
-    params2.(fieldNames{iField})=eval(['params1.' fieldNames{iField}]);
-    for jParams = 1:length(params2.paramInfo)
-      if strcmp(params2.paramInfo{jParams}{1},fieldNames{iField})
-        params2.paramInfo{jParams}=thisParamInfo;
-        break;
-      end
-    end
-  else
-    params2.(fieldNames{iField})=eval(['params1.' fieldNames{iField}]);
-    if isfield(params2,'paramInfo')
-      params2.paramInfo{end+1} = thisParamInfo;
-    else
-      params2.paramInfo{1} = thisParamInfo;
-    end
+  if ~foundParam
+    params2.paramInfo{end+1} = params1.paramInfo{iParam};
   end
 end
+
