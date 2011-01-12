95c593c3882efaea35fbd9f74d474b20fab326d8
julien <julien@d7859a6d-3d6a-0410-b503-b92f2c4db295>
2011-01-12 00:23:17 +0000

    added transformROIs Plugin and transform function expandROI

---
+++ branches/julien/mrLoadRet/Plugin/transformROIs/TransformROIsFunctions/expandROI.m
@@ -0,0 +1,55 @@
+% expandROI.m
+%
+%        $Id: expandROI.m 1969 2010-12-19 19:14:32Z julien $ 
+%      usage: transformedRoi = expandROI(roi,margin,<kernelType>)
+%         by: julien besle
+%       date: 11/01/2011
+%
+%    purpose: expands roi coordinates in 3D by margin voxels
+%      input:   - margin: number of voxels that will be added around the ROI
+% kernelType:   - (optional) type of kernel to convole the ROI with: 
+%                     sphere (default), cube, disc, square or radius/half-side margin
+%                     discs and squares are in the X-Y plane  
+
+function roi = expandROI(roi,margin,kernelType)
+
+
+if ieNotDefined('kernelType')
+  kernelType = 'sphere';
+end
+if ~ismember(kernelType,{'sphere','cube','disc','square'})
+  mrWarnDlg(['(expandROI) unknown kernel type ' kernelType]);
+  roi=[];
+end
+
+boxCoords = [min(roi.coords,[],2)-margin  max(roi.coords,[],2)+margin];
+
+%shift coordinates so that the boxes starts at 1 on all dimensions
+voxelShift = -boxCoords(:,1)+1;
+
+boxCoords = boxCoords+repmat(voxelShift,1,2);
+roiCoords = roi.coords+repmat(voxelShift,1,size(roi.coords,2));
+
+volume = zeros(boxCoords(:,2)');
+volume(sub2ind(boxCoords(:,2)',roiCoords(1,:),roiCoords(2,:),roiCoords(3,:)))=1;
+
+switch(kernelType)
+  case 'sphere'
+    [sphereX,sphereY,sphereZ] = ndgrid(-margin:margin,-margin:margin,-margin:margin);
+    kernel = zeros(2*margin+1,2*margin+1,2*margin+1);
+    kernel((sphereX.^2+sphereY.^2+sphereZ.^2)<margin^2)=1;
+  case 'cube'
+    kernel = ones(2*margin,2*margin,2*margin);
+  case 'square'
+    kernel = ones(2*margin,2*margin);
+  case 'disc'
+    [discX,discY] = ndgrid(-margin:margin,-margin:margin);
+    kernel = zeros(2*margin+1,2*margin+1);
+    kernel((discX.^2+discY.^2)<margin^2)=1;
+end
+
+volume = logical(convn(volume,kernel,'same'));
+[newCoordsX,newCoordsY,newCoordsZ] = ind2sub(boxCoords(:,2)',find(volume));
+roi.coords = [newCoordsX-voxelShift(1) newCoordsY-voxelShift(2) newCoordsZ-voxelShift(3)]';
+
+
