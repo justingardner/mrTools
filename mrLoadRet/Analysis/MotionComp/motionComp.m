4ae15853e58dfd06669a0438998ddba705295b5b
julien <julien@d7859a6d-3d6a-0410-b503-b92f2c4db295>
2011-02-13 12:15:47 +0000

    AverageTSeries.m, motionComp.m: removed versn output argument from call to fileparts

--- mrLoadRet/Analysis/MotionComp/motionComp.m
+++ mrLoadRet/Analysis/MotionComp/motionComp.m
@@ -308,7 +308,7 @@ for s = 1:length(targetScans)
 
   % Save evalstring for recomputing and params
   evalstr = ['view = newView(','''','Volume','''','); view = motionComp(view,params);'];
-  [pathstr,filename,ext,versn] = fileparts(tseriesFileName);
+  [pathstr,filename] = fileparts(tseriesFileName);
   tseriesdir = viewGet(viewMotionComp,'tseriesdir');
   save(fullfile(tseriesdir,filename),'evalstr','params','transforms','tseriesFileName');
 
