bc8ea147c8b91f727859e399bfb926c7a13aaeeb
julien <julien@d7859a6d-3d6a-0410-b503-b92f2c4db295>
2011-04-14 15:49:12 +0000

    findMaxRoi.m: added center-of-mass info, added combineOverlayFunction clusterComMax

--- branches/julien/mrLoadRet/Plugin/interrogatorFolder/interrogatorFunctions/findMaxRoi.m
+++ branches/julien/mrLoadRet/Plugin/interrogatorFolder/interrogatorFunctions/findMaxRoi.m
@@ -52,17 +52,25 @@ for iOverlay = overlayNum;
      if ~isempty(scanRoiCoords)   
         scanRoiCoordsIndex = sub2ind(size(thisOverlayData), scanRoiCoords(1,:)',  scanRoiCoords(2,:)',  scanRoiCoords(3,:)' );
         roiOverlayData = thisOverlayData(scanRoiCoordsIndex);
+        totalNumberVoxels = numel(roiOverlayData);
 
         [max_value max_index] = max(roiOverlayData);
         [min_value min_index] = min(roiOverlayData);
-
         max_coordinates = scanRoiCoords(:,max_index);
         min_coordinates = scanRoiCoords(:,min_index);
 
+        %find the center of mass
+        scanRoiCoordsIndex = scanRoiCoordsIndex(~isnan(roiOverlayData));
+        roiOverlayData = roiOverlayData(~isnan(roiOverlayData));
+        [coordsX,coordsY,coordsZ] = ind2sub(size(thisOverlayData),scanRoiCoordsIndex);
+        COM_coordinates = round(sum(repmat(roiOverlayData,1,3)/sum(roiOverlayData) .* [coordsX,coordsY,coordsZ],1))';
+
         max_base_coordinates = (base2scan\[max_coordinates;1]);
         min_base_coordinates = (base2scan\[min_coordinates;1]);
+        COM_base_coordinates = (base2scan\[COM_coordinates;1]);
+        
 
-        fprintf(1,['\tROI ' roi{iRoi}.name '(' num2str(numel(find(~isnan(roiOverlayData)))) '/' num2str(numel(roiOverlayData)) ' scan voxels)\n']);
+        fprintf(1,['\tROI ' roi{iRoi}.name '(' num2str(numel(roiOverlayData)) '/' num2str(totalNumberVoxels) ' scan voxels)\n']);
         fprintf(1,['\t\tmax value :' num2str(max_value) '\n']);
         fprintf(1,['\t\tmax scan coordinates :' num2str(max_coordinates(1:3)') '\n']);
         fprintf(1,['\t\tmax base coordinates :' num2str(round(max_base_coordinates(1:3)')) '\n']);
@@ -77,6 +85,14 @@ for iOverlay = overlayNum;
         if ~isempty(base2tal)
            min_tal_coords = round(scan2tal*min_base_coordinates);
            fprintf(1,['\t\tmin Talairach coordinates: ' num2str(min_tal_coords(1:3)') '\n']);
+        end
+        fprintf(1,'\n');
+
+        fprintf(1,['\t\tcenter-of-mass scan coordinates :' num2str(COM_coordinates(1:3)') '\n']);
+        fprintf(1,['\t\tcenter-of-mass base coordinates :' num2str(round(COM_base_coordinates(1:3)')) '\n']);
+        if ~isempty(base2tal)
+           COM_tal_coords = round(scan2tal*COM_base_coordinates);
+           fprintf(1,['\t\tcenter-of-mass Talairach coordinates: ' num2str(COM_tal_coords(1:3)') '\n']);
         end
         fprintf(1,'\n');
 
