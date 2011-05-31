67cb3a34411db4c8512522606fae382249ffde18
julien <julien@d7859a6d-3d6a-0410-b503-b92f2c4db295>
2011-05-31 10:23:12 +0000

    applyWarpOverlays.m: installs all overlays at once

--- mrLoadRet/Plugin/applyFslFnirtWarp/applyWarpOverlays.m
+++ mrLoadRet/Plugin/applyFslFnirtWarp/applyWarpOverlays.m
@@ -52,37 +52,36 @@ overlayNames = viewGet(thisView,'overlayNames');
 overlayNames = overlayNames(overlayList);
 overlaysNumber = length(overlayList);
 for iOverlay = 1:overlaysNumber
-   overlays{iOverlay} = viewGet(thisView,'overlay',overlayList(iOverlay));
-   if isfield(overlays{iOverlay},'alphaOverlay') && ~isempty(overlays{iOverlay}.alphaOverlay) && ~ismember(overlays{iOverlay}.alphaOverlay,overlayNames)
+   overlays(iOverlay) = viewGet(thisView,'overlay',overlayList(iOverlay));
+   if isfield(overlays(iOverlay),'alphaOverlay') && ~isempty(overlays(iOverlay).alphaOverlay) && ~ismember(overlays(iOverlay).alphaOverlay,overlayNames)
       overlaysNumber = overlaysNumber+1;
-      alphaOverlayName = overlays{iOverlay}.alphaOverlay;
+      alphaOverlayName = overlays(iOverlay).alphaOverlay;
       overlayNames = [overlayNames {alphaOverlayName}];
-      overlays{overlaysNumber} = viewGet(thisView,'overlay',viewGet(thisView,'overlaynum',alphaOverlayName));
-      overlays{iOverlay}.alphaOverlay = ['warp(' alphaOverlayName ') - ' warpCoefFilename];
+      overlays(overlaysNumber) = viewGet(thisView,'overlay',viewGet(thisView,'overlaynum',alphaOverlayName));
+      overlays(iOverlay).alphaOverlay = ['warp(' alphaOverlayName ') - ' warpCoefFilename];
       disp(['(applyWarpOverlays) Alpha overlay ''' alphaOverlayName ''' will also be warped']);
    end
 end
 
 for iOverlay = 1:overlaysNumber
-   newOverlay = overlays{iOverlay};
    overlayIndices = false(1,length(scanList));
    for iScan = 1:length(scanList)
-      overlayIndices(iScan) = ~isempty(overlays{iOverlay}.data(scanList));
+      overlayIndices(iScan) = ~isempty(overlays(iOverlay).data(scanList));
    end
    if any(overlayIndices)
-      newOverlay.data = cell(1,length(overlays{iOverlay}.data));
       %transform the non empty overlay in a 4D matrix
-      data = permute(reshape(cell2mat(overlays{iOverlay}.data(scanList(overlayIndices))),[scanDims([1 2]) sum(overlayIndices) scanDims(3)]), [1 2 4 3]);
+      data = permute(reshape(cell2mat(overlays(iOverlay).data(scanList(overlayIndices))),[scanDims([1 2]) sum(overlayIndices) scanDims(3)]), [1 2 4 3]);
       warpedData = applyFSLWarp(data, [warpCoefPathname warpCoefFilename], 'tempInput.img', scanHdr, interpMethod);
-      newOverlay.data(scanList(overlayIndices)) = mat2cell(warpedData,scanDims(1),scanDims(2),scanDims(3),ones(1,sum(overlayIndices)));
-      newOverlay.name = ['warp(' overlays{iOverlay}.name ') - ' warpCoefFilename];
+      overlays(iOverlay).data = cell(1,length(overlays(iOverlay).data));
+      overlays(iOverlay).data(scanList(overlayIndices)) = mat2cell(warpedData,scanDims(1),scanDims(2),scanDims(3),ones(1,sum(overlayIndices)));
+      overlays(iOverlay).name = ['warp(' overlays(iOverlay).name ') - ' warpCoefFilename];
       %this used to be helpful when it wasn't possible to readily identify which overlays are clipped
 % % % %       %reset the clip values. This is due to the fact that warped data can go beyond the min and max of the original data (even when using nearest-neighboor, probably due to a bug of FSL
-% % % %       newOverlay.clip = [min(warpedData(:)) max(warpedData(:))];
-      thisView = viewSet(thisView,'newoverlay',newOverlay);
+% % % %       overlays.clip = [min(warpedData(:)) max(warpedData(:))];
 
    end
 end
+thisView = viewSet(thisView,'newoverlay',overlays);
 warning('on','MATLAB:mat2cell:TrailingUnityVectorArgRemoved');
 
 refreshMLRDisplay(thisView.viewNum);
