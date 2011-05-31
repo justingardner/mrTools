5dc588abefb102586820966b1cf1964224c6dbb8
julien <julien@d7859a6d-3d6a-0410-b503-b92f2c4db295>
2011-05-31 08:46:29 +0000

    copyOverlay.m,pasteOverlay.m: automatically copy/past overlays that are alpha overlays of another overlay

--- branches/julien/mrLoadRet/Plugin/flexibleCopyPasteOverlays/pasteOverlay.m
+++ branches/julien/mrLoadRet/Plugin/flexibleCopyPasteOverlays/pasteOverlay.m
@@ -1,4 +1,4 @@
-function pasteOverlay(thisView, overlay)
+function pasteOverlay(thisView, overlays)
 % pasteOverlay.m
 %
 %        $Id$
@@ -18,38 +18,44 @@ if isempty(scanList)
    mrWarnDlg('(pasteOverlay) Could not paste overlay because the group is empty (no scan)' );
    return;
 end
-while length(scanList)~= length(overlay(1).data)
+while length(scanList)~= length(overlays(1).data)
    scanList=selectInList(thisView,'scans');
+   if isempty(scanList)
+     return;
+   end
 end
 curGroupName = viewGet(thisView,'groupName');
 curGroupNum =  viewGet(thisView,'groupNum',curGroupName);
 
-for iOverlay = 1:length(overlay)
-   [check thisOverlay] = isoverlay(overlay(iOverlay));
+for iOverlay = 1:length(overlays)
+   [check thisOverlay] = isoverlay(overlays(iOverlay));
    if ~check
        mrErrorDlg('(pasteOverlay) Cannot paste. Clipboard does not contain a valid overlay. Use Edit -> Overlay -> Copy Overlay.')
    end
    fromGroupNum = viewGet(thisView,'groupNum',thisOverlay.groupName);
    scan2scan = viewGet(thisView,'scan2scan',1,curGroupNum,1,fromGroupNum);
    scandims = viewGet(thisView, 'scandims', 1);
-   overlay(iOverlay).data = cell(1,nScans);
+   overlays(iOverlay).data = cell(1,nScans);
    if ~all(all((scan2scan - eye(4))<1e-6)) %check if we're in the scan space
       %transform values in current scan space
       [Ycoords,Xcoords,Zcoords] = meshgrid(1:scandims(2),1:scandims(1),1:scandims(3));
       for iScan = 1:length(scanList)
          fprintf(1,['Overlay ' num2str(iOverlay) ', Scan ' num2str(iScan) '\n']);
-         overlay(iOverlay).data{scanList(iScan)} = getNewSpaceOverlay(thisOverlay.data{iScan}, scan2scan,Xcoords,Ycoords,Zcoords);
+         overlays(iOverlay).data{scanList(iScan)} = getNewSpaceOverlay(thisOverlay.data{iScan}, scan2scan,Xcoords,Ycoords,Zcoords);
       end
    else
-      overlay(iOverlay).data(scanList) = thisOverlay.data;
+      overlays(iOverlay).data(scanList) = thisOverlay.data;
    end
-   overlay(iOverlay).groupName =  curGroupName;
-   overlay(iOverlay).name = [overlay(iOverlay).name ' (from ' thisOverlay.groupName ')'];
+   overlays(iOverlay).groupName =  curGroupName;
+   overlays(iOverlay).name = [overlays(iOverlay).name ' (from ' thisOverlay.groupName ')'];
+   if isfield(overlays(iOverlay),'alphaOverlay') && ~isempty(overlays(iOverlay).alphaOverlay)
+     overlays(iOverlay).alphaOverlay = [overlays(iOverlay).alphaOverlay ' (from ' thisOverlay.groupName ')'];
+   end
    %remove any specific reconcile function and params just in case, this overlay has to emancipate 
-   overlay(iOverlay).reconcileFunction = 'defaultReconcileParams';
-   overlay(iOverlay).params = [];
+   overlays(iOverlay).reconcileFunction = 'defaultReconcileParams';
+   overlays(iOverlay).params = [];
 
-   thisView = viewSet(thisView,'newOverlay',overlay(iOverlay));
 end
+thisView = viewSet(thisView,'newOverlay',overlays);
 
 
