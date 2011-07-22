8e652cadc66c69cbbfc3df26469223c7358f260d
julien <julien@d7859a6d-3d6a-0410-b503-b92f2c4db295>
2011-07-22 12:17:18 +0000

    improvedGUIPlugin.m, mrSliceExport.m: added option to export flat maps at different cortical depths

--- mrLoadRet/Plugin/improvedGUI/mrSliceExport.m
+++ mrLoadRet/Plugin/improvedGUI/mrSliceExport.m
@@ -1,10 +1,10 @@
 % mrSliceExport - output series of images across all slices
 %
-%      usage: [  ] = mrSliceExportTest( <v>, <imrange>, <sliceList>, <fname>, <nRows>, <nColumns>, <scanList>) )
+%      usage: [  ] = mrSliceExportTest( <thisView>, <imrange>, <sliceList>, <fname>, <nRows>, <nColumns>, <scanList>) )
 %         by: denis schluppeck, modified by julien besle for integration in mrLoadRet
 %       date: 2007-11-22
 %        $Id: mrSliceExportCor.m 583 2010-10-15 09:54:53Z lpzjb $:
-%     inputs:       v: mrLoadRet View structure
+%     inputs:       thisView: mrLoadRet View structure
 %             imrange: relative dimensions of the display to export between 0 and 1 ([minX maxX minY maxY] form left to right and top to bottom)
 %           sliceList:
 %               fname: base name of the tif file to export to
@@ -16,7 +16,7 @@
 %
 %        e.g:
 %
-function [  ]=mrSliceExport( v, imrange, sliceList, fname, nRows, scanList)
+function [  ]=mrSliceExport( thisView, imrange, sliceList, fname, nRows, scanList)
 
 mrGlobals
 
@@ -27,24 +27,34 @@ end
 imrange = min(imrange,1);
 imrange = max(imrange,0);
   
-if ieNotDefined('v')
-  v = viewGet([],'view',viewGet([],'viewnums'));
+if ieNotDefined('thisView')
+  thisView = viewGet([],'view',viewGet([],'viewnums'));
   disp('... using current view')
-  %disp(v)
+  %disp(thisView)
 end
 
 if ieNotDefined('fname')
    fname = 'montage00.tif';
 end
 
-if ieNotDefined('sliceList')
-   sliceList = viewGet(v,'nslices'):-1:1;% 1:nSlices;
+baseType=viewGet(thisView,'basetype');
+switch(baseType)
+  case 0
+    if ieNotDefined('sliceList')
+      sliceList = viewGet(thisView,'nslices'):-1:1;% 1:nSlices;
+    end
+  case{1,2}
+    if ieNotDefined('sliceList')
+     %if sliceList is empty and we have a flat or a surface base, we just want to export the image in the current settings of depths
+      baseType= -1;
+      sliceList=1;
+    end
 end
 
 if ieNotDefined('scanList')
-   scanList = viewGet(v,'curscan');
+   scanList = viewGet(thisView,'curscan');
 else
-   nScans = viewGet(v,'nscans');
+   nScans = viewGet(thisView,'nscans');
    if any(scanList>nScans)
       mrWarnDlg(['There are only ' num2str(nScans) ' scans']);
       return;
@@ -55,37 +65,37 @@ nSlices = length(sliceList);
 if ieNotDefined('nRows')
   nRows = 2;
 end
-nColumns= ceil(nSlices/nRows);
+nColumns= ceil(length(scanList)*nSlices/nRows);
 
 
 
 
-if isempty(viewGet(v,'curanalysis'))
+if isempty(viewGet(thisView,'curanalysis'))
   % load in current analaysis
-  v = loadAnalysis(v, 'corAnal');
+  thisView = loadAnalysis(thisView, 'corAnal');
 end
 
-if isempty(viewGet(v,'curbase'))
+if isempty(viewGet(thisView,'curbase'))
   % load anatomy
-  v = loadAnat(v);
+  thisView = loadAnat(thisView);
 end
 
 % don't want to hardcode any parameters here, but you could roll your own
 % e.g.
-% v = viewSet(v, 'curoverlay', 1); % co is 1
-% v = viewSet(v,'overlaymin', 0.35);
-% v = viewSet(v,'overlaymax', 1.0); 
+% thisView = viewSet(thisView, 'curoverlay', 1); % co is 1
+% thisView = viewSet(thisView,'overlaymin', 0.35);
+% thisView = viewSet(thisView,'overlaymax', 1.0); 
 
-viewNum = viewGet(v,'viewnum');
-curGroup = viewGet(v,'curgroup');
-curOverlay = viewGet(v,'currentoverlay');
+viewNum = viewGet(thisView,'viewnum');
+curGroup = viewGet(thisView,'curgroup');
+curOverlay = viewGet(thisView,'currentoverlay');
 
 % for alpha transparency, have to go via mlrGuiSet -- is this bad?
-% mlrGuiSet(v,'alpha', 0.75);
+% mlrGuiSet(thisView,'alpha', 0.75);
 
 % loop over slices and get images
 
-basedims = viewGet(v,'basedims');
+basedims = viewGet(thisView,'basedims');
 newx = ceil(imrange(1:2)*(basedims(1)-1))+1; % indeces are from 1 to basedims
 newy =  ceil(imrange(3:4)*(basedims(2)-1))+1;
 
@@ -97,27 +107,61 @@ fname = [fname extension];
 % check that the directory exists
 if ~exist(pathname, 'dir')
   mkdir(pathname)
-  disp(['(mrSliceExport) Created directory ' path]);
+  disp(['(mrSliceExport) Created directory ' pathname]);
 else
   % rm all the im*.tif files in Etc
   unix(['rm ' pathname '/im*.tif']);
 end
 
+curscan = viewGet(thisView,'curscan');
+switch baseType
+  case 0
+    curSlice = viewGet(thisView,'curslice');
+  otherwise
+    minDepth = viewGet(thisView,'corticalmindepth');
+    maxDepth = viewGet(thisView,'corticalmaxdepth');
+end
+      
 % loop through scans and slices.
 for iScan = scanList
-   v=viewSet(v, 'curscan', iScan);
-   for iSlice = sliceList
-      v=viewSet(v, 'curslice', iSlice);
+  thisView=viewSet(thisView, 'curscan', iScan);
+  for iSlice = sliceList
+    switch baseType
+      case 0
+        %set the slice
+        thisView=viewSet(thisView, 'curslice', iSlice);
+        disp(sprintf('rendering scan %d slice %d .',iScan,iSlice));
+        imageFilename = sprintf('%s/im_%02d_%02d.tif',pathname,iScan, iSlice);
+      case {1,2}
+        %set the cortical depth
+        mlrGuiSet(viewNum,'corticalMinDepth',iSlice);
+        mlrGuiSet(viewNum,'corticalMaxDepth',iSlice);
+        disp(sprintf('rendering scan %d depth %f .',iScan,iSlice));
+        imageFilename = sprintf('%s/im_%02d_%f.tif',pathname,iScan, iSlice);
+      case -1
+        %do nothing
+        disp(sprintf('rendering scan %d .',iScan));
+        imageFilename = sprintf('%s/im_%02d_%f_%f.tif',pathname,iScan,minDepth, maxDepth);
+    end
 
-      disp(sprintf('rendering scan %d slice %d .',iScan,iSlice));
 
-      img = refreshMLRDisplay(viewNum);
-      reducedim = img(newy(1):newy(2),newx(1):newx(2),:);
+    img = refreshMLRDisplay(viewNum);
+    reducedim = img(newy(1):newy(2),newx(1):newx(2),:);
 
-      imwrite(reducedim, sprintf('%s/im_%02d_%02d.tif',pathname,iScan, iSlice), 'tif');
-   end
+    imwrite(reducedim, imageFilename, 'tif');
+  end
 end
 
+%re-instal original scan and slice/depth
+thisView=viewSet(thisView, 'curscan', curscan);
+switch baseType
+  case 0
+    viewSet(thisView,'curslice',curSlice);
+  otherwise
+    mlrGuiSet(viewNum,'corticalMinDepth',minDepth);
+    mlrGuiSet(viewNum,'corticalMaxDepth',maxDepth);
+end
+refreshMLRDisplay(viewNum);
 
 % and create a montage for a final tif image (this is taken from mrCreateMontage by D. Schluppeck)
 dirOutput = dir([pathname '/im*.tif']);
