ea3beec6356a2805b8b5b81e5aba7b39a60254eb
justin <justin@d7859a6d-3d6a-0410-b503-b92f2c4db295>
2011-08-15 09:52:03 +0000

    mlrLoadImage/Header can now handle sdt files (need to put that in database somewhere). These will be used by mlrVol

---
+++ trunk/mrUtilities/File/mlrImage/mlrLoadImage.m
@@ -0,0 +1,36 @@
+% mlrLoadImage.m
+%
+%        $Id:$ 
+%      usage: mlrLoadImage(filename)
+%         by: justin gardner
+%       date: 08/15/11
+%    purpose: Loads an mlr image (this is usually a nifti file, but we can expand this
+%             to load any kind of image).
+%
+function [data header] = mlrLoadImage(filename)
+
+% check arguments
+if ~any(nargin == [1])
+  help mlrLoadImage
+  return
+end
+
+% set filenames
+if isempty(getext(filename))
+  hdrFilename = setext(filename,mrGetPref('niftiFileExtension'));
+else
+  hdrFilename = filename;
+end
+
+% load the header first
+header = mlrLoadImageHeader(filename);
+
+% load the data
+switch lower(getext(filename))
+ case {'hdr','img','nii'}
+  data = cbiReadNifti(filename);
+ case {'sdt','spr','edt','epr'}
+  data = readsdt(filename);
+  data = data.data;
+end
+
--- trunk/mrUtilities/File/mlrImage/mlrLoadImageHeader.m
+++ trunk/mrUtilities/File/mlrImage/mlrLoadImageHeader.m
@@ -22,7 +22,6 @@ if isempty(getext(filename))
 else
   hdrFilename = filename;
 end
-matlabFilename = setext(filename,'mat');
 
 %check for file
 if ~isfile(hdrFilename) 
@@ -30,11 +29,87 @@ if ~isfile(hdrFilename)
   return
 end
 
+% set inital fields of header
+header.filename = filename;
+header.ext = getext(filename);
+
+% set defaults
+header.qform_code = 0;
+header.qform44 = eye(4);
+header.sform_code = 0;
+header.sform44 = eye(4);
+header.permuationMatrix = eye(4);
+header.nDim = 0;
+header.dim = [];
+header.pixdim = [];
+
+switch lower(getext(filename))
+  case {'hdr','nii','img'}
+    header = mlrLoadImageHeaderNifti(filename,header);
+  case {'sdt','spr','edt','epr'}
+    header = mlrLoadImageHeaderSDT(filename,header);
+ otherwise
+    disp(sprintf('(mlrLoadImageHeader) Unknown image header type: %s',getext(filename)));
+    return
+end
+
+% load the associated matlab header
+header.matHeader = loadAssociatedMatlabHeader(filename);
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%    mlrLoadImageHeaderSDT    %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function header = mlrLoadImageHeaderSDT(filename,header)
+
+% load sdt
+d = readsdt(filename,0);
+if isempty(d),return,end
+
+% set fileds
+if any(strcmp(getext(filename),{'sdt','spr'}))
+  header.type = 'sdt';
+else
+  header.type = 'edt';
+end
+header.hdr = d;
+
+% set fields
+header.nDim = length(d.dim);
+header.dim = d.dim;
+if isfield(d,'interval')
+  header.pixdim  = d.interval(1:3)*10;
+elseif isfield(d,'fov')
+  header.pixdim = d.fov(1:3)./d.dim(1:3)*10
+end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%    mlrLoadImageHeaderNifti    %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function header = mlrLoadImageHeaderNifti(filename,header)
+
 % read the nifti header
 nifti = cbiReadNiftiHeader(hdrFilename);
 
-% check for matlab file
-header = [];
+% set some info
+header.type = 'nifti';
+header.hdr = nifti;
+
+% set other fields
+header.qform_code = header.hdr.qform_code;
+header.qform44 = header.hdr.qform44;
+header.sform_code = header.hdr.sform_code;
+header.sform44 = header.hdr.sform44;
+header.pixdim = header.hdr.pixdim;
+header.dim = header.hdr.dim;
+header.permutationMatrix = getPermutationMatrix(nifti);
+ 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%    loadAssociatedMatlabHeader    %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function matHeader = loadAssociatedMatlabHeader(filename)
+
+matHeader = [];
+matlabFilename = setext(filename,'mat');
 if isfile(matlabFilename)
   % load the header
   matHeader = load(matlabFilename);
@@ -49,32 +124,3 @@ if isfile(matlabFilename)
     end
   end
 end
-
-% no already created header, so make a new one
-if isempty(header)
-  header.data = [];
-  header.hdr = nifti;
-  header.name = filename;
-  header.permutationMatrix = getPermutationMatrix(nifti);
-  [tf header] = isbase(header);
-  if ~tf
-    disp(sprintf('(mlrLoadImageHeader) Could not init header'));
-    return
-  end
-end
-
-% set the nifti field
-header.hdr = nifti;
-
-% set other fields
-header.qform_code = header.hdr.qform_code;
-header.qform44 = header.hdr.qform44;
-header.sform_code = header.hdr.sform_code;
-header.sform44 = header.hdr.sform44;
-header.pixdim = header.hdr.pixdim;
-header.dim = header.hdr.dim;
-    
-
-
-
-
