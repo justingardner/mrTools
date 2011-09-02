a5f987dbec3410bb6e1bba210b448b5280b87f03
justin <justin@d7859a6d-3d6a-0410-b503-b92f2c4db295>
2011-09-02 13:39:19 +0000

    fid2xform now works close to correct for epi images. A few display bug fixes for mlrVol

--- mrUtilities/File/Varian/fid2xform.m
+++ mrUtilities/File/Varian/fid2xform.m
@@ -222,8 +222,14 @@ swapDim2 =[0 0 1 0;0 1 0 0;1 0 0 0;0 0 0 1];
 % for now - will need to update all of our transforms ;-(.... -j.
 swapDim2 =[0 0 -1 0;0 1 0 0;-1 0 0 0;0 0 0 1];
 
+if info.isepi
+  epiSwap = [-1 0 0 0;0 -1 0 0;0 0 1 0;0 0 0 1];
+else
+  epiSwap = eye(4);
+end
+
 % now create the final shifted rotation matrix
-xform = swapDim2*rotmat*swapDim*offset*diag(voxspacing)*originOffset;
+xform = swapDim2*rotmat*swapDim*offset*diag(voxspacing)*epiSwap*originOffset;
 
 % testing rotmat
 %rotmatpsi = euler2rotmatrix(procpar.psi,0,0);
