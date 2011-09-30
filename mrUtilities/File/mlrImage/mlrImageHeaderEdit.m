ebe5fdcf4574cc24d30e30790d7608473d836936
justin <justin@d7859a6d-3d6a-0410-b503-b92f2c4db295>
2011-09-30 08:20:21 +0000

    Adding ability to clear xforms

--- mrUtilities/File/mlrImage/mlrImageHeaderEdit.m
+++ mrUtilities/File/mlrImage/mlrImageHeaderEdit.m
@@ -38,13 +38,13 @@ paramsInfo = {...
     {'dim',h.dim,'editable=0','Size of data'}...
     {'pixdim',h.pixdim,'Size of data in mm. For volume dimension should be time in s'}...
     {'qform',qform,'Specifies the xform from the image coordinates to the magnet coordinates. Same as a nifti header qform when qform_code = 1. Will be empty if no such information exists'}...
-    {'qformSet',{'Set qform','Identity','sform','vol2mag','vol2tal'},'callback',@setXform,'callbackArg','qform','passParams=1','Set the qform'}...
+    {'qformSet',{'Set qform','Identity','sform','vol2mag','vol2tal','clear'},'callback',@setXform,'callbackArg','qform','passParams=1','Set the qform'}...
     {'sform',sform,'Specifies the xform from the image coordinates to the magnet coordinates for the canonical volume. This is the same as a nifti header qform when sform_code = 1. Will be empty if no such info exists.'}...
-    {'sformSet',{'Set sform','Identity','qform','vol2mag','vol2tal'},'callback',@setXform,'callbackArg','sform','passParams=1','Set the sform'}...
+    {'sformSet',{'Set sform','Identity','qform','vol2mag','vol2tal','clear'},'callback',@setXform,'callbackArg','sform','passParams=1','Set the sform'}...
     {'vol2mag',vol2mag,'This is the xform from the canonical volume coordinates to the magnet coordinates for the canonical. This is useful for checking which volume the image was aligned to (i.e. where it got the sform from). It is also useful for when trying to compute the talairach coordinates. Will be empty if no such info exists.'}...
-    {'vol2magSet',{'Set vol2mag','Identity','qform','sform','vol2tal'},'callback',@setXform,'callbackArg','vol2mag','passParams=1','Set the vol2mag'}...
+    {'vol2magSet',{'Set vol2mag','Identity','qform','sform','vol2tal','clear'},'callback',@setXform,'callbackArg','vol2mag','passParams=1','Set the vol2mag'}...
     {'vol2tal',vol2tal,'This is the xform from the canonical volume coordinates to the talairach coordinates for the canonical. Note that we keep this matrix since it allows us to go back and change the talairach points on the canonical volume and then easily redo the tal xforma for the image by simply replacing this matrix. To compute the xform from the image coordinates to talairach coordiates, you can do: img2tal = vol2tal * inv(vol2mag) * sform * shiftOriginXform; Will be empty if no such info exists.'}...
-    {'vol2talSet',{'Set vol2tal','Identity','qform','sform','vol2mag'},'callback',@setXform,'callbackArg','vol2tal','passParams=1','Set the vol2tal'}...
+    {'vol2talSet',{'Set vol2tal','Identity','qform','sform','vol2mag','clear'},'callback',@setXform,'callbackArg','vol2tal','passParams=1','Set the vol2tal'}...
 	     };
 
 
@@ -64,15 +64,23 @@ h.pixdim = params.pixdim;
 % now do xform matrices 
 if all(~isnan(params.qform(:)))
   h.qform = params.qform;
+else
+  h.qform = [];
 end
 if all(~isnan(params.sform(:)))
   h.sform = params.sform;
+else
+  h.sform = [];
 end
 if all(~isnan(params.vol2mag(:)))
   h.vol2mag = params.vol2mag;
+else
+  h.vol2mag = [];
 end
 if all(~isnan(params.vol2tal(:)))
   h.vol2tal = params.vol2tal;
+else
+  h.vol2tal = [];
 end
 
 % validate header
@@ -107,6 +115,8 @@ elseif strcmp(setTo,'vol2tal')
   xform = params.vol2tal;
 elseif strcmp(setTo,'qform')
   xform = params.qform;
+elseif strcmp(setTo,'clear')
+  xform = nan(4,4);
 end
 
 
