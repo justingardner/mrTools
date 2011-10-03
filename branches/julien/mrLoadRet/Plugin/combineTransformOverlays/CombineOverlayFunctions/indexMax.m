2ffc2290f1f8fd703e92afd4f69ec4bbf15680c9
julien <julien@d7859a6d-3d6a-0410-b503-b92f2c4db295>
2011-10-03 09:51:54 +0000

    indexMax.m: generalized function to arrays

--- branches/julien/mrLoadRet/Plugin/combineTransformOverlays/CombineOverlayFunctions/indexMax.m
+++ branches/julien/mrLoadRet/Plugin/combineTransformOverlays/CombineOverlayFunctions/indexMax.m
@@ -2,10 +2,26 @@
 %
 %        $Id$
 %
-%   example: index = max(x,y,z) returns 2 if y>x and y>z
+%   example: index = max(x,y,z) returns [2 3] if y(1)>x(1), y(1)>z(1) and z(2)>x(2), z(2)>y(2)
+%           
 
 function index = indexMax(varargin)
 
-numbers = cell2mat(varargin);
-[dump,index] = max(numbers);
+
+nDims = length(size(varargin{1}));
+array = varargin{1};
+for i = 2:nargin
+  %first check that all inputs have the same size
+  if ~isequal(size(varargin{i}),size(varargin{1}))
+    error('All inputs must have the same size')
+  else
+    %concatenate
+    array=cat(nDims+1,array,varargin{i});
+  end
+end
+
+[dump,index] = max(array,[],nDims+1);
+
+%put NaNs if all values are NaNs
+index(all(isnan(array),nDims+1))=NaN;
 
