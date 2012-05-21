5d1499d975291e95cfdcfb3d2c55dd6c8fcb76e3
julien <julien@d7859a6d-3d6a-0410-b503-b92f2c4db295>
2012-05-21 14:42:51 +0000

    corrected small differences between some files between nottingham and julien branches (branch nottingham)

--- branches/nottingham/mrUtilities/File/Nifti/cbiReadNifti.m
+++ branches/nottingham/mrUtilities/File/Nifti/cbiReadNifti.m
@@ -264,26 +264,20 @@ end
 fclose(fPtr);
 
 % Scaling
-if (strcmp(prec,'double') || strcmp(prec,'single'))  
-
+if (strcmp(prec,'double')||strcmp(prec,'single'))
   if hdr.scl_slope~=0
     % NaN handling for int16 data
     if (hdr.datatype==4 & short_nan) 
       data(data==-32768)=NaN;
     end
-    % apply slope and intercept
     data=data.*hdr.scl_slope+hdr.scl_inter;
   end
-  
   % convert to single precision down here--probably better to read
   % in the data as a single, but that requires changing a lot of
   % code above -jg.
-  
-  % if prec asked for is SINGLE, then convert, otherwise leave as DOUBLE
   if strcmp(prec,'single')
     data=single(data);
   end
-  
 end
 
 % this part is payback for a cheap hack, I did above. Rewrite
