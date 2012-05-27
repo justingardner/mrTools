646105e2bac999bb9d3789d0d69734ee487cd7fd
julien <julien@d7859a6d-3d6a-0410-b503-b92f2c4db295>
2012-05-27 06:06:23 +0000

    Undid previous change (r2565) in mrSaveView, saveSform and mrUpdateNiftiHdr for backward compatibility (branch julien)

--- mrLoadRet/GUI/mrSaveView.m
+++ mrLoadRet/GUI/mrSaveView.m
@@ -14,17 +14,32 @@ if ~isempty(fig)
   mrSetFigLoc('mrLoadRetGUI',get(fig,'Position'));
 end
 
-homeDir = viewGet(v,'homeDir');
+%--------(This is obsolete, but kept for backward compatibility)
+% remember GUI settings 
+viewSettings.curBase = viewGet(v,'curBase');
+viewSettings.rotate = viewGet(v,'rotate');
+viewSettings.curScan = viewGet(v,'curScan');
+viewSettings.curSlice = viewGet(v,'curSlice');
+viewSettings.curGroup = viewGet(v,'curGroup');
+viewSettings.sliceOrientation = viewGet(v,'sliceOrientation');
+viewSettings.overlayMin = viewGet(v,'overlayMin');
+viewSettings.overlayMax = viewGet(v,'overlayMax');
+viewSettings.alpha = viewGet(v,'alpha');
+viewSettings.showROIs = viewGet(v,'showROIs');
+viewSettings.labelROIs = viewGet(v,'labelROIs');
+viewSettings.roiGroup = viewGet(v,'roiGroupNames');
+%--------(This is obsolete, but kept for backward compatibility)
 
+homeDir = viewGet(v,'homeDir');
 try
   disppercent(-inf,sprintf('(mrSaveView) Saving %s/mrLastView',homeDir));
         % save the view in the current directory
   view = v;
   if getfield(whos('view'),'bytes')<2e9
-    eval(sprintf('save %s view -V6;',fullfile(homeDir,'mrLastView')));
+    eval(sprintf('save %s view viewSettings -V6;',fullfile(homeDir,'mrLastView')));
   else
     mrWarnDlg('(mrSaveView) Variable view is more than 2Gb, using option -v7.3 to save');
-    eval(sprintf('save %s view -v7.3;',fullfile(homeDir,'mrLastView')));
+    eval(sprintf('save %s view viewSettings -v7.3;',fullfile(homeDir,'mrLastView')));
   end
   % save .mrDefaults in the home directory
   disppercent(inf);
