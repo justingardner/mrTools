55f1d873724d68de2b93553135f082152c66c165
justin <justin@d7859a6d-3d6a-0410-b503-b92f2c4db295>
2012-06-21 23:45:06 +0000

    adding missing r2d function

--- trunk/mrUtilities/File/mlrImage/mlrImageGetAxisLabels.m
+++ trunk/mrUtilities/File/mlrImage/mlrImageGetAxisLabels.m
@@ -94,3 +94,22 @@ retval.mapping = axisMapping;
 retval.reverseMapping = axisReverseMapping;
 retval.dir = axisDir;
 retval.orient = orient;
+
+% convert radians to degrees
+%
+% usage: degrees = r2d(radians);
+function degrees = r2d(angle)
+
+degrees = (angle/(2*pi))*360;
+
+% if larger than 360 degrees then subtract
+% 360 degrees
+while (sum(degrees>360))
+  degrees = degrees - (degrees>360)*360;
+end
+
+% if less than 360 degreees then add 
+% 360 degrees
+while (sum(degrees<-360))
+  degrees = degrees + (degrees<-360)*360;
+end
