8d13fccbec89bbd08c7b76599c1e333e3686fb1e
julien <julien@d7859a6d-3d6a-0410-b503-b92f2c4db295>
2012-08-21 16:47:45 +0000

    Merged revisions 2617 via svnmerge from https://cbi.nyu.edu/svn/mrTools/branches/nottingham
    ................
      r2617 | julien | 2012-08-21 17:45:55 +0100 (Tue, 21 Aug 2012) | 9 lines
      
      Merged revisions 2612 via svnmerge from 
      https://cbi.nyu.edu/svn/mrTools/trunk
      
      ........
        r2612 | justin | 2012-08-15 02:48:57 +0100 (Wed, 15 Aug 2012) | 1 line
        
        Small change set that checks for installed toolboxes and gives you a one time warning if you don't have them installed. Also, mrLoadRetVersion updated to the latest version of matlab.
      ........
    ................

--- mrLoadRet/mrLoadRetVersion.m
+++ mrLoadRet/mrLoadRetVersion.m
@@ -1,6 +1,12 @@
-function [ver, expectedMatlabVersion] = mrLoadRetVersion
+function [ver, expectedMatlabVersion,expectedToolboxNames] = mrLoadRetVersion
 %     $Id$	
 
 ver = 4.5;
-expectedMatlabVersion = [7.2 7.3 7.4 7.5 7.6 7.7 7.8 7.9 7.10];  % Change this after testing Matlab upgrades
+
+% Change this after testing Matlab upgrades
+expectedMatlabVersion = [7.2 7.3 7.4 7.5 7.6 7.7 7.8 7.9 7.10 7.11 7.12 7.13 7.14];
+
+% expected toolbox
+expectedToolboxNames = {'Statistics Toolbox','Image Processing Toolbox','Optimization Toolbox'};
+
 
