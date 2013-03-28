ea60cb956431fd73872f5c3ad6bd1743c103dd89
julien <julien@d7859a6d-3d6a-0410-b503-b92f2c4db295>
2013-03-28 17:59:55 +0000

    getGlmEVParamsGUI.m, getGlmScanParamsGUI.m (branch nottingham): moved timing parameters from scan to design parameter menu

--- mrLoadRet/Plugin/improvedGUI/newGlmAnalysis/getGlmScanParamsGUI.m
+++ mrLoadRet/Plugin/improvedGUI/newGlmAnalysis/getGlmScanParamsGUI.m
@@ -27,7 +27,6 @@ while keepAsking
   % user which variable they want to do the anlysis on
   for iScan = params.scanNum
    % get scan info and description
-    framePeriod = viewGet(thisView,'framePeriod',iScan,groupNum);
     if ~isfield(scanParams{iScan},'scan')
        scanParams{iScan}.scan = sprintf('%i: %s',iScan,viewGet(thisView,'description',iScan,groupNum));
     end
@@ -40,15 +39,6 @@ while keepAsking
     if ~isfield(scanParams{iScan},'subsetBox') || ~strcmp(params.analysisVolume,'Subset box')
        scanDims = viewGet(thisView,'dims',iScan,groupNum);
        scanParams{iScan}.subsetBox = ['[1 ' num2str(scanDims(1)) ';1 ' num2str(scanDims(2)) ';1 ' num2str(scanDims(3)) ']'];
-    end
-    if ~isfield(scanParams{iScan},'designSupersampling') || isempty(scanParams{iScan}.designSupersampling)
-       scanParams{iScan}.designSupersampling = 1;
-    end
-    if ~isfield(scanParams{iScan},'estimationSupersampling') || isempty(scanParams{iScan}.estimationSupersampling)
-       scanParams{iScan}.estimationSupersampling = 1;
-    end
-    if fieldIsNotDefined(scanParams{iScan},'acquisitionDelay')
-       scanParams{iScan}.acquisitionDelay = framePeriod/2;
     end
     if ~isfield(scanParams{iScan},'sameForNextScans') || isempty(scanParams{iScan}.sameForNextScans)
        scanParams{iScan}.sameForNextScans = iScan == params.scanNum(1);
@@ -84,7 +74,6 @@ while keepAsking
     {'subsetBox', scanParams{iScan}.subsetBox, subsetBoxVisibleOption, subsetBoxEditableOption, subsetBoxHelp};
          };
 
-  % Timing parameters
     % make sure we are running on a set with a stimfile
     stimfile = viewGet(thisView,'stimfile',iScan,groupNum);
 
@@ -183,46 +172,6 @@ while keepAsking
           scanParams{iScan}.stimDuration = 'fromFile';
     end
 
-    if fieldIsNotDefined(scanParams{iScan},'stimDuration') 
-       scanParams{iScan}.stimDuration = num2str(framePeriod);
-    elseif isnumeric(scanParams{iScan}.stimDuration)
-      if ~strcmp(stimfile{1}.filetype,'eventtimes') %if the stimfile is not the mylog type
-        scanParams{iScan}.stimDuration = max(framePeriod,framePeriod*round(scanParams{iScan}.stimDuration/framePeriod)); %round the duration to a multiple of the frame period
-      end
-      scanParams{iScan}.stimDuration = num2str(scanParams{iScan}.stimDuration);
-    end
-
-    if strcmp(params.hrfModel,'hrfDeconvolution') 
-      canonicalVisibleOption = 'visible=0';
-      designSupersamplingOption = 'visible=0';
-      if strcmp(stimfile{1}.filetype,'eventtimes')
-        deconvolutionVisibleOption = 'visible=1';
-      else
-        scanParams{iScan}.estimationSupersampling=1;
-%         scanParams{iScan}.acquisitionSubsample=1;
-        deconvolutionVisibleOption = 'visible=0';
-      end
-      scanParams{iScan}.designSupersampling=scanParams{iScan}.estimationSupersampling;
-    else
-      scanParams{iScan}.estimationSupersampling=1;
-%       scanParams{iScan}.acquisitionSubsample=1;
-      canonicalVisibleOption = 'visible=1';
-      deconvolutionVisibleOption = 'visible=0';
-      if strcmp(stimfile{1}.filetype,'eventtimes')
-        designSupersamplingOption = 'visible=1';
-      else
-        designSupersamplingOption = 'visible=0';
-        scanParams{iScan}.designSupersampling=1;
-      end
-    end
-    
-    paramsInfo = [paramsInfo {...
-      {'estimationSupersampling',scanParams{iScan}.estimationSupersampling, deconvolutionVisibleOption,'incdec=[-1 1]','incdecType=plusMinus','minmax=[1 inf]','Supersampling factor of the deconvolution HRF model. Set this to more than one in order to resolve the estimated HDR at a temporal resolution that is less than the frame rate. This is only required if both the design and the acquisition have been designed to achieve subsample HDR estimation, and is not supported for MGL/AFNI stim files.'},...
-      {'acquisitionDelay',scanParams{iScan}.acquisitionDelay, sprintf('minmax=[0 %f]',framePeriod-0.05),'Time (in sec) at which the signal is actually acquired, on average across slices. This is normally set to half the frame period. If slice motion correction has been used, change to reference slice acquisition time. You might also need to change this value for sparse imaging designs.'},...
-      {'designSupersampling',scanParams{iScan}.designSupersampling, designSupersamplingOption,'incdec=[-1 1]','incdecType=plusMinus','minmax=[1 inf]','Supersampling factor of the GLM model. Set this to more than one in order to take into account stimulus times and duration that do not coincide with the frame period, or to round stimulus presentation times to the nearest sample onset. This is not supported for MGL/AFNI stim files.'},...
-      {'stimDuration', scanParams{iScan}.stimDuration, canonicalVisibleOption, 'Duration of stimulation events (in sec, resolution=0.05s) = duration the boxcar function that will be convolved with the HRF model. Use ''fromFile'' is the stimulus durations are specified in the (mylog) stim files. If using MGL/AFNI files, the duration should be a multiple of the frame period (TR).  If using Mylog files, the value of designSupersampling might automatically change to accomodate durations that are not multiples of the frame period).'},...
-       }];
-
     if isfield(d,'concatInfo') %in case of concatenation files
       % ask if hipass filter should be applied to the design matrix
       if isfield(d.concatInfo,'hipassfilter') && ~isempty(d.concatInfo.hipassfilter{1})
@@ -257,21 +206,12 @@ while keepAsking
        return
     end
     
-    if strcmp(params.hrfModel,'hrfDeconvolution')
-      tempParams.stimDuration = framePeriod/tempParams.estimationSupersampling;
-    elseif ~strcmp(tempParams.stimDuration,'fromFile')
-      tempParams.stimDuration= str2num(tempParams.stimDuration);
-    end
-    scanParams{iScan} = mrParamsCopyFields(tempParams,scanParams{iScan});
     
-    subsetBox = eval(scanParams{iScan}.subsetBox);
+    subsetBox = eval(tempParams.subsetBox);
     
     %various controls and variables settings
     if params.covCorrection && strcmp(params.analysisVolume,'Subset box') any(diff(subsetBox(1:2,:),1,2)<=params.covEstimationAreaSize)
       mrWarnDlg('(getScanParamsGUI) subset box size is too small for covariance estimation area size','Yes');
-      scanParams{iScan} = [];
-    elseif isempty(scanParams{iScan}.stimDuration) 
-      mrWarnDlg('(getScanParamsGUI) unknown stimDuration parameter','Yes');
       scanParams{iScan} = [];
     else
       keepAsking = 0;
@@ -279,18 +219,20 @@ while keepAsking
       % check if the varname is a cell array, then convert to a cell array
       % instead of a string this is so that the user can specify a variable
       % name like {{'varname'}}
-      if (isfield(scanParams{iScan},'varname') &&...
-         ischar(scanParams{iScan}.varname) && ...
-         (length(scanParams{iScan}.varname) > 1) && ...
-         (scanParams{iScan}.varname(1) == '{'))
-         scanParams{iScan}.varname = eval(scanParams{iScan}.varname);
+      if (isfield(tempParams,'varname') &&...
+         ischar(tempParams.varname) && ...
+         (length(tempParams.varname) > 1) && ...
+         (tempParams.varname(1) == '{'))
+         tempParams.varname = eval(tempParams.varname);
       end
 
+      scanParams{iScan} = mrParamsCopyFields(tempParams,scanParams{iScan});
+      
       % if sameForNextScans is set, copy all parameters into remaining scans and break out of loop
-      if isfield(scanParams{iScan},'sameForNextScans') && scanParams{iScan}.sameForNextScans
+      if isfield(tempParams,'sameForNextScans') && tempParams.sameForNextScans
          for jScan = params.scanNum(find(params.scanNum>iScan,1,'first'):end)
             % set the other scans params to the same as this one
-            scanParams{jScan} = scanParams{iScan};
+            scanParams{jScan} = mrParamsCopyFields(tempParams,scanParams{jScan});
             % change the description and scan fields appropriately for this scan num
             scanParams{jScan}.scan = viewGet(thisView,'description',jScan,groupNum);
             description = scanParams{iScan}.description;
@@ -319,17 +261,6 @@ while keepAsking
     end
   end
   
-  %here check if different estimation super-sampling for different scans because incompatible with statistics
-  if strcmp(params.hrfModel,'hrfDeconvolution') && (params.computeTtests || params.numberFtests)
-    estimationSupersampling = scanParams{params.scanNum(1)}.estimationSupersampling;
-    for iScan = params.scanNum(2:end)
-      if estimationSupersampling~=scanParams{iScan}.estimationSupersampling
-        keepAsking=1;
-        mrWarnDlg('(getScanParamsGUI) Cannot perform statistics on deconvolution weights if estimation supersampling varies between scans','Yes');
-        break;
-      end
-    end
-  end
   %if analysis on whole volume, need to set subset box to scan dimensions 
   if ~strcmp(params.analysisVolume,'Subset box')
     for iScan = params.scanNum(1:end)
@@ -337,9 +268,7 @@ while keepAsking
       scanParams{iScan}.subsetBox = ['[1 ' num2str(scanDims(1)) ';1 ' num2str(scanDims(2)) ';1 ' num2str(scanDims(3)) ']'];
     end
   end
-%   if iScan>params.scanNum(1)
-%     scanParams{params.scanNum(1)}.sameForNextScans = 0;
-%   end
+  
 end
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
