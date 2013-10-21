85d12f5c29f14309683b38f9a086eb42a3cf517a
julien <julien@d7859a6d-3d6a-0410-b503-b92f2c4db295>
2013-10-21 16:48:52 +0000

    indexMax.m (branch julien): returns maximum value as second output argument

--- branches/julien/mrLoadRet/Plugin/GLM_v2/combineTransformOverlayFunctions/indexMax.m
+++ branches/julien/mrLoadRet/Plugin/GLM_v2/combineTransformOverlayFunctions/indexMax.m
@@ -1,11 +1,12 @@
-% indexMax.m: returns index of maximum value 
+% indexMax.m: returns index of maximum value (and maximum value as second output) 
 %
 %        $Id$
 %
-%   example: index = indexMax(x,y,z) returns [2 3] if y(1)>x(1), y(1)>z(1) and z(2)>x(2), z(2)>y(2)
+%   example: if x=[4 5], y=[6 4] and z=[5 6], indexMax(x,y,z) returns 
+%             index=[2 3] and max=[6 6] (because y(1)>x(1), y(1)>z(1) and z(2)>x(2), z(2)>y(2) )
 %           
 
-function index = indexMax(varargin)
+function [index, maximum] = indexMax(varargin)
 
 
 nDims = length(size(varargin{1}));
@@ -20,7 +21,7 @@ for i = 2:nargin
   end
 end
 
-[dump,index] = max(array,[],nDims+1);
+[maximum,index] = max(array,[],nDims+1);
 
 %put NaNs if all values are NaNs
 index(all(isnan(array),nDims+1))=NaN;
