2de05eb5dfe18f2e654f7459cd4a5ae52dce00b4
julien <julien@d7859a6d-3d6a-0410-b503-b92f2c4db295>
2014-02-20 11:59:53 +0000

    spatialSmooth.m (branch julien): 3D gaussian kernel can have different widths in different dimensions + masks output with NaNs from input

--- mrLoadRet/Plugin/GLM_v2/combineTransformOverlayFunctions/spatialSmooth.m
+++ mrLoadRet/Plugin/GLM_v2/combineTransformOverlayFunctions/spatialSmooth.m
@@ -6,15 +6,20 @@
 function smoothed = spatialSmooth(overlay,FWHM)
 
 smoothed = nanconvn(overlay,gaussianKernel(FWHM),'same');
+smoothed(isnan(overlay))=NaN;
 
 function kernel = gaussianKernel(FWHM)
 
 sigma_d = FWHM/2.35482;
 w = ceil(FWHM); %deals with resolutions that are not integer
 %make the gaussian kernel large enough for FWHM
-kernelDims = 2*[w w w]+1;
+if length(w)==1
+  w = [w w w];
+  sigma_d = [sigma_d sigma_d sigma_d];
+end
+kernelDims = 2*w+1;
 kernelCenter = ceil(kernelDims/2);
 [X,Y,Z] = meshgrid(1:kernelDims(1),1:kernelDims(2),1:kernelDims(3));
-kernel = exp(-((X-kernelCenter(1)).^2+(Y-kernelCenter(2)).^2+(Z-kernelCenter(3)).^2)/(2*sigma_d^2)); %Gaussian function
+kernel = exp(-((X-kernelCenter(1)).^2/(2*sigma_d(1)^2)+(Y-kernelCenter(2)).^2/(2*sigma_d(2)^2)+(Z-kernelCenter(3)).^2/(2*sigma_d(3)^2))); %Gaussian function
 kernel = kernel./sum(kernel(:));
 
