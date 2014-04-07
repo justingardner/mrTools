aa9415ac05c6259e6086f172e128ce5d1c020a6e
julien <julien@d7859a6d-3d6a-0410-b503-b92f2c4db295>
2014-04-07 12:16:34 +0000

    (branch julien) change set removing assumption taht freesurfer surfaces are from a 1 mm isotropic anatomy (mrFlatMesh only) - mrFlatViewer.m: converts current starting point form base to world coordinates (mm) before getting patch vertices - makeFlat.m: get voxel size from base anatomy and pass them to flattenSurfaceMFM - flattenSurfaceMFM.m: 	- handles scaling factor other than [1 1 1] 			- corrected bug in vertex distance computation (sqrt was applied twice, which went unnoticed when the average distance was ~1mm) 			- pass desired perimeter to floodFillFindPerimeter for control - orderMeshPerimeterPointsAll.m: corrected bug in while loop stop condition so that all perimeters are now ordered instead of just the first one - floodFillFindPerimeter.m: compares average perimeter of last ring to desired perimeter and if >5%, recompute using distance as stopping criterion

--- mrUtilities/mrFlatMesh/floodFillFindPerim.m
+++ mrUtilities/mrFlatMesh/floodFillFindPerim.m
@@ -1,4 +1,4 @@
-function [insideNodes,insideNodeStruct] = floodFillFindPerim(mesh,perimDist,startNode,busyHandle)
+function [insideNodes,insideNodeStruct] = floodFillFindPerim(mesh,perimDist,startNode,statusHandle)
 % A flood-fill method to find the mesh perimeter
 %
 % [insideNodes,insideNodeStruct]=floodFillFindPerim(mesh,perimDist,startNode,busyHandle)
@@ -15,50 +15,78 @@ function [insideNodes,insideNodeStruct] = floodFillFindPerim(mesh,perimDist,star
 % AUTHOR:  Wade
 % DATE : 020701 last modified
  
+compute=1;
+recompute=0;
 
-allWithinDist = 1;
-insideNodes   = startNode;
-currentNodes  = startNode;
-counter       = 0;
-insideNodeStruct.offset = 0;
-insideNodeStruct.avDist = 0;
-nVerts = length(mesh.connectionMatrix);
+while compute
+  withinDist = 1;
+  insideNodes   = startNode;
+  currentNodes  = startNode;
+  counter       = 0;
+  insideNodeStruct.offset = 0;
+  insideNodeStruct.avDist = 0;
+  nVerts = length(mesh.connectionMatrix);
+  cmBackup = mesh.connectionMatrix;
+  % Limit to generate no more that 10000 rings....
+  while ((withinDist) && (counter<10000)) 
 
-% Limit to generate no more that 10000 rings....
-while ((allWithinDist) && (counter<10000)) 
-   
-   % What nodes are connected to the current ones?
-   [newRows connected]=find(mesh.connectionMatrix(currentNodes,:));
- 
-   if (~isempty(connected))
+     % What nodes are connected to the current ones?
+     [newRows connected]=find(mesh.connectionMatrix(currentNodes,:));
 
-      connected=unique(connected(:));   
-      insideNodes=[insideNodes;connected];
-      
-      currentNodes=connected;
-	  notCnodes=setdiff(1:nVerts,currentNodes);
-	  
-	  % This is a cute way of zeroing columns in a sparse matrix. Much
-	  % faster than foo(:,currentNodes)=0;  
-	  diagMat=sparse(notCnodes,notCnodes,ones(length(notCnodes),1),nVerts,nVerts);
-	  mesh.connectionMatrix=(mesh.connectionMatrix)*(diagMat);
-   
-	  nodeDists=mesh.dist(currentNodes);
-      f=find(nodeDists<perimDist);     
-      allWithinDist=(sum(f(:)));
-      
-      % Average distance
-      insideNodeStruct.avDist=[insideNodeStruct.avDist;mean(nodeDists(:))];
-      
-      % Offset to the list of current rings
-      insideNodeStruct.offset=[insideNodeStruct.offset;length(connected)];
-          
-   else
-         allWithinDist=0;
-   end   
-   
-   counter=counter+1; 
-   
+     if (~isempty(connected))
+
+        connected=unique(connected(:));   
+        insideNodes=[insideNodes;connected];
+
+        currentNodes=connected;
+        notCnodes=setdiff(1:nVerts,currentNodes);
+
+        % This is a cute way of zeroing columns in a sparse matrix. Much
+        % faster than foo(:,currentNodes)=0;  
+        diagMat=sparse(notCnodes,notCnodes,ones(length(notCnodes),1),nVerts,nVerts);
+        mesh.connectionMatrix=(mesh.connectionMatrix)*(diagMat);
+
+        nodeDists=mesh.dist(currentNodes);
+        if recompute  % if the flood fill has gone too far the first time, we change the criterion 
+                      % to stop as soon as the average perimeter is reached  
+          withinDist=mean(nodeDists(:))<perimDist;
+        else
+          withinDist=any(nodeDists<perimDist);
+        end
+
+        % Average distance
+        insideNodeStruct.avDist=[insideNodeStruct.avDist;mean(nodeDists(:))];
+
+        % Offset to the list of current rings
+        insideNodeStruct.offset=[insideNodeStruct.offset;length(connected)];
+  %       round([nnz(mesh.connectionMatrix) insideNodeStruct.avDist(counter+2) insideNodeStruct.offset(counter+2) nnz(nodeDists<perimDist)])
+
+     else
+           withinDist=0;
+     end   
+
+     counter=counter+1; 
+  end
+  if recompute
+    str=sprintf('** Warning: new max average perimeter:%.2f mm\n',insideNodeStruct.avDist(end));
+    statusStringAdd(statusHandle,str);
+    compute=0;
+  else
+    if insideNodeStruct.avDist(end)>perimDist %if the last ring found is more than the perimeter asked
+      beep;
+      str=sprintf('** Warning: flood fill went beyond perimeter (max average: %.2f mm). Have you checked/corrected for topological defects?',insideNodeStruct.avDist(end));
+      statusStringAdd(statusHandle,str);
+      if insideNodeStruct.avDist(end)>perimDist*1.05 %if the last ring found is more than 105% the perimeter asked
+        recompute=1;
+        mesh.connectionMatrix = cmBackup;
+        statusStringAdd(statusHandle,sprintf('** Warning: Recomputing with different stopping criterion...'));
+      else
+        compute=0;
+      end
+    else
+      compute=0;
+    end
+  end
 end
 
 insideNodes=unique(insideNodes);
