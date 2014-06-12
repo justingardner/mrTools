0e284a7c865ad97d84d4529d187692f7bca524b6
julien <julien@d7859a6d-3d6a-0410-b503-b92f2c4db295>
2014-06-12 12:43:06 +0000

    viewGet.m (case alphaOverlayExponent): added optional overlayNum argument (was r2946 on branch julien).

--- mrLoadRet/View/viewGet.m
+++ mrLoadRet/View/viewGet.m
@@ -3592,7 +3592,7 @@ switch lower(param)
       end
     end
   case {'alphaoverlay'}
-    % alphaoverlay = viewGet(view,'alphaOverlay',[overlaynumn])
+    % alphaoverlay = viewGet(view,'alphaOverlay',[overlaynum])
     analysisNum = viewGet(view,'currentAnalysis');
     if isempty(varargin) || isempty(varargin{1})
       overlayNum = viewGet(view,'currentOverlay',analysisNum);
@@ -3610,10 +3610,14 @@ switch lower(param)
       end
     end
   case {'alphaoverlayexponent'}
-    % overlayclip = viewGet(view,'alphaOverlayExponent')
+    % overlayclip = viewGet(view,'alphaOverlayExponent',[overlaynum])
     val = 1;
     analysisNum = viewGet(view,'currentAnalysis');
-    overlayNum = viewGet(view,'currentOverlay',analysisNum);
+    if isempty(varargin) || isempty(varargin{1})
+      overlayNum = viewGet(view,'currentOverlay',analysisNum);
+    else
+      overlayNum = varargin{1};
+    end
     analysis = viewGet(view,'analysis',analysisNum);
     if ~isempty(analysis) & ~isempty(analysis.overlays)
       n = viewGet(view,'numberofOverlays',analysisNum);
