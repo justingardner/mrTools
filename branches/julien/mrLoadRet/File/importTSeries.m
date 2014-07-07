7c69d3f46dcad52b2f11abc84194d1c55f6c9193
julien <julien@d7859a6d-3d6a-0410-b503-b92f2c4db295>
2014-07-07 11:32:38 +0000

    importTSeries.m (branch julien): made scriptable

--- branches/julien/mrLoadRet/File/importTSeries.m
+++ branches/julien/mrLoadRet/File/importTSeries.m
@@ -1,15 +1,19 @@
 % importTSeries.m
 %
 %        $Id$ 
-%      usage: importTSeries(v)
+%      usage: v = importTSeries(v,params,varargin)
 %         by: justin gardner
 %       date: 07/03/09
-%    purpose: 
+%    purpose: import timeseries as a new scan in current group
 %
-function retval = importTSeries(v)
+%             to just get a default parameter structure:
+%             [thisView,params] = importTSeries(thisView,[],'justGetParams=1','defaultParams=1','pathname=fileToImport');
+%
+
+function [v,params] = importTSeries(v,params,varargin)
 
 % check arguments
-if ~any(nargin == [1])
+if nargin < 1
   help importTSeries
   return
 end
@@ -17,73 +21,110 @@ end
 minFramePeriod = .01;  %frame period in sec outside which the user is prompted
 maxFramePeriod = 100;  % that something weird's goin on
 
-% go find the group that user wants to load here
-[filename pathname] = uigetfile({'*.nii;*.img','Nifti files'},'Select nifti tSeries that you want to import','multiselect','on');
+% other arguments
+eval(evalargs(varargin));
+if ieNotDefined('justGetParams'),justGetParams = 0;end
+if ieNotDefined('defaultParams'),defaultParams = 0;end
 
-if isnumeric(filename)
+
+if ieNotDefined('params')
+  if ~ieNotDefined('pathname')
+    [pathname,filename,ext]=fileparts(pathname);
+    filename = [filename,ext];
+  else
+    if defaultParams
+      mrWarnDlg('(importTSeries) Specify a filename when using option defaultParams');
+      return
+    end
+
+    % go find the file that user wants to load here
+    [filename, pathname] = uigetfile({'*.nii;*.img','Nifti files'},'Select nifti tSeries that you want to import','multiselect','on');
+
+    if isnumeric(filename)
+      return
+    end
+  end
+else
+  filename = params.filename;
+  pathname = params.pathname;
+end
+
+if ~iscell(filename)
+      filename = {filename};
+end
+if length(filename)>1 && (justGetParams || defaultParams)
+  mrWarnDlg('(importTSeries) Scripting mode not implemented for multiple files');
   return
-elseif ~iscell(filename)
-  filename = {filename};
 end
 
 nScans = viewGet(v,'nScans');
 cScan=0;  %count of succesfully added scans
+scanNum=[];
 weirdFramePeriods = 0;
 
 for iFile = 1:length(filename)
   
   if ~isempty(strfind(stripext(filename{iFile}),'.'))
     mrWarnDlg(sprintf('(importTSeries) Ignoring file %s because it has a . in the filename that does not mark the file extension. If you want to use this file, consider renaming to %s',filename{iFile},setext(fixBadChars(stripext(filename{iFile}),{'.','_'}),'hdr')));
-    return
-  end
-
-  % get the full file name
-  fullFilename = fullfile(pathname,filename{iFile});
-
-  % read the nifti header
-  hdr = cbiReadNiftiHeader(fullFilename);
-  if isempty(hdr),return,end
-  % make sure it is 4 dimensional and then get the number of frames
-  if ~ismember(hdr.dim(1),[3 4])
-    mrWarnDlg(sprintf('(importTSeries) Could not import tSeries because it is a %i dimensional file',hdr.dim(1)));
-    return
-  elseif hdr.dim(1)==3
-    nFrames = 1;
   else
-    nFrames = hdr.dim(5);
-  end
 
-  paramsInfo = {{'filename',filename{iFile},'The name of the nifti file that you are importing','editable=0'}};
-  paramsInfo{end+1} = {'description','','A description for the nifti tSeries you are imporint'};
-  paramsInfo{end+1} = {'nFrames',nFrames,'incdec=[-1 1]',sprintf('minmax=[0 %i]',nFrames),'Number of total frames in your nfiti tSeries'};
-  paramsInfo{end+1} = {'junkFrames',0,'incdec=[-1 1]',sprintf('minmax=[0 %i]',nFrames),'How many frames should be junked at the beginning'};
+    % get the full file name
+    fullFilename = fullfile(pathname,filename{iFile});
 
-  params = mrParamsDialog(paramsInfo);
-  if isempty(params),return,end
-
-  thisScanParams.fileName = params.filename;
-  thisScanParams.description = params.description;
-  thisScanParams.nFrames = params.nFrames;
-  thisScanParams.junkFrames = params.junkFrames;
-
-  % now read the file
-  %tSeries = cbiReadNifti(fullFilename);
-
-  v = saveNewTSeries(v,fullFilename,thisScanParams);
-  
-  %get the new scan params and check that frame period is a reasonable value
-  newScanNum = viewGet(v,'nScans');
-  if newScanNum>nScans %if the new scan has been succesfully added
-    cScan=cScan+1;
-    scanNum(cScan)=newScanNum;
-    scanParams(cScan) = viewGet(v,'scanparams',newScanNum);
-    if scanParams(cScan).nFrames>1 && (scanParams(cScan).framePeriod>maxFramePeriod || scanParams(cScan).framePeriod<minFramePeriod)
-      weirdFramePeriods(cScan) = scanParams(cScan).framePeriod;
+    % read the nifti headerfl
+    hdr = cbiReadNiftiHeader(fullFilename);
+    if isempty(hdr)
+      mrWarnDlg(sprintf('(importTSeries) Could not read timeseries file %s',fullFilename));
+      return
+    end
+    % make sure it is 4 dimensional and then get the number of frames
+    if ~ismember(hdr.dim(1),[3 4])
+      mrWarnDlg(sprintf('(importTSeries) Could not import tSeries because it is a %i dimensional file',hdr.dim(1)));
+      return
+    elseif hdr.dim(1)==3
+      nFrames = 1;
     else
-      weirdFramePeriods(cScan) = 0;
+      nFrames = hdr.dim(5);
+    end
+
+    if ieNotDefined('params') || iFile>1
+      paramsInfo = {{'pathname',pathname,'The folder of the nifti file that you are importing','editable=0'}};
+      paramsInfo{end+1} = {'filename',filename{iFile},'The name of the nifti file that you are importing','editable=0'};
+      paramsInfo{end+1} = {'description','','A description for the nifti tSeries you are imporint'};
+      paramsInfo{end+1} = {'nFrames',nFrames,'incdec=[-1 1]',sprintf('minmax=[0 %i]',nFrames),'Number of total frames in your nfiti tSeries'};
+      paramsInfo{end+1} = {'junkFrames',0,'incdec=[-1 1]',sprintf('minmax=[0 %i]',nFrames),'How many frames should be junked at the beginning'};
+
+      if defaultParams
+        params = mrParamsDefault(paramsInfo);
+      else
+        params = mrParamsDialog(paramsInfo);
+      end
+    end
+    if isempty(params) || justGetParams,return,end
+
+    thisScanParams.fileName = params.filename;
+    thisScanParams.description = params.description;
+    thisScanParams.nFrames = params.nFrames;
+    thisScanParams.junkFrames = params.junkFrames;
+
+    % now read the file
+    %tSeries = cbiReadNifti(fullFilename);
+
+    v = saveNewTSeries(v,fullFilename,thisScanParams);
+
+    %get the new scan params and check that frame period is a reasonable value
+    newScanNum = viewGet(v,'nScans');
+    if newScanNum>nScans %if the new scan has been succesfully added
+      cScan=cScan+1;
+      scanNum(cScan)=newScanNum;
+      scanParams(cScan) = viewGet(v,'scanparams',newScanNum);
+      if scanParams(cScan).nFrames>1 && (scanParams(cScan).framePeriod>maxFramePeriod || scanParams(cScan).framePeriod<minFramePeriod)
+        weirdFramePeriods(cScan) = scanParams(cScan).framePeriod;
+      else
+        weirdFramePeriods(cScan) = 0;
+      end
     end
   end
-  
 end
 
 %fix apparently abnormal frame periods
