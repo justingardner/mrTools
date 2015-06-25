d4018c9d3a90c2a959f5e1df58f16989378285d4
Justin Gardner <jlg@stanford.edu>
2015-06-25 12:59:00 -0700

    Fixing in repo command to do valid comparison even if the directory link structure gives different names

--- mrLoadRet/PluginAlt/mlrAnatDB/mlrAnatDBInLocalRepo.m
+++ mrLoadRet/PluginAlt/mlrAnatDB/mlrAnatDBInLocalRepo.m
@@ -11,11 +11,33 @@
 %
 function tf = mlrAnatDBInLocalRepo(v)
 
+tf = false;
+
+% get locations of session and repo
 localRepoTop = mlrReplaceTilde(mrGetPref('mlrAnatDBLocalRepo'));
 homeDir = mlrReplaceTilde(viewGet(v,'homeDir'));
-% if they are not the same, then offer add session as a menu item,
-% but nothing else.
-if ~strncmp(localRepoTop,homeDir,length(localRepoTop))
+
+% This is not a straight forward check since we 
+% need to deal with links that can put directories
+% in different places. So first find whether the
+% directory of the repo top is in the homeDir
+localRepoTopLoc = findstr(getLastDir(localRepoTop),homeDir);
+if isempty(localRepoTopLoc),return,end
+
+% it is, so now replace the path in homeDir with the localRepoTop
+homeDirLocalRepo = fullfile(localRepoTop,homeDir(localRepoTopLoc+length(getLastDir(localRepoTop))+1:end));
+
+% now check if the are the same directory by getting each ones link
+% resolved path using pwd -P
+curdir = pwd;
+cd(homeDir);
+[success homeDirResolved] = system('pwd -P');
+cd(homeDirLocalRepo);
+[success homeDirLocalRepoResolved] = system('pwd -P');
+cd(curdir);
+
+% see if they are the same or not
+if ~strcmp(homeDirResolved,homeDirLocalRepoResolved)
   tf = false;
 else
   tf = true;
