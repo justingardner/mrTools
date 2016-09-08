4f0f9086086d450e0fe43a244fda24671fa5a4d7
Akshay Jagadeesh <akjags@gmail.com>
2016-09-08 15:23:37 -0700

    Completed new file structure for models

--- mrLoadRet/Plugin/pRF/pRF_gaussianhdr.m
+++ mrLoadRet/Plugin/pRF/pRF_gaussianhdr.m
@@ -1,7 +1,7 @@
 % pRF_gaussian.m
 %
 %        $Id:$ 
-%      usage: pRF_gaussian(varargin)
+%      usage: pRF_gaussianhdr(varargin)
 %         by: akshay jagadeesh
 %       date: 09/02/16
 %    purpose: Template file to create new models
@@ -13,7 +13,7 @@
 %
 %     This model template is set up for the standard Gaussian model.
 
-function output = pRF_gaussian(varargin)
+function output = pRF_gaussianhdr(varargin)
 
 if nargin < 2
   disp(sprintf('Not enough arguments'));
@@ -21,8 +21,8 @@ if nargin < 2
 end
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%% pRF_gaussian('getModelResponse', fitParams, rfModel, hrf, p, i) %%%%
-%%%                  Called from getModelResidual                   %%%%
+%%%%pRF_gaussianhdr('getModelResponse', fitParams, rfModel, hrf, i)%%%%%
+%%%%                Called from getModelResidual                   %%%%%
 if strcmp(varargin{1}, 'getModelResponse')
 
   fitParams = varargin{2};
@@ -44,29 +44,42 @@ if strcmp(varargin{1}, 'getModelResponse')
   output = thisModelResponse;
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%% pRF_gaussian('setParams', fitParams) %%%%
-%%%        Called from setParams         %%%%
+%%%%pRF_gaussian('setParams', fitParams)%%%%%
+%%%%        Called from setParams       %%%%%
 
 elseif strcmp(varargin{1}, 'setParams')
 
   fitParams = varargin{2};
 
-  fitParams.paramNames = {'x','y','rfWidth'};
-  fitParams.paramDescriptions = {'RF x position','RF y position','RF width (std of gaussian)'};
-  fitParams.paramIncDec = [1 1 1];
-  fitParams.paramMin = [-inf -inf 0];
-  fitParams.paramMax = [inf inf inf];
+  fitParams.paramNames = {'x','y','rfWidth', 'timelag', 'tau'};
+  fitParams.paramDescriptions = {'RF x position','RF y position','RF width (std of gaussian)', 'Time before start of rise of hemodynamic function', 'Width of the hemodynamic function (tau parameter of gamma)'};
+  fitParams.paramIncDec = [1 1 1 0.1 0.5];
+  fitParams.paramMin = [-inf -inf 0 0 0];
+  fitParams.paramMax = [inf inf inf inf inf];
   % set min/max and init
-  fitParams.minParams = [fitParams.stimExtents(1) fitParams.stimExtents(2) 0];
-  fitParams.maxParams = [fitParams.stimExtents(3) fitParams.stimExtents(4) inf];
-  fitParams.initParams = [0 0 4];
+  fitParams.minParams = [fitParams.stimExtents(1) fitParams.stimExtents(2) 0 0 0];
+  fitParams.maxParams = [fitParams.stimExtents(3) fitParams.stimExtents(4) inf 3 inf];
+  fitParams.initParams = [0 0 4 fitParams.timelag fitParams.tau];
+  % add on parameters for difference of gamma
+  if fitParams.diffOfGamma
+    % parameter names/descriptions and other information for allowing user to set them
+    fitParams.paramNames = {fitParams.paramNames{:} 'amp2' 'timelag2','tau2'};
+    fitParams.paramDescriptions = {fitParams.paramDescriptions{:} 'Amplitude of second gamma for HDR' 'Timelag for second gamma for HDR','tau for second gamma for HDR'};
+    fitParams.paramIncDec = [fitParams.paramIncDec(:)' 0.1 0.1 0.5];
+    fitParams.paramMin = [fitParams.paramMin(:)' 0 0 0];
+    fitParams.paramMax = [fitParams.paramMax(:)' inf inf inf];
+    % set min/max and init
+    fitParams.minParams = [fitParams.minParams 0 0 0];
+    fitParams.maxParams = [fitParams.maxParams inf 6 inf];
+    fitParams.initParams = [fitParams.initParams fitParams.amplitudeRatio fitParams.timelag2 fitParams.tau2];
+  end
 
   % return fitParams with modified values
   output = fitParams;
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%%%  pRF_gaussian(command, fitParams, params)  %%%%%
-%%%%         Called from getFitParams           %%%%% 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%0%%%%%
+%%%%  pRF_gaussianhdr(command, fitParams, params)%%%%%
+%%%%         Called from getFitParams            %%%%% 
 
 elseif strcmp(varargin{1}, 'getFitParams')
 
@@ -82,17 +95,18 @@ elseif strcmp(varargin{1}, 'getFitParams')
   % use a fixed single gaussian
   p.canonical.type = 'gamma';
   p.canonical.lengthInSeconds = 25;
-  p.canonical.timelag = fitParams.timelag;
-  p.canonical.tau = fitParams.tau;
+  p.canonical.timelag = params(4);
+  p.canonical.tau = params(5);
   p.canonical.exponent = fitParams.exponent;
   p.canonical.offset = 0;
   p.canonical.diffOfGamma = fitParams.diffOfGamma;
-  p.canonical.amplitudeRatio = fitParams.amplitudeRatio;
-  p.canonical.timelag2 = fitParams.timelag2;
-  p.canonical.tau2 = fitParams.tau2;
-  p.canonical.exponent2 = fitParams.exponent2;
-  p.canonical.offset2 = 0;
-
+  if fitParams.diffOfGamma
+    p.canonical.amplitudeRatio = params(6);
+    p.canonical.timelag2 = params(7);
+    p.canonical.tau2 = params(8);
+    p.canonical.exponent2 = fitParams.exponent2;
+    p.canonical.offset2 = 0;
+  end
 end
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
