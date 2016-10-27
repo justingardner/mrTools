98712ff8c844c8bbaecad0dbcacdde9b880561e9
Akshay Jagadeesh <akjags@gmail.com>
2016-10-27 12:47:00 -0700

    Modifying trainPRF and testPRF

--- mrLoadRet/Plugin/pRF/testPRF.m
+++ mrLoadRet/Plugin/pRF/testPRF.m
@@ -8,20 +8,38 @@
 %
 %
 %  Input variables:
-%       observe:   true voxel response for the left-out scan at a single timepoint
+%       testTSeries:   true voxel response for the left-out scan at a single timepoint
 %       modelResp:   expected voxel response trained on other n-1 scans
 %       covarMat:   Covariance matrix (size: m x m, where m is # of voxels)
-%       roiVoxels:   list of voxels in this ROI (length m)
+%
+%  Output variables:
+%       logLikelihood: total likelihood of all the observed tSeries given model response
+%       probTable: value at (i, j) is the probability that i'th timepoint in tseries is 
+%                  explained by j'th timepoint in model response.
 
-function testPRF(voxelList, tSeries, residual, modelResponse, covarMat)
+function probTable = testPRF(testTSeries, modelResponse, covarMat)
 
-% Y = MVNPDF(X,MU,SIGMA) %returns the density of the multivariate normal
-%   distribution with mean MU and covariance SIGMA, evaluated at each row
-%   of X.
-mvnpdf(testResponse, modelResponse, covarMat)
+numTimePoints = size(modelResponse, 2);
+probTable = zeros(numTimePoints, numTimePoints);
+logprobTable = zeros(numTimePoints, numTimePoints);
 
+disppercent(-inf,'(testPRF) Calculating likelihoods');
+for i = 1:numTimePoints
+  model_i = modelResponse(:, i);
+  tSeries_i = testTSeries(:, i);
 
-%compute log likelihood of the data and sum
-
-
-
+  for j = 1:numTimePoints
+    tSeries_j = testTSeries(:, j);
+    probTable(j, i) = mvnpdf(tSeries_j, model_i, covarMat);
+    if probTable(j, i) == 0
+      disp(sprintf('\n(testPRF) mvnpdf at index (%i, %i) returning 0; Exiting...', j, i));
+      return;
+    end
+    logProbTable(j, i) = log(probTable(j,i));
+  end
+  
+  %modelLikelihood(i) = mvnpdf(tSeries_i, model_i, covarMat);
+  %logLikelihood = logLikelihood + log(modelLikelihood(i));
+  disppercent(i/numTimePoints);
+end
+disppercent(inf);
