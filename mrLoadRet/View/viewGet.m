justingardner/mrTools

commit c8b9bc0bcd8837a449491a75c62cb7df3769b35d

Changeset modifying the behaviour of the GUI regarding overlay clip and range values (was r1854,1933,1937,1944,2027,2915,2945,2949 on branch julien).
In the original implementation, the 'range' field controls both the range of the color scale and the range of the min/max overlay sliders.
The 'clip' field controls either only the clipping values (position of the slider cursors) or also the color range (depending on other settings)
In the new implementation, the slider range, cursor position and color range can be set independently (with some constraints):
- I added a colorRange field that only controls the color range.
- 'clip' only controls the min/max slider cursors.
- 'range' only controls the range of the slider.
- constraints: clip must be included in range.

mrLoadRet/View/viewGet.m
@@ -2907,14 +2907,14 @@ switch lower(param)
         for i = 1:length(view.analyses{analysisNum}.overlays)
           clip = [clip view.analyses{analysisNum}.overlays(i).clip];
         end
-        overlayRange = view.analyses{analysisNum}.overlays(curOverlay).range;
+        overlayRange = view.analyses{analysisNum}.overlays(curOverlay).colorRange;
         scanNum = viewGet(view,'curScan');
         alpha = viewGet(view,'alpha');
         alphaOverlay = char(viewGet(view,'alphaOverlay'))';
         alphaOverlayExponent = viewGet(view,'alphaOverlayExponent');
         clipAcrossOverlays = viewGet(view,'clipAcrossOverlays');
         multiSliceProjection = mrGetPref('multiSliceProjectionMethod');
-        % need to recalculate overlay if this is aflat
+        % need to recalculate overlay if this is a flat
         % and the cortical depth has changed
         if viewGet(view,'baseType',baseNum)
           corticalDepth = viewGet(view,'corticalDepth');
@@ -3860,6 +3860,43 @@ switch lower(param)
       n = viewGet(view,'numberofOverlays',analysisNum);
       if overlayNum & (overlayNum > 0) & (overlayNum <= n)
         val = analysis.overlays(overlayNum).range;
+      end
+    end
+    
+  case {'overlaycolorrange'}
+    % overlaycolorrange = viewGet(view,'overlaycolorrange',[overlayNum],[analysisNum])
+    % overlaycolorrange = viewGet(view,'overlaycolorrange',overlayNum,[])
+    % overlaycolorrange = viewGet(view,'overlaycolorrange',[],analysisNum)
+    % overlaycolorrange = viewGet(view,'overlaycolorrange',[],[])
+    % overlaycolorrange = viewGet(view,'overlaycolorrange',overlayNum)
+    % overlaycolorrange = viewGet(view,'overlaycolorrange')
+    if ieNotDefined('varargin')
+      analysisNum = viewGet(view,'currentAnalysis');
+      overlayNum = viewGet(view,'currentOverlay',analysisNum);
+    end
+    switch (length(varargin))
+      case 1
+        overlayNum = varargin{1};
+        analysisNum = viewGet(view,'currentAnalysis');
+      case 2
+        overlayNum = varargin{1};
+        analysisNum = varargin{2};
+    end
+    if isempty(analysisNum)
+      analysisNum = viewGet(view,'currentAnalysis');
+    end
+    if isempty(overlayNum)
+      overlayNum = viewGet(view,'currentOverlay',analysisNum);
+    end
+    analysis = viewGet(view,'analysis',analysisNum);
+    if ~isempty(analysis) & ~isempty(analysis.overlays)
+      n = viewGet(view,'numberofOverlays',analysisNum);
+      if overlayNum & (overlayNum > 0) & (overlayNum <= n)
+        if isfield(analysis.overlays(overlayNum),'colorRange') && ~isempty(analysis.overlays(overlayNum).colorRange)
+          val = analysis.overlays(overlayNum).colorRange;
+        else
+          val = analysis.overlays(overlayNum).range;
+        end
       end
     end
     

mrLoadRet/Edit/editOverlayGUImrParams.m
@@ -105,6 +105,7 @@ function mrCmapCallback(v,params)
 
   % set the overlay range & clip
   o.range = [params.overlayRange(1) params.overlayRange(2)];
+  o.colorRange = [params.overlayRange(1) params.overlayRange(2)];
   o.clip = [params.overlayClip(1) params.overlayClip(2)];
   
 

mrLoadRet/GUI/computeOverlay.m
@@ -196,47 +196,47 @@ if ~isempty(overlayImages)
     end
     switch(overlayCType)
       case 'normal'
-        overlays.range(iOverlay,:) = viewGet(thisView,'overlayRange',curOverlays(iOverlay));
+        overlays.colorRange(iOverlay,:) = viewGet(thisView,'overlayColorRange',curOverlays(iOverlay));
       case 'setRangeToMax'
-        overlays.range(iOverlay,:) = viewGet(thisView,'overlayClip',curOverlays(iOverlay));
+        overlays.colorRange(iOverlay,:) = viewGet(thisView,'overlayClip',curOverlays(iOverlay));
         if ~isempty(overlays.overlayIm(overlayMasks(:,:,:,iOverlay)))
           thisOverlayIm=overlays.overlayIm(:,:,iOverlay);
-          overlays.range(iOverlay,1) = max(overlays.range(iOverlay,1),min(thisOverlayIm(overlayMasks(:,:,:,iOverlay))));
-          overlays.range(iOverlay,2) = min(max(thisOverlayIm(overlayMasks(:,:,:,iOverlay))),overlays.range(iOverlay,2));
+          overlays.colorRange(iOverlay,1) = max(overlays.colorRange(iOverlay,1),min(thisOverlayIm(overlayMasks(:,:,:,iOverlay))));
+          overlays.colorRange(iOverlay,2) = min(max(thisOverlayIm(overlayMasks(:,:,:,iOverlay))),overlays.colorRange(iOverlay,2));
         end
       case 'setRangeToMaxAroundZero'
        if (viewGet(thisView,'baseType') == 0) && viewGet(thisView,'baseMultiAxis')
 	 oneTimeWarning('setRangeToMaxWithMultiAxis','(computeOverlay) !!! Overlay set to setRangeToMaxAroundZero and you are displaying with multiple axis. Note that the colorbar will only be accurate for the axial view. Consider setting Edit/Overlay/Edit OVerlay to setRangeToMaxAcrossSlices. !!!',1);
        end
-        overlays.range(iOverlay,:) = viewGet(thisView,'overlayClip',curOverlays(iOverlay));
+        overlays.colorRange(iOverlay,:) = viewGet(thisView,'overlayClip',curOverlays(iOverlay));
         if ~isempty(overlays.overlayIm(overlayMasks(:,:,:,iOverlay)))
           maxval = max(abs(overlays.overlayIm(overlayMasks(:,:,:,iOverlay))));
-          overlays.range(iOverlay,1) = -maxval;
-          overlays.range(iOverlay,2) = maxval;
+          overlays.colorRange(iOverlay,1) = -maxval;
+          overlays.colorRange(iOverlay,2) = maxval;
         end
       case 'setRangeToMaxAcrossSlices' %this doesn't take into account voxels that would be masked in other slices than the one displayed
-        overlays.range(iOverlay,:) = viewGet(thisView,'overlayClip',curOverlays(iOverlay));
+        overlays.colorRange(iOverlay,:) = viewGet(thisView,'overlayClip',curOverlays(iOverlay));
         minOverlay = viewGet(thisView,'minOverlaydata',curOverlays(iOverlay),analysisNum,scan); 
         maxOverlay = viewGet(thisView,'maxOverlaydata',curOverlays(iOverlay),analysisNum,scan);
         if ~isempty(minOverlay)
-          overlays.range(iOverlay,1) = max(minOverlay,overlays.range(1));
+          overlays.colorRange(iOverlay,1) = max(minOverlay,overlays.colorRange(1));
         end
         if ~isempty(maxOverlay)
-          overlays.range(iOverlay,2) = min(maxOverlay,overlays.range(2));
+          overlays.colorRange(iOverlay,2) = min(maxOverlay,overlays.colorRange(2));
         end
       case 'setRangeToMaxAcrossSlicesAndScans' %same here
-        overlays.range(iOverlay,:) = viewGet(thisView,'overlayClip',curOverlays(iOverlay));
+        overlays.colorRange(iOverlay,:) = viewGet(thisView,'overlayClip',curOverlays(iOverlay));
         minOverlay = viewGet(thisView,'minOverlaydata',curOverlays(iOverlay),analysisNum); 
         maxOverlay = viewGet(thisView,'maxOverlaydata',curOverlays(iOverlay),analysisNum);
         if ~isempty(minOverlay)
-          overlays.range(iOverlay,1) = max(minOverlay,overlays.range(1));
+          overlays.colorRange(iOverlay,1) = max(minOverlay,overlays.colorRange(1));
         end
         if ~isempty(maxOverlay)
-          overlays.range(iOverlay,2) = min(maxOverlay,overlays.range(2));
+          overlays.colorRange(iOverlay,2) = min(maxOverlay,overlays.colorRange(2));
         end
     end
-    overlays.RGB(:,:,:,iOverlay) = rescale2rgb(overlayImages(:,:,:,iOverlay),overlays.cmap(:,:,iOverlay),overlays.range(iOverlay,:));
-      
+    overlays.RGB(:,:,:,iOverlay) = rescale2rgb(overlayImages(:,:,:,iOverlay),overlays.cmap(:,:,iOverlay),overlays.colorRange(iOverlay,:));
+
   end
   %alpha based on the alpha slider
   if ieNotDefined('alpha')

mrLoadRet/GUI/mrLoadRetGUI.m
@@ -567,12 +567,22 @@ refreshMLRDisplay(viewNum);
 function overlayMaxText_Callback(hObject, eventdata, handles)
 viewNum = handles.viewNum;
 value = str2num(get(hObject,'String'));
-if length(value) ~= 1 %if the user just erased the value, get it from the slider and do nothing
-  set(hObject,'String',num2str(get(handles.overlayMaxSlider,'value')));
-else %otherwise, set the new value in the view and the GUI
-  viewSet(viewNum,'overlayMax',value,viewGet(viewNum,'curClippingOverlay'));
-  refreshMLRDisplay(viewNum);
+if length(value) ~= 1 %if the user just erased the value
+  % then  get it from the slider
+  value = get(handles.overlayMaxSlider,'value');
+else
+  sliderMaxValue=get(handles.overlayMaxSlider,'max');
+  sliderMinValue=get(handles.overlayMinSlider,'min');
+  if value>sliderMaxValue %if the value is beyond the slider max
+    %set the new value as the new max range
+    viewSet(viewNum,'overlayRange',[sliderMinValue value]);
+  elseif value<sliderMinValue %if the value is below the slider min
+    value = sliderMinValue;
+  end
 end
+%set the new value in the view and the GUI
+viewSet(viewNum,'overlayMax',value,viewGet(viewNum,'curClippingOverlay'));
+refreshMLRDisplay(viewNum);
 
 % --- overlayMin
 function overlayMinSlider_CreateFcn(hObject, eventdata, handles)
@@ -600,11 +610,22 @@ function overlayMinText_Callback(hObject, eventdata, handles)
 viewNum = handles.viewNum;
 value = str2num(get(hObject,'String'));
 if length(value) ~= 1%if the user just erased the value, get it from the slider and do nothing
-  set(hObject,'String',num2str(get(handles.overlayMinSlider,'value')));
+  % then  get it from the slider
+  value = get(handles.overlayMinSlider,'value');
+%  set(hObject,'String',num2str(get(handles.overlayMinSlider,'value')));
 else %otherwise, set the new value in the view and the GUI
-  viewSet(viewNum,'overlayMin',value,viewGet(viewNum,'curClippingOverlay'));
-  refreshMLRDisplay(viewNum);
+  sliderMaxValue=get(handles.overlayMaxSlider,'max');
+  sliderMinValue=get(handles.overlayMinSlider,'min');
+  if value>sliderMaxValue %if the value is beyond the slider max
+    value = sliderMaxValue;
+  elseif value<sliderMinValue %if the value is below the slider min
+    %set the new value as the new min range
+    viewSet(viewNum,'overlayRange',[value sliderMaxValue]);
+%     value = sliderMinValue;
+  end
 end
+viewSet(viewNum,'overlayMin',value,viewGet(viewNum,'curClippingOverlay'));
+refreshMLRDisplay(viewNum);
 
 % --- alpha
 function alphaSlider_CreateFcn(hObject, eventdata, handles)

mrLoadRet/GUI/refreshMLRDisplay.m
@@ -356,14 +356,14 @@ if ~isempty(base.RGB) & ~isempty(overlays.RGB)
         img=base.RGB; %only display base in the background
         contours=overlays.overlayIm;  %display first overlay as contours 
         contours(~overlays.alphaMaps(:,:,1,1))=NaN;   % use non-zero alpha map as mask
-        contourCscale=overlays.range(1,:); 
+        contourCscale=overlays.colorRange(1,:); 
         contourCmap = overlays.cmap(:,:,1);
       else
         %combine first overlay and base
         img=overlays.alphaMaps(:,:,:,1).*overlays.RGB(:,:,:,1)+(1-overlays.alphaMaps(:,:,:,1)).*base.RGB;
         contours=overlays.overlayIm(:,:,2); %display second overlay as contours
         contours(~overlays.alphaMaps(:,:,1,2))=NaN;   % use non-zero alpha map as mask
-        contourCscale=overlays.range(2,:);
+        contourCscale=overlays.colorRange(2,:);
         contourCmap = overlays.cmap(:,:,2);
       end
       if size(overlays.RGB,4)>2
@@ -377,7 +377,7 @@ if ~isempty(base.RGB) & ~isempty(overlays.RGB)
       img = reshape(img,[base.dims 3]);
   end
   cmap = overlays.cmap;
-  cbarRange = overlays.range;
+  cbarRange = overlays.colorRange;
 elseif ~isempty(base.RGB)
   img = base.RGB;
   cmap = base.cmap;

mrLoadRet/Plugin/GLM_v2/editOverlayGUI.m
@@ -36,11 +36,9 @@ function editOverlayGUI(viewNum)
     return;
   end
   overlay = viewGet(thisView, 'overlay', overlayNum, analysisNum);
-%   overlayUsefulRange = viewGet(thisView,'overlayRange', overlayNum, analysisNum);
-%   overlayColorRange = viewGet(thisView,'overlayColorRange', overlayNum, analysisNum);
-  overlayRange = viewGet(thisView,'overlayRange', overlayNum, analysisNum);
-%   overlayClipRange = viewGet(thisView,'overlayClip', overlayNum, analysisNum);
-  overlayClip = viewGet(thisView,'overlayClip', overlayNum, analysisNum);
+  overlayUsefulRange = viewGet(thisView,'overlayRange', overlayNum, analysisNum);
+  overlayColorRange = viewGet(thisView,'overlayColorRange', overlayNum, analysisNum);
+  overlayClipRange = viewGet(thisView,'overlayClip', overlayNum, analysisNum);
   overlayName = viewGet(thisView, 'overlayName', overlayNum, analysisNum);
   overlayType = viewGet(thisView, 'overlayType', overlayNum,analysisNum);
   alphaOverlay = viewGet(thisView,'alphaOverlay');
@@ -72,17 +70,13 @@ function editOverlayGUI(viewNum)
   paramsInfo{end+1} = {'shiftColormap', 0, 'incdec=[-16 16]', 'shift the colormap -- this can be useful for retinotopy scans with circular colormaps'}; 
   paramsInfo{end+1} = {'overlayColormapType',overlayColormapTypeMenu , 'type=popupmenu',...
       '''normal'' scales the colormap to the value specified by colormap range; ''setRangeToMax'' scales the colormap to the min amnd max of the displayed overlay slice and ignores the color range (like R2 maps)'};
-%   paramsInfo{end+1} = {'overlayColorRange', overlayColorRange, 'The lower and upper bound on the colormap when overlayColormapType=''normal'''};
-%   paramsInfo{end+1} = {'overlayClipRange', overlayClipRange, 'callback',{@checkCmapParams,'cliprange'},'passCallbackOutput=1','passValue=1','passParams=1',...
-%       'The lower and upper clip points beyond which the overlay is masked. These should be inside the useful range. If clip(1)>clip(2), then values inside the clip range are masked.'};
-%   paramsInfo{end+1} = {'overlayUsefulRange', overlayUsefulRange, 'callback',{@checkCmapParams,'usefulrange'},'passCallbackOutput=1','passValue=1','passParams=1',...
-%       'The lower and upper bound on the clip slider. These should be lower/higher than the clip values'};
-%   paramsInfo{end+1} = {'setUsefulRange', 0, 'type=pushbutton','callback',@mrCmapSetUsefulRange,'callbackArg',viewNum,'buttonString=Set useful range to overlay min/max','passParams=1','passCallbackOutput=0',...
-%       'Sets the useful range to the min/max values of this overlay accross scans'};
-  paramsInfo{end+1} = {'overlayRange', overlayRange, 'callback',{@checkCmapParams,'range'},'passCallbackOutput=1','passValue=1','passParams=1',...
-      'The lower and upper bound on the clip slider. These should be lower/higher than the clip values'};
-  paramsInfo{end+1} = {'overlayClip', overlayClip, 'callback',{@checkCmapParams,'clip'},'passCallbackOutput=1','passValue=1','passParams=1',...
+  paramsInfo{end+1} = {'overlayColorRange', overlayColorRange, 'callback',{@checkCmapParams,'colorrange'},'passCallbackOutput=1','passValue=1','passParams=1', 'The lower and upper bound on the colormap when overlayColormapType=''normal'''};
+  paramsInfo{end+1} = {'overlayClipRange', overlayClipRange, 'callback',{@checkCmapParams,'cliprange'},'passCallbackOutput=1','passValue=1','passParams=1',...
       'The lower and upper clip points beyond which the overlay is masked. These should be inside the useful range. If clip(1)>clip(2), then values inside the clip range are masked.'};
+  paramsInfo{end+1} = {'overlayUsefulRange', overlayUsefulRange, 'callback',{@checkCmapParams,'usefulrange'},'passCallbackOutput=1','passValue=1','passParams=1',...
+      'The lower and upper bound on the clip slider. These should be lower/higher than the clip values'};
+  paramsInfo{end+1} = {'setUsefulRange', 0, 'type=pushbutton','callback',@mrCmapSetUsefulRange,'callbackArg',viewNum,'buttonString=Set useful range to overlay min/max','passParams=1','passCallbackOutput=0',...
+      'Sets the useful range to the min/max values of this overlay accross scans'};
   paramsInfo{end+1} = {'overlayType', overlayType, 'The type of overlay (ph, amp, co ...'};
   paramsInfo{end+1} = {'interrogator', interrogator, 'Sets the overlay default interrogator function'};
   paramsInfo{end+1} = {'alphaOverlay', alphaOverlayMenu, 'You can specify the name of another overlay in the analysis to use as an alpha map. For instance, you might want to display one overlay with the alpha set to the r2 or the coherence value.'};
@@ -108,18 +102,13 @@ maxOverlayData = ceil(double(viewGet(thisView,'maxOverlayData'))*1e6)/1e6;
 
 if isempty(maxOverlayData) || isempty(minOverlayData)
   mrWarnDlg('(editOverlayGUI) overlay seems to be empty');
-% elseif all(abs(params.overlayUsefulRange-[minOverlayData maxOverlayData])<5e-7)
-%   mrWarnDlg('(editOverlayGUI) Useful range is already set to min/max');
-elseif all(abs(params.overlayRange-[minOverlayData maxOverlayData])<5e-7)
+elseif all(abs(params.overlayUsefulRange-[minOverlayData maxOverlayData])<5e-7)
   mrWarnDlg('(editOverlayGUI) Useful range is already set to min/max');
 else
-%   params.overlayUsefulRange = [minOverlayData maxOverlayData];
-  params.overlayRange = [minOverlayData maxOverlayData];
+  params.overlayUsefulRange = [minOverlayData maxOverlayData];
   %make sure clip values are within this range
-%   params.overlayClipRange(1) = max(params.overlayClipRange(1),minOverlayData);
-%   params.overlayClipRange(2) = min(params.overlayClipRange(2),maxOverlayData);
-  params.overlayClip(1) = max(params.overlayClip(1),minOverlayData);
-  params.overlayClip(2) = min(params.overlayClip(2),maxOverlayData);
+  params.overlayClipRange(1) = max(params.overlayClipRange(1),minOverlayData);
+  params.overlayClipRange(2) = min(params.overlayClipRange(2),maxOverlayData);
   mrParamsSet(params,1);
 end
 
@@ -151,19 +140,21 @@ end
 function value = checkCmapParams(params,value,whichParam)
 
 switch(whichParam)
+  case 'colorrange'
+    if diff(params.overlayColorRange)<=0
+      mrWarnDlg('(editOverlayGUI) colorange values cannot be equal or decreasing.');
+      value=[];
+    end  
   case 'cliprange'
     %we don't know what value we got so we need to find out first
-%     indexInArray = find(params.overlayClipRange==value);
-    indexInArray = find(params.overlayClip==value);
+    indexInArray = find(params.overlayClipRange==value);
     switch(indexInArray)
       case 1
-%         if params.overlayClipRange(indexInArray)<params.overlayUsefulRange(1)
-        if params.overlayClip(indexInArray)<params.overlayRange(1)
+        if params.overlayClipRange(indexInArray)<params.overlayUsefulRange(1)
           value = [];
         end
       case 2
-%         if params.overlayClipRange(indexInArray)>params.overlayUsefulRange(2)
-        if params.overlayClip(indexInArray)>params.overlayRange(2)
+        if params.overlayClipRange(indexInArray)>params.overlayUsefulRange(2)
           value = [];
         end
     end
@@ -172,17 +163,14 @@ switch(whichParam)
     end
   case 'usefulrange'
     %we don't know what value we got so we need to find out first
-%     indexInArray = find(params.overlayUsefulRange==value);
-    indexInArray = find(params.overlayRange==value);
+    indexInArray = find(params.overlayUsefulRange==value);
     switch(indexInArray)
       case 1
-%         if params.overlayUsefulRange(indexInArray)>params.overlayClipRange(1)
-        if params.overlayRange(indexInArray)>params.overlayClip(1)
+        if params.overlayUsefulRange(indexInArray)>params.overlayClipRange(1)
           value = [];
         end
       case 2
-%         if params.overlayUsefulRange(indexInArray)<params.overlayClipRange(2)
-        if params.overlayRange(indexInArray)<params.overlayClip(2)
+        if params.overlayUsefulRange(indexInArray)<params.overlayClipRange(2)
           value = [];
         end
     end
@@ -190,8 +178,7 @@ switch(whichParam)
       mrWarnDlg('(editOverlayGUI) useful range must contain clip range');
     end  
     %check that min<max
-%     if diff(params.overlayUsefulRange)<0
-    if diff(params.overlayRange)<0
+    if diff(params.overlayUsefulRange)<0
       mrWarnDlg('(editOverlayGUI) useful range must be increasing');
       value=[];
     end
@@ -228,21 +215,20 @@ function mrCmapCallback(params,viewNum)
     return;
   end
   % set the overlay useful range 
-%   newOverlay.range = [params.overlayUsefulRange(1) params.overlayUsefulRange(2)];
-  newOverlay.range = [params.overlayRange(1) params.overlayRange(2)];
+  newOverlay.range = [params.overlayUsefulRange(1) params.overlayUsefulRange(2)];
   %if the range has changed, we only need to update the slider
   if any(abs(newOverlay.range-currentOverlay.range)>5e-7)
     viewSet(thisView,'overlayRange',newOverlay.range, overlayNum);
     refreshMLRDisplay(viewNum);
     return;
   end
-%   % set the overlay color range 
-%   newOverlay.colorRange = [params.overlayColorRange(1) params.overlayColorRange(2)];
-%   if any(abs(newOverlay.colorRange-currentOverlay.colorRange)>5e-7)% && ~strcmp(newOverlay.colormapType,'normal')
-%     viewSet(thisView,'overlayColorRange',newOverlay.colorRange, overlayNum);
-%     refreshMLRDisplay(viewNum);
-%     return;
-%   end
+  % set the overlay color range 
+  newOverlay.colorRange = [params.overlayColorRange(1) params.overlayColorRange(2)];
+  if any(abs(newOverlay.colorRange-currentOverlay.colorRange)>5e-7)% && ~strcmp(newOverlay.colormapType,'normal')
+    viewSet(thisView,'overlayColorRange',newOverlay.colorRange, overlayNum);
+    refreshMLRDisplay(viewNum);
+    return;
+  end
   
   %parameters for which the overlay has to be recomputed as a whole (this should be changed by adding cases to viewSet)
   % set which color cmap to use
@@ -292,8 +278,7 @@ function mrCmapCallback(params,viewNum)
   newOverlay.colormapType = params.overlayColormapType;
 
   % set the overlay clip
-%   newOverlay.clip = [params.overlayClipRange(1) params.overlayClipRange(2)];
-  newOverlay.clip = [params.overlayClip(1) params.overlayClip(2)];
+  newOverlay.clip = [params.overlayClipRange(1) params.overlayClipRange(2)];
   
 
 %   % set the name of the overlay

mrLoadRet/View/isoverlay.m
@@ -20,33 +20,7 @@ function [tf overlay] =  isoverlay(overlay)
 if (nargout == 2)
   % Add optional fields and return true if the overlay with optional
   % fields is valid.
-  requiredFields = {'function','groupName','name','params','range'};
-  % get some defaults, if the required fields exist to make the defaults
-  % then take from there, otherwise put in empty and the code will
-  % later catch that the defaults are missing.
-  if isfield(overlay,'name') 
-    defaultType = overlay.name;
-  else
-    defaultType = '';
-  end
-  if isfield(overlay,'range')
-    defaultClip = overlay.range;
-  else
-    defaultClip = [];
-  end
-  optionalFields = {'date',datestr(now);
-                    'type',defaultType,
-                    'alpha',1;
-                    'clip',defaultClip;
-                    'colormap',jet(256);
-                    'interrogator','timecoursePlot';
-                    'reconcileFunction','defaultReconcileParams';
-                    'mergeFunction','defaultMergeParams';
-                    'colormapType', 'normal';
-                    'data',[];
-		    'alphaOverlay',[];
-		    'alphaOverlayExponent',1;
-		   };
+  requiredFields = {'groupName','name','params','range'};
 else
   % Return 0 if the overlay structure is missing any fields required or
   % optional (since w/out changing the analysis structure it is invalid).
@@ -54,7 +28,6 @@ else
                     'date','type','alpha','clip','colormap',...
                     'interrogator','reconcileFunction','mergeFunction',...
 		    'data','alphaOverlay','alphaOverlayExponent'};
-  optionalFields = {};
 end
 
 % Initialize return value
@@ -77,6 +50,42 @@ for f = 1:length(requiredFields)
   end
 end
 
+%if not valid, no point in trying to make it valid
+if ~tf
+  return
+end
+
+%do not allow empty range
+if isempty(overlay.range) || any(size(overlay.range)~=[1 2])
+  overlay.range(1) = -1;  
+  overlay.range(2) = 1; 
+end
+
+%only set optional fields once required fields have been checked
+if (nargout == 2)
+  % Add optional fields and return true if the overlay with optional
+  % fields is valid.
+  optionalFields = {'function','unknown';
+                    'date',datestr(now);
+                    'type',overlay.name;
+                    'alpha',1;
+                    'clip',overlay.range;
+                    'colorRange',overlay.range;
+                    'colormap',jet(256);
+                    'interrogator','timecoursePlot';
+                    'reconcileFunction','defaultReconcileParams';
+                    'mergeFunction','defaultMergeParams';
+                    'colormapType', 'normal';
+                    'data',[];
+		    'alphaOverlay',[];
+		    'alphaOverlayExponent',1;
+		   };
+else
+  % Return 0 if the overlay structure is missing any fields required or
+  % optional (since w/out changing the analysis structure it is invalid).
+  optionalFields = {'colorRange',overlay.range};
+end
+
 % Optional fields and defaults
 for f = 1:size(optionalFields,1)
   fieldName = optionalFields{f,1};
@@ -89,3 +98,16 @@ for f = 1:size(optionalFields,1)
   end
 end
 overlay = orderfields(overlay);
+
+%Do not allow empty clip and color ranges and force range to include clip range
+if tf
+  if isempty(overlay.clip) || any(size(overlay.clip)~=[1 2])
+    overlay.clip = overlay.range;
+  end
+  if isempty(overlay.colorRange) || any(size(overlay.colorRange)~=[1 2])
+    overlay.colorRange = overlay.range;
+  end
+  overlay.range(1) = min(overlay.clip(1),overlay.range(1));
+  overlay.range(2) = max(overlay.clip(2),overlay.range(2));
+end
+  

mrLoadRet/View/viewSet.m
@@ -1696,12 +1696,6 @@ switch lower(param)
         % set overlay min and max sliders
         overlayClip = viewGet(view,'overlayClip',curClippingOverlay,analysisNum);
         overlayRange = viewGet(view,'overlayRange',curClippingOverlay,analysisNum); 
-        if isempty(overlayClip)
-          overlayClip = [0 1];
-        end
-        if isempty(overlayRange)
-          overlayRange = [0 1];
-        end
         mlrGuiSet(view,'overlayMinRange',overlayRange);
         mlrGuiSet(view,'overlayMaxRange',overlayRange);
         mlrGuiSet(view,'overlayMin',overlayClip(1));
@@ -1898,6 +1892,22 @@ switch lower(param)
         clip = view.analyses{analysisNum}.overlays(overlayNum).clip;
         mlrGuiSet(view,'overlayMin',clip(1));
         mlrGuiSet(view,'overlayMax',clip(2));
+      end
+    end
+    
+  case {'overlaycolorrange'}
+    % view = viewSet(view,'overlaycolorrange',[min max],[overlayNum]);
+    curOverlay = viewGet(view,'currentOverlay');
+    if ieNotDefined('varargin')
+      overlayNum = curOverlay;
+    else
+      overlayNum = varargin{1};
+    end
+    analysisNum = viewGet(view,'currentAnalysis');
+    if ~isempty(analysisNum) & ~isempty(overlayNum) & ...
+        ~isempty(view.analyses{analysisNum}.overlays)
+      for iOverlay = overlayNum
+        view.analyses{analysisNum}.overlays(iOverlay).colorRange = val;
       end
     end
 

commit 220d5c8173956c93da4f86afcf31fcffb593fa1d

viewGet.m (overlayCacheId): now gets overlay range, colorrange and alphaoverlayexponent from all displayed overlays. This fixes a bug introduced in c8b9bc0, which is that viewGet now throws an error when trying to get the color range for multiple overlays at once (which is not implemented). It also makes more sense since different caches should be used for different combinations of these parameters

mrLoadRet/View/viewGet.m
@@ -2913,12 +2913,18 @@ switch lower(param)
         for i = 1:length(view.analyses{analysisNum}.overlays)
           clip = [clip view.analyses{analysisNum}.overlays(i).clip];
         end
-        overlayRange = viewGet(view,'overlayRange');
-        overlayColorRange = viewGet(view,'overlayColorRange');
+        % get overlay ranges for each selected overlay
+        overlayRange = [];
+        overlayColorRange = [];
+        alphaOverlayExponent = [];
+        for i = 1:length(curOverlay)
+          overlayRange = [overlayRange viewGet(view,'overlayRange',curOverlay(i))];
+          overlayColorRange = [overlayColorRange viewGet(view,'overlayColorRange',curOverlay(i))];
+          alphaOverlayExponent = [alphaOverlayExponent viewGet(view,'alphaOverlayExponent')];
+        end
         scanNum = viewGet(view,'curScan');
         alpha = viewGet(view,'alpha');
         alphaOverlay = char(viewGet(view,'alphaOverlay'))';
-        alphaOverlayExponent = viewGet(view,'alphaOverlayExponent');
         clipAcrossOverlays = viewGet(view,'clipAcrossOverlays');
         multiSliceProjection = mrGetPref('multiSliceProjectionMethod');
         % need to recalculate overlay if this is a flat