a80e4e7277759ff1211f0e6b4fe19369af37678d
Akshay <akjags@gmail.com>
2017-06-29 14:48:47 -0700

    Minor changes

--- mrLoadRet/Plugin/pRF/pRFRunSplits.m
+++ mrLoadRet/Plugin/pRF/pRFRunSplits.m
@@ -7,16 +7,17 @@
 function splits = pRFRunSplits(splitFileName)
 
 % Load the split file
-l = load(splitFileName);
+l = load(sprintf('Splits/%s', splitFileName));
 s = l.split;
 % split contains: nVoxels, scanCoords, tSeries, stim, concatInfo, prefit, pRFFitParams
 
 % Call pRFFit on the voxels
 
-parfor i = 1:s.nVoxels
-  x = scanCoords(1,:); y = scanCoords(2,:); z = scanCoords(3,:);
+tic
+for i = 1:s.nVoxels
+  x = s.scanCoords(1,:); y = s.scanCoords(2,:); z = s.scanCoords(3,:);
 
-  fit = pRFFit(v, s.scanNum, x(i),y(i),z(i), 'stim', s.stim, 'concatInfo', s.concatInfo, 'prefit', s.prefit, 'fitTypeParams', s.pRFFitParams, 'tSeries', s.tSeries(i,:), 'paramsInfo', s.paramsInfo);
+  fit = pRFFit(s.v, s.scanNum, x(i),y(i),z(i), 'stim', s.stim, 'concatInfo', s.concatInfo, 'prefit', s.prefit, 'fitTypeParams', s.pRFFitParams, 'tSeries', s.tSeries(i,:)', 'paramsInfo', s.paramsInfo);
 
   if ~isempty(fit)
     thisR2(i) = fit.r2;
@@ -28,10 +29,11 @@ parfor i = 1:s.nVoxels
   end
 
 end
-splitAnal.r2 = thisR2;
-splitAnal.polarAngle = thisPolarAngle;
-splitAnal.eccentricity = thisEccentricity;
-splitAnal.rfHalfWidth = thisRfHalfWidth;
-splitAnal.params = rawParams;
-splitAnal.r = r;
-save(sprintf('%s_Anal.mat', splitFileName(1:end-4)), splitAnal);
+toc
+splits.r2 = thisR2;
+splits.polarAngle = thisPolarAngle;
+splits.eccentricity = thisEccentricity;
+splits.rfHalfWidth = thisRfHalfWidth;
+splits.params = rawParams;
+splits.r = r;
+save(sprintf('Splits/Analysis/%s_Anal.mat', splitFileName(1:end-4)), 'splits');
