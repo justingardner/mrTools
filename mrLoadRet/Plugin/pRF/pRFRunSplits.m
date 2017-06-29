5033c0db24b144e2b395608556654772fe5c4d38
Akshay <akjags@gmail.com>
2017-06-28 20:06:36 -0700

    Fixed to work on sherlock server

--- mrLoadRet/Plugin/pRF/pRFRunSplits.m
+++ mrLoadRet/Plugin/pRF/pRFRunSplits.m
@@ -11,11 +11,9 @@ l = load(splitFileName);
 s = l.split;
 % split contains: nVoxels, scanCoords, tSeries, stim, concatInfo, prefit, pRFFitParams
 
-
-
 % Call pRFFit on the voxels
 
-for i = 1:s.nVoxels
+parfor i = 1:s.nVoxels
   x = scanCoords(1,:); y = scanCoords(2,:); z = scanCoords(3,:);
 
   fit = pRFFit(v, s.scanNum, x(i),y(i),z(i), 'stim', s.stim, 'concatInfo', s.concatInfo, 'prefit', s.prefit, 'fitTypeParams', s.pRFFitParams, 'tSeries', s.tSeries(i,:), 'paramsInfo', s.paramsInfo);
@@ -30,3 +28,10 @@ for i = 1:s.nVoxels
   end
 
 end
+splitAnal.r2 = thisR2;
+splitAnal.polarAngle = thisPolarAngle;
+splitAnal.eccentricity = thisEccentricity;
+splitAnal.rfHalfWidth = thisRfHalfWidth;
+splitAnal.params = rawParams;
+splitAnal.r = r;
+save(sprintf('%s_Anal.mat', splitFileName(1:end-4)), splitAnal);
