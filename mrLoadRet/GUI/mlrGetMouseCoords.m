justingardner/mrTools

commit 375c2583ce28a6e685719ce5111bc9942432ccd2

Change set allows selection of points (with mrInterrogator) and drawing of rois on surfaces which was busted in the latest version of matlab because select3D could no longer get image transformation properties - uses the callback on the surface which returns points clicked on surface in the event

mrLoadRet/GUI/mrInterrogator.m
@@ -249,7 +249,7 @@ if ~isempty(coords.scan)
     for roinum = roinums
       roicoords = getROICoordinates(view,roinum,0);
       % see if this is a matching roi
-      if ismember(round([coords.base.x coords.base.y coords.base.z]),roicoords','rows')
+      if ~isempty(roicoords) && ismember(round([coords.base.x coords.base.y coords.base.z]),roicoords','rows')
 	% get the roi
 	roi{end+1} = viewGet(view,'roi',roinum);
       end
@@ -265,8 +265,6 @@ else
   MLR.interrogator{viewNum}.mouseDownScanCoords = [nan nan nan];
 end
 
-% eval the old handler
-%eval(MLR.interrogator{viewNum}.windowButtonDownFcn);
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % end the mrInterrogator
@@ -280,6 +278,15 @@ fignum = viewGet(v,'figNum');
 % check to see if it is running
 if ~isfield(MLR.interrogator{viewNum},'fignum') || ~isequal(MLR.interrogator{viewNum}.fignum,fignum)
   return
+end
+
+% for surface mode and new matlab turn off callback on display surface
+if ~verLessThan('matlab','8.4') && (viewGet(v,'baseType') == 2)
+  % get the handle for the patch
+  handles = guidata(fignum);
+  if isfield(handles,'displaySurface')
+    set(handles.displaySurface(1),'ButtonDownFcn',[]);
+  end
 end
 
 % set the callbacks back to their originals
@@ -336,16 +343,44 @@ figure(fignum);MLR.interrogator{viewNum}.axesnum = MLR.interrogator{viewNum}.gui
 MLR.interrogator{viewNum}.isActive = 1;
 
 if ~restart
-    % remember old callbacks
-    MLR.interrogator{viewNum}.windowButtonMotionFcn = get(fignum,'WindowButtonMotionFcn');
-    MLR.interrogator{viewNum}.windowButtonDownFcn = get(fignum,'WindowButtonDownFcn');
-    MLR.interrogator{viewNum}.windowButtonUpFcn = get(fignum,'WindowButtonUpFcn');
+  % remember old callbacks
+  MLR.interrogator{viewNum}.windowButtonMotionFcn = get(fignum,'WindowButtonMotionFcn');
+  MLR.interrogator{viewNum}.windowButtonDownFcn = get(fignum,'WindowButtonDownFcn');
+  MLR.interrogator{viewNum}.windowButtonUpFcn = get(fignum,'WindowButtonUpFcn');
 end
 
-% set the callbacks appropriately
-set(fignum,'WindowButtonMotionFcn',sprintf('mrInterrogator(''mouseMove'',%i)',viewNum));
-set(fignum,'WindowButtonDownFcn',sprintf('mrInterrogator(''mouseDown'',%i)',viewNum));
-set(fignum,'WindowButtonUpFcn',sprintf('mrInterrogator(''mouseUp'',%i)',viewNum));
+% In the latest versions of Matlab we are no longer able to use the select3D
+% function which allowed you to find the mouse location on the 3D surface
+% using some hidden view transform properties of the figure. So, instead
+% if we are display a surface, then we set its callback function when mouse
+% clicked to call the functions we want. This does not work for mouseMove (not
+% implemented by matlab, I guess. Will wait until they have something). If 
+% mathworks implements something to make select3D work, then remove this
+% section here, remove the endHandler section which unlinks this handler.
+if ~verLessThan('matlab','8.4') && (viewGet(v,'baseType') == 2)
+  % get the handle for the patch
+  handles = guidata(fignum);
+  if isfield(handles,'displaySurface');
+    % set the first in the list - note that there may be more if 
+    % we are displaying multiple bases at once, but for now we ignore
+    % all those "alt bases" and just respond to clicks on the main base
+    set(handles.displaySurface(1),'ButtonDownFcn',@mrInterrogatorSurfaceCallback);
+    % set the viewNum in the handles
+    userData = get(handles.displaySurface(1),'UserData');
+    userData.viewNum = viewNum;
+    set(handles.displaySurface(1),'UserData',userData);
+    guidata(fignum,handles);
+  end
+  % set the other callbacks
+  set(fignum,'WindowButtonMotionFcn',sprintf('mrInterrogator(''mouseMove'',%i)',viewNum));
+  set(fignum,'WindowButtonUpFcn',sprintf('mrInterrogator(''mouseUp'',%i)',viewNum));
+else
+  
+  % set the callbacks appropriately
+  set(fignum,'WindowButtonMotionFcn',sprintf('mrInterrogator(''mouseMove'',%i)',viewNum));
+  set(fignum,'WindowButtonDownFcn',sprintf('mrInterrogator(''mouseDown'',%i)',viewNum));
+  set(fignum,'WindowButtonUpFcn',sprintf('mrInterrogator(''mouseUp'',%i)',viewNum));
+end
 
 % set pointer to crosshairs
 MLR.interrogator{viewNum}.pointer = get(fignum,'pointer');
@@ -511,3 +546,37 @@ defaultInterrogators = viewGet(v,'defaultInterrogators');
 if ~isempty(defaultInterrogators)
   interrogatorList = {interrogatorList{:} defaultInterrogators{:}};
 end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%   mrInterrogatorSurfaceCallback  %%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function mrInterrogatorSurfaceCallback(hObject,e,handles,varargin)
+
+% get viewNum
+userData = get(hObject,'UserData');
+% if no viewNum set then, something is wrong
+% since this should have been set when mrInterrogator was initialized
+% so giveup
+if ~isfield(userData,'viewNum')
+  return
+end
+% otherwise we should have viewNum
+click.viewNum = userData.viewNum;
+
+% this will now log where in the surface a user clicks
+% to replace the broken select3d
+% get the point that the user clicked
+click.pos = e.IntersectionPoint;
+% compute distance to every vertex and pick the vertex that is closest to the intersectoin point
+[minDist click.vertexIndex] = min(sum((e.Source.Vertices-repmat(click.pos',1,size(e.Source.Vertices,1))').^2,2));
+% now get the vertex position
+click.vertex = e.Source.Vertices(click.vertexIndex,:);
+
+% set the user data to include this latest click information
+set(hObject,'UserData',click);
+
+% call mouse down - note that the handler will get the click location
+% because we have stored in the userdata for the surface. This
+% will be seen select3D
+mouseDownHandler(click.viewNum);
+

mrLoadRet/GUI/refreshMLRDisplay.m
@@ -24,6 +24,9 @@ baseType = viewGet(v,'baseType');
 %curCoords = viewGet(v,'curCoords');
 %disp(sprintf('(refreshMLRDisplay) DEBUG: [%i %i %i]',curCoords(1),curCoords(2),curCoords(3)));
 
+% set display surface to empty
+mlrGuiSet(v,'displaySurface',[]);
+
 % if no base then clear axis and return
 if isempty(baseNum)
   if ~isempty(fig)
@@ -424,7 +427,13 @@ else
   % get alpha
   baseAlpha = viewGet(v,'baseAlpha',baseNum);
   % display the surface
-  patch('vertices', baseSurface.vtcs, 'faces', baseSurface.tris,'FaceVertexCData', squeeze(img),'facecolor','interp','edgecolor','none','Parent',hAxis,'FaceAlpha',baseAlpha);
+  hSurface = patch('vertices', baseSurface.vtcs, 'faces', baseSurface.tris,'FaceVertexCData', squeeze(img),'facecolor','interp','edgecolor','none','Parent',hAxis,'FaceAlpha',baseAlpha);
+  % keep the surface handle
+  mlrGuiSet(v,'displaySurface',hSurface);
+  % if the interrogator is on, then we need to reinitialize it
+  if ~verLessThan('matlab','8.4') && mrInterrogator('isactive',viewGet(v,'viewNum'))
+    mrInterrogator('init',viewGet(v,'viewNum'));
+  end
   % make sure x direction is normal to make right/right
   set(hAxis,'XDir','reverse');
   set(hAxis,'YDir','normal');
@@ -862,5 +871,4 @@ end
 
 if verbose>1,disppercent(inf);,end
 return;
-
 

mrLoadRet/GUI/mlrGuiSet.m
@@ -115,6 +115,19 @@ switch lower(field)
     set(handles.roiDisplayModePopup,'value',roiDisplayMode);
   end
   
+ case {'displaysurface'}
+  % mlrGuiSet(view,'displaySurface',h);
+  % where h is a handle to a surface. If h is empty, clears the field
+  % otherwise adds to the array of surfaces
+  if isempty(value)
+    handles.displaySurface = [];
+  else
+    if isfield(handles,'displaySurface')
+      handles.displaySurface(end+1) = value;
+    else
+      handles.displaySurface = value;
+    end
+  end
  case {'basetype'}
   % mlrGuiSet(view,'baseType',value);
   % value = 0 for regular or 1 for flat, 2 for surface

mrLoadRet/ROI/drawSurfaceROI.m
@@ -1,34 +1,54 @@
 % drawSurfaceROI.m
 %
 %        $Id$ 
-%      usage: drawSurfaceROI()
+%      usage: coords = drawSurfaceROI(v)
 %         by: Luke Robot Hospadaruk <hospada1@msu.edu>
 %       date: 11/28/09
 %    purpose: draw an ROI on 3D surface
 %
 function coords = drawSurfaceROI(view)
 
+coords = [];
 %use modified getpts function to grab the correct screen locations
 %TODO figure out how to draw stuff on the screen
 
-oneTimeWarning('drawROIOnSurface','(drawROI) Click on each boundary vertex of ROI. To finish making the ROI double click on a point that you want inside the ROI boundary',-1)
-
-hold(get(view.figure, 'CurrentAxes'), 'on');
-[xi, yi] = mlr_getpts_3d(view.figure);
-hold(get(view.figure, 'CurrentAxes'), 'off');
-
-disp('(drawSurfaceROI) Calculating key vertices');
-
-cmap = view.baseVolumes(view.curBase).coordMap;
+% get figure and make sure hold is on
+fig = viewGet(view,'figureNumber');
+hold(get(fig, 'CurrentAxes'), 'on');
 roiKeyVertices = [];
-%calculate vertex index of each clicked location
-for i=1:(numel(xi))
-	[x y s xBase yBase sBase xT yT sT vi] = getMouseCoords(view.viewNum, [xi(i) yi(i)]);
-	roiKeyVertices = [roiKeyVertices vi];
+% for surfaces in matlab past 8.4 select3d is busted, so need a new
+% method to get points which is implemented mlrGetptsSurface
+if ~verLessThan('matlab','8.4') && (viewGet(view,'baseType') == 2)
+  % tell the user what to do
+  oneTimeWarning('drawROIOnSurface','(drawROI) Click on each boundary vertex of ROI. To finish making the ROI control-click (click while holding down the control key) on a point that you want inside the ROI boundary',-1);
+  % get the display surface
+  handles = guidata(viewGet(view,'figureNumber'));
+  if isfield(handles,'displaySurface');
+    % and get user points that are clicked on
+    roiKeyVertices = mlrGetptsSurface(view,handles.displaySurface(1));
+  else
+    disp(sprintf('(drawSurfaceROI) Could not find surface'));
+    return
+  end
+else
+  % tell the user what to do
+  oneTimeWarning('drawROIOnSurface','(drawROI) Click on each boundary vertex of ROI. To finish making the ROI double click on a point that you want inside the ROI boundary',-1);
+  % old style (before mathworks broke select3d)
+  [xi, yi] = mlr_getpts_3d(view.figure);
+  % calculate key vertices from list of x, y points
+  disp('(drawSurfaceROI) Calculating key vertices');
+  for i=1:(numel(xi))
+    [x y s xBase yBase sBase xT yT sT vi] = getMouseCoords(view.viewNum, [xi(i) yi(i)]);
+    roiKeyVertices = [roiKeyVertices vi];
+  end
 end
+hold(get(fig, 'CurrentAxes'), 'off');
+
+% get coord map for base
+cmap = viewGet(view,'baseCoordMap');
 %make sure there are at least 3 perimiter and 1 interior vertices
 if(numel(roiKeyVertices) < 4)
-	mrWarnDlg('(drawSurfaceROI) Not enough vertices selected, you need at least 4');
+  mrWarnDlg('(drawSurfaceROI) Not enough vertices selected, you need at least 4');
   coords = [];
   return
 end
@@ -52,10 +72,31 @@ distances = sqrt(sum([...
 %bi-directional edges
 edgeDistances = sparse([edges(:,2);edges(:,3)], [edges(:,3);edges(:,2)], [distances;distances], size(cmap.coords, 2), size(cmap.coords, 2));
 
-
+% tell the user what is going on
 disp('(drawSurfaceROI) Searching for boundary vertices');
 
+% get shortest paths between roiKeyVertices
 [dist pred] = dijkstrap(edgeDistances, roiKeyVertices);
+
+% display vertices
+hold(get(fig, 'CurrentAxes'), 'on');
+% get the base surface
+baseSurface = viewGet(view,'baseSurface');
+% set up the list of vertices to draw - to draw back to the original
+drawVertices = [roiKeyVertices(1:end-1) roiKeyVertices(1)];
+for vertexIndex = 2:length(drawVertices)
+  % get the initial vertex to draw
+  thisVertex = drawVertices(vertexIndex);
+  % now until we find the start vertex in the predecessor list, keep drawing points
+  while(thisVertex ~= drawVertices(vertexIndex-1))
+    % plot it
+    plot3(baseSurface.vtcs(thisVertex,1),baseSurface.vtcs(thisVertex,2),baseSurface.vtcs(thisVertex,3),'r.');
+    % look up the next one in the predecessor list
+    thisVertex = pred(vertexIndex-1,thisVertex);
+  end
+end
+hold(get(fig, 'CurrentAxes'), 'off');
+drawnow
 pred(pred == -1) = 0;
 
 boundaryVertices = [];
@@ -460,13 +501,12 @@ hobj = get(get(view.figure, 'CurrentAxes'),'Children');
 % last. But if this is not always the case, then we may need
 % to do a little more work here to find the correct object
 [pos v vi] = select3d(hobj(end));
+if isempty(pos),return,end
 % convert the index to the coordinates
 
-if ~isempty(pos)
-	line_points =[line_points pos];
-	if(numel(line_points) > 3)
-		plot3(line_points(1,:), line_points(2,:), line_points(3,:),'Parent', get(view.figure, 'CurrentAxes'));
-	end
+line_points =[line_points pos];
+if(numel(line_points) > 3)
+  plot3(line_points(1,:), line_points(2,:), line_points(3,:),'Parent', get(view.figure, 'CurrentAxes'));
 end
 set([GETPTS_H1 GETPTS_H2], ...
 	'XData', x, ...

mrUtilities/ImageProcessing/select3d.m
@@ -61,6 +61,20 @@ viout = [];
 facevout = [];
 faceiout = [];
 
+if ~verLessThan('matlab','8.4')
+  userData = get(obj,'UserData');
+  % if data has been stored in the object (via our callback function surfClickLogger
+  % which is in refreshMLRDisplay) then return that, otherwise we will return empty
+  % since otherwise the code will crash below when it tries to grab undocumented
+  % view transforms from the figure 
+  if ~isempty(userData) && isfield(userData,'pos')
+    pout = userData.pos;
+    vout = userData.vertex;
+    viout = userData.vertexIndex;
+  end
+  return
+end
+
 % other variables
 ERRMSG = 'Input argument must be a valid graphics handle';
 isline = logical(0);

commit 7cf6f6a5aa639d3e24accc146cadacfdb41e9233

Changeset fixing the interrogator when ROIs are visible on a surface (which apparently has been broken since 375c2583, at least in matlab version > 8.4)
In order to fix it, all surface ROI handles are now saved in
view.baseVolumes.hROI (in refreshMLRDisplay). When mrInterrogaotr
initializes, the mrInterrogatorSurfaeCallback is associated not only
with the base surface but with all the ROI surfaces that are displayed,
so that it is called when the user clicks on any of these surfaces (the
bug was due to the fact that the base surface is hidden by the ROI
surfaces). Then in the callback, the current click coordinates are saved
in the base surface and can be retrieved later by mlrGetMouseCoords.

mrLoadRet/GUI/mlrGetMouseCoords.m
@@ -110,17 +110,23 @@ else
   if all(pointerLoc(1,:) >= 0)
     % then use select3d which is slooow, but accurate
     if isfield(MLR.interrogator{viewNum},'axesnum')
-      hobj = get(MLR.interrogator{viewNum}.axesnum,'Children');
+      if verLessThan('matlab','8.4')
+        hobj = get(MLR.interrogator{viewNum}.axesnum,'Children');
+        hobj = hobj(end);
       % make sure we are using the correct object (should be the 3D
       % brain). Use end here because with searchForVoxel we plot a
       % point on the image and the brain object always seems to be
       % last. But if this is not always the case, then we may need
       % to do a little more work here to find the correct object
-      [pos vertex vertexIndex] = select3d(hobj(end));
+      else %from  matlab version 8.5a, the handle to the base has been 
+        %stored in the view, so use that
+        hobj = viewGet(v,'baseHandle');
+      end
+      [pos vertex vertexIndex] = select3d(hobj);
       % convert the index to the coordinates
       if ~isempty(pos)
 	baseCoordMap = viewGet(v,'baseCoordMap');
-	%we'll take the coordinates of the middle of whatever range of cortical depth is currenlty selected
+	%we'll take the coordinates of the middle of whatever range of cortical depth is currently selected
 	corticalSlice = max(1,ceil(mean(viewGet(v,'corticalDepth'))*size(baseCoordMap.coords,5)));
 	pos = round(squeeze(baseCoordMap.coords(1,vertexIndex,1,:,corticalSlice)));
 	%pos = round(squeeze(baseCoordMap.coords(1,vertexIndex,1,:)));

mrLoadRet/GUI/mrInterrogator.m
@@ -359,28 +359,30 @@ end
 % section here, remove the endHandler section which unlinks this handler.
 if ~verLessThan('matlab','8.4') && (viewGet(v,'baseType') == 2)
   % get the handle for the patch
-  h = viewGet(v,'baseHandle');
-  if ~isempty(h)
-    % set the first in the list - note that there may be more if 
+  hBase = viewGet(v,'baseHandle');
+  hROI = viewGet(v,'surfaceROIHandle');
+  for h = [hBase hROI]
+    % set the first base in the list - note that there may be more if 
     % we are displaying multiple bases at once, but for now we ignore
     % all those "alt bases" and just respond to clicks on the main base
-    set(h,'ButtonDownFcn',@mrInterrogatorSurfaceCallback);
+    set(h,'ButtonDownFcn',{@mrInterrogatorSurfaceCallback,hBase});
     % set the viewNum in the handles
     userData = get(h,'UserData');
     userData.viewNum = viewNum;
     set(h,'UserData',userData);
-    v = viewSet(v,'baseHandle',h);
   end
-  % set the other callbacks
-  set(fignum,'WindowButtonMotionFcn',sprintf('mrInterrogator(''mouseMove'',%i)',viewNum));
-  set(fignum,'WindowButtonUpFcn',sprintf('mrInterrogator(''mouseUp'',%i)',viewNum));
+  if ~isempty(hBase)  
+    v = viewSet(v,'baseHandle',hBase);
+  end
+  if ~isempty(hROI)  
+    v = viewSet(v,'surfaceROIHandle',hROI);
+  end
 else
-  
-  % set the callbacks appropriately
-  set(fignum,'WindowButtonMotionFcn',sprintf('mrInterrogator(''mouseMove'',%i)',viewNum));
   set(fignum,'WindowButtonDownFcn',sprintf('mrInterrogator(''mouseDown'',%i)',viewNum));
-  set(fignum,'WindowButtonUpFcn',sprintf('mrInterrogator(''mouseUp'',%i)',viewNum));
 end
+% set the other callbacks
+set(fignum,'WindowButtonMotionFcn',sprintf('mrInterrogator(''mouseMove'',%i)',viewNum));
+set(fignum,'WindowButtonUpFcn',sprintf('mrInterrogator(''mouseUp'',%i)',viewNum));
 
 % set pointer to crosshairs
 MLR.interrogator{viewNum}.pointer = get(fignum,'pointer');
@@ -553,8 +555,9 @@ end
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 function mrInterrogatorSurfaceCallback(hObject,e,handles,varargin)
 
-% get viewNum
-userData = get(hObject,'UserData');
+% get viewNum from the surface base handle (even if called from another surface,
+% which might be a surface ROI.
+userData = get(handles,'UserData');
 % if no viewNum set then, something is wrong
 % since this should have been set when mrInterrogator was initialized
 % so giveup
@@ -568,13 +571,13 @@ click.viewNum = userData.viewNum;
 % to replace the broken select3d
 % get the point that the user clicked
 click.pos = e.IntersectionPoint;
-% compute distance to every vertex and pick the vertex that is closest to the intersectoin point
+% compute distance to every vertex and pick the vertex that is closest to the intersection point
 [minDist click.vertexIndex] = min(sum((e.Source.Vertices-repmat(click.pos',1,size(e.Source.Vertices,1))').^2,2));
 % now get the vertex position
 click.vertex = e.Source.Vertices(click.vertexIndex,:);
 
 % set the user data to include this latest click information
-set(hObject,'UserData',click);
+set(handles,'UserData',click);
 
 % call mouse down - note that the handler will get the click location
 % because we have stored in the userdata for the surface. This

mrLoadRet/GUI/refreshMLRDisplay.m
@@ -515,10 +515,6 @@ else
   hSurface = patch('vertices', baseSurface.vtcs, 'faces', baseSurface.tris,'FaceVertexCData', squeeze(img),'facecolor','interp','edgecolor','none','Parent',hAxis,'FaceAlpha',baseAlpha);
   % keep the surface handle
   v = viewSet(v,'baseHandle',hSurface,baseNum);
-  % if the interrogator is on, then we need to reinitialize it
-  if ~verLessThan('matlab','8.4') && mrInterrogator('isactive',viewGet(v,'viewNum'))
-    mrInterrogator('init',viewGet(v,'viewNum'));
-  end
   % make sure x direction is normal to make right/right
   set(hAxis,'XDir','reverse');
   set(hAxis,'YDir','normal');
@@ -545,7 +541,7 @@ if verbose>1,disppercent(inf);,end
 % Display ROIs
 nROIs = viewGet(v,'numberOfROIs');
 if nROIs
-  roi = displayROIs(v,hAxis,slice,sliceIndex,baseNum,base.coordsHomogeneous,base.dims,rotate,verbose);
+  [roi,v] = displayROIs(v,hAxis,slice,sliceIndex,baseNum,base.coordsHomogeneous,base.dims,rotate,verbose);
 else
   roi = [];
 end
@@ -554,6 +550,12 @@ end
 if ~baseType
   v = displaySurfaceOnVolume(v,hAxis,slice,sliceIndex,rotate,base.dims,slice,baseNum);
 end
+
+% if the interrogator is on, then we need to reinitialize it
+if baseType==2 && ~verLessThan('matlab','8.4') && mrInterrogator('isactive',viewGet(v,'viewNum'))
+  mrInterrogator('init',viewGet(v,'viewNum'));
+end
+
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % displayColorbar 
@@ -728,7 +730,7 @@ end
 %%%%%%%%%%%%%%%%%%%%%
 %    displayROIs    %
 %%%%%%%%%%%%%%%%%%%%%
-function roi = displayROIs(view,hAxis,sliceNum,sliceIndex,baseNum,baseCoordsHomogeneous,imageDims,rotate,verbose)
+function [roi,view] = displayROIs(view,hAxis,sliceNum,sliceIndex,baseNum,baseCoordsHomogeneous,imageDims,rotate,verbose)
 %
 % displayROIs: draws the ROIs in the current slice.
 %
@@ -783,7 +785,9 @@ if verbose>1,disppercent(-inf,'Drawing ROI');,end
 selectedROIColor = mrGetPref('selectedROIColor');
 if isempty(selectedROIColor),selectedROIColor = [1 1 1];end
 
+c=0;
 for r = order
+  c = c+1;
   if (r == selectedROI)
     % Selected ROI: set color
     if strcmp(selectedROIColor,'none')
@@ -869,7 +873,7 @@ for r = order
     roi{r}.vertices=y;
     roi{r}.overlayImage = roiColors;
     if ~isempty(fig) && ~isempty(hAxis)
-      patch('vertices', baseSurface.vtcs, 'faces', baseSurface.tris,'FaceVertexCData', roiColors,'facecolor','interp','edgecolor','none','FaceAlpha',0.4,'Parent',hAxis);
+      hSurface(c) = patch('vertices', baseSurface.vtcs, 'faces', baseSurface.tris,'FaceVertexCData', roiColors,'facecolor','interp','edgecolor','none','FaceAlpha',0.4,'Parent',hAxis);
     end
     continue
   end
@@ -972,7 +976,12 @@ for r = order
     roi{r}.lines.y = [];
   end
 end
-if baseType == 2,return,end
+if baseType == 2
+  if ~isempty(order) % store surface ROI handles for use in mrInterogator
+    view = viewSet(view,'surfaceROIHandle',hSurface,baseNum);
+  end
+  return
+end
 
 % label them, if labelROIs is set
 if labelROIs && ~isempty(hAxis)

mrLoadRet/View/viewGet.m
@@ -1481,6 +1481,17 @@ switch lower(param)
       % set to empty if handle is stale 
       if ~ishandle(val),val = [];end
     end
+  case {'surfaceroihandle'}
+    % basedata = viewGet(view,'baseHandle',[baseNum])
+    b = getBaseNum(view,varargin);
+    n = viewGet(view,'numberofbasevolumes');
+    if b & (b > 0) & (b <= n)
+      if isfield(view.baseVolumes(b),'hROI')
+        val = view.baseVolumes(b).hROI;
+      end
+      % set to empty if handle is stale 
+      if ~ishandle(val),val = [];end
+    end
   case {'basehdr'}
     % basedata = viewGet(view,'basehdr',[baseNum])
     b = getBaseNum(view,varargin);

mrLoadRet/View/viewSet.m
@@ -1146,7 +1146,15 @@ switch lower(param)
     if ~isempty(baseNum) & ~isempty(view.baseVolumes)
       view.baseVolumes(baseNum).h = h;
     end
-
+    
+case{'surfaceroihandle'}
+    % view = viewSet(view,'baseHandle',h,[baseNum]);
+    h = val;
+    baseNum = getBaseNum(view,varargin);
+    if ~isempty(baseNum) & ~isempty(view.baseVolumes)
+      view.baseVolumes(baseNum).hROI = h;
+    end
+   
  case{'basemultidisplay'}
     % view = viewSet(view,'baseMultiDisplay',multiDisplay,[baseNum]);
     baseNum = getBaseNum(view,varargin);