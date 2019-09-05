09b39d56c3ab73d49c8fd189c38410598a4ac715
Justin Gardner <jlg@stanford.edu>
2019-09-05 09:25:46 -0700

    Ability to find surface files, log transform of overlay values for better viewing of eccentricity, both srufaces display

--- mrUtilities/File/neuropythy/mlrImportNeuropythy.m
+++ mrUtilities/File/neuropythy/mlrImportNeuropythy.m
@@ -6,13 +6,16 @@
 %    purpose: Import anatomy predicted retinotopy from neuropythy into
 %             mlr rois and overlays. See: http://gru.stanford.edu/doku.php/mrtools/atlas
 %
-function retval = mlrImportNeuropythy(lh_retino,rh_retino)
+function retval = mlrImportNeuropythy(lh_retino,rh_retino,varargin)
 
 % check arguments
 if ~any(nargin == [2])
   help mlrImportNeuropythy
   return
 end
+
+% parse arguments
+getArgs(varargin,{'surfPath',[]});
 
 % check names
 [tf lh_retino] = checkNames(lh_retino);if ~tf,return,end
@@ -28,9 +31,60 @@ if ~checkExpectedFields(rh,{'varea','eccen','angle','sigma'}),return,end
 
 % make the overlays
 [lOverlays lOverlayNames] = makeOverlays(lh);
+[rOverlays rOverlayNames] = makeOverlays(rh);
 
-mrSurfViewer('jg_left_GM.off','','','','','',lOverlays,lOverlayNames);
+% get volume directory
+if isempty(surfPath)
+  titleStr = 'Choose left gray matter (outer) surface for this subject';
+  disp(sprintf('(mlrImportNeuropythy) %s',titleStr));
+  surfPath = mlrGetPathStrDialog(mrGetPref('volumeDirectory'),titleStr,'*.off');
+  if isempty(surfPath),return,end
+end
+
+% make sure this is a GM (not WM or Inf)
+surfFilename = getLastDir(surfPath);
+surfPath = fileparts(surfPath);
+surfFilename = replaceStr(surfFilename,'WM.','GM.');
+surfFilename = replaceStr(surfFilename,'Inf.','GM.');
+
+% make this into a left
+leftSurfFilename = replaceStr(surfFilename,'right','left');
+rightSurfFilename = replaceStr(surfFilename,'left','right');
+
+% bring up in viewer
+curpwd = pwd;
+try
+  cd(surfPath);
+  leftSurfaces = mrSurfViewer(leftSurfFilename,'','','','','',lOverlays,lOverlayNames);
+  if isempty(leftSurfaces),
+    disp(sprintf('(mlrImportNeuropythy) Left overlays have been rejected. Quitting'));
+    cd(curpwd);
+    return;
+  end
+  rightSurfaces = mrSurfViewer(rightSurfFilename,'','','','','',rOverlays,rOverlayNames);
+  if isempty(rightSurfaces),
+    disp(sprintf('(mlrImportNeuropythy) Right overlays have been rejected. Quitting'));
+    cd(curpwd);
+    return;
+  end
+catch
+  cd(curpwd);
+  keyboard
+end
+
+cd(curpwd);
+
 keyboard
+
+%%%%%%%%%%%%%%%%%%%%
+%    replaceStr    %
+%%%%%%%%%%%%%%%%%%%%
+function s = replaceStr(s,searchstr,replacestr)
+
+foundloc = strfind(s,searchstr);
+if ~isempty(foundloc)
+  s = sprintf('%s%s%s',s(1:(foundloc(1)-1)),replacestr,s((foundloc(1)+length(searchstr)):end));
+end
 
 %%%%%%%%%%%%%%%%%%%%%%
 %    makeOverlays    %
@@ -55,20 +109,20 @@ overlays{1}(visualAreaVertices,:) = areaColors(x.varea(visualAreaVertices),:);
 
 % make eccentricity overlay
 overlayNames{2} = 'eccentricity';
-overlays{2} = makeOverlay(x.eccen,visualAreaVertices,'parula');
+overlays{2} = makeOverlay(x.eccen,visualAreaVertices,'parula',true);
 
 % polar angle
 overlayNames{3} = 'polar angle';
-overlays{3} = makeOverlay(x.angle,visualAreaVertices,'hsv');
+overlays{3} = makeOverlay(x.angle,visualAreaVertices,'hsv',false);
 
 % sigma
 overlayNames{4} = 'sigma';
-overlays{4} = makeOverlay(x.sigma,visualAreaVertices,'parula');
+overlays{4} = makeOverlay(x.sigma,visualAreaVertices,'parula',true);
 
 %%%%%%%%%%%%%%%%%%%%%
 %    makeOverlay    %
 %%%%%%%%%%%%%%%%%%%%%
-function overlay = makeOverlay(data,visualAreaVertices,colorMapName)
+function overlay = makeOverlay(data,visualAreaVertices,colorMapName,logtransform)
 
 % get number of vertices
 nVertex = length(data);
@@ -84,7 +138,16 @@ valRange = maxVal - minVal;
 
 % set overlay
 overlay = nan(nVertex,3);
-overlay(visualAreaVertices,:) = colorMap(max(1,round(nColors*(data(visualAreaVertices)-minVal)/valRange)),:);
+% get values
+vals = (data(visualAreaVertices)-minVal)/valRange;
+% log transform if needed
+if logtransform
+  vals = log(vals+0.0001);
+  vals = (vals-min(vals))/(max(vals)-min(vals));
+end
+% and set into color range
+vals = max(1,round(vals*nColors));
+overlay(visualAreaVertices,:) = colorMap(vals,:);
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%
