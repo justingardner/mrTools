36186c1f7e2d8155e2673ea82b58a40fae698806
Julien Besle <julienbesle@gmail.com>
2020-08-03 19:40:05 +0300

    applyInverseBaseCoordMap.m: takes NaNs into account, which avoids holes in the resulting volume

--- mrLoadRet/Plugin/GLM_v2/applyInverseBaseCoordMap.m
+++ mrLoadRet/Plugin/GLM_v2/applyInverseBaseCoordMap.m
@@ -11,14 +11,17 @@
 function volumeData = applyInverseBaseCoordMap(flat2volumeMap,volumeDims,flatData)
 
 volumeData = zeros(volumeDims);
-datapoints=zeros(prod(volumeDims),1);
-hWaitBar = mrWaitBar(-inf,'(applyInverseBaseCoordMap) Converting from flat to volume');
+datapoints = zeros(volumeDims);
+hWaitBar = mrWaitBar(-inf,'(applyInverseBaseCoordMap) Converting from surface to volume');
 maxInstances = size(flat2volumeMap,2);
 for i=1:maxInstances
   mrWaitBar( i/maxInstances, hWaitBar);
   thisBaseCoordsMap = full(flat2volumeMap(:,i));
-  volumeData(logical(thisBaseCoordsMap)) = volumeData(logical(thisBaseCoordsMap)) + flatData(thisBaseCoordsMap(logical(thisBaseCoordsMap)));
-  datapoints = datapoints+logical(thisBaseCoordsMap);
+  newData = flatData(thisBaseCoordsMap(logical(thisBaseCoordsMap)));
+  indices = find(thisBaseCoordsMap);
+  notNaN = ~isnan(newData);
+  volumeData(indices(notNaN)) = volumeData(indices(notNaN)) + newData(notNaN);
+  datapoints(indices(notNaN)) = datapoints(indices(notNaN)) + 1;
 end
 datapoints = reshape(datapoints,volumeDims);
 volumeData = volumeData ./datapoints;
