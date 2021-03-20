d0fcdda8f31611324274809408343abe033ba35e
Julien Besle <julienbesle@gmail.com>
2021-03-20 12:58:13 +0200

    mlrExportROI.m: added option to export to space of any loaded base or scan

--- mrLoadRet/File/mlrExportROI.m
+++ mrLoadRet/File/mlrExportROI.m
@@ -1,12 +1,14 @@
 % mlrExportROI.m
 %
 %        $Id$ 
-%      usage: mlrExportROI(v,saveFilename,<'hdr',hdr>,<'exportToFreesurferLabel',true/false>)
+%      usage: mlrExportROI(v,saveFilename,<'baseNum',baseNum>,<'scanNum',scanNum>,<'groupNum',groupNum>,<'hdr',hdr>,<'exportToFreesurferLabel',true/false>)
 %         by: justin gardner
 %       date: 07/14/09
 %    purpose: Export ROI(s) to a nifti image or Freesurfer label file. Uses
-%             current roi(s) and current base in view to export. Pass in a nifti
-%             header as hdr argument if you want to use a different header
+%             current roi(s) and current base in view to export. To use a different
+%             base, specify 'baseNum'. To use a scan, specify 'scanNum' and 'groupNum'.
+%             Pass in a nifti header as hdr argument if you want to use a different header
+%             'baseNum', 'scanNum', 'groupNum' and 'hdr' are ignored if exportToFreesurferLabel is true
 %
 function mlrExportROI(v,saveFilename,varargin)
 
@@ -17,7 +19,7 @@ if nargin < 2
 end
 
 % optional arguments
-getArgs(varargin,{'hdr=[]','exportToFreesurferLabel=0'});
+getArgs(varargin,{'baseNum=[]','scanNum=[]','groupNum=[]','hdr=[]','exportToFreesurferLabel=0'});
 
 % get the roi we are being asked to export
 roiNum = viewGet(v,'currentroi');
@@ -34,20 +36,37 @@ if ~isequal(length(roiNum),length(saveFilename))
   return
 end
 
+if ~isempty(scanNum) && ~isempty(groupNum)
+  exportToScanSpace = true;
+  exportVolumeString = 'scan';
+  baseCoordMap = [];
+  baseType = 0;
+else
+  exportToScanSpace = false;
+  exportVolumeString = 'base';
+  if isempty(baseNum)
+    baseNum = viewGet(v,'curBase');
+  end
+  baseType = viewGet(v,'basetype',baseNum);
+  baseCoordMap = viewGet(v,'basecoordmap',baseNum);
+end
+
 % get the base nifti header
 passedInHeader = false;
 if ~isempty(hdr)
   passedInHeader = true;
 else
-  hdr = viewGet(v,'basehdr');
+  if exportToScanSpace
+    hdr = viewGet(v,'niftiHdr',scanNum,groupNum);
+  else
+    hdr = viewGet(v,'basehdr',baseNum);
+  end
   if isempty(hdr)
-    mrWarnDlg('(mlrExportROI) Could not get base anatomy header');
+    mrWarnDlg(sprintf('(mlrExportROI) Could not get %s header',exportVolumeString));
     return
   end
 end
 
-baseCoordMap = viewGet(v,'basecoordmap');
-baseType = viewGet(v,'basetype');
 if ~ismember(baseType,[2]) && exportToFreesurferLabel
   mrWarnDlg('(mlrExportROI) Load a surface in order to export to freesurfer label format');
   % for surfaces, the required list of surface vertices is in baseCoordMap
@@ -57,7 +76,7 @@ end
 if ~isempty(baseCoordMap) && (baseType==1 || exportToFreesurferLabel) %for flats, or when exporting to freesurfer label file, use basecoordmap 
   
   if baseType == 1 && ~exportToFreesurferLabel 
-      [~,baseCoords,baseCoordsHomogeneous] = getBaseSlice(v,1,3,viewGet(v,'baseRotate'),viewGet(v,'curBase'),baseType);
+      [~,baseCoords,baseCoordsHomogeneous] = getBaseSlice(v,1,3,viewGet(v,'baseRotate',baseNum),baseNum,baseType);
   else  
       baseCoords = permute(baseCoordMap.coords,[1 2 4 5 3]);
       baseCoordsHomogeneous = [permute(reshape(baseCoordMap.coords, ...
@@ -65,21 +84,21 @@ if ~isempty(baseCoordMap) && (baseType==1 || exportToFreesurferLabel) %for flats
         [3 1 4 2]); ones(1,size(baseCoordMap.coords,1)*size(baseCoordMap.coords,2),size(baseCoordMap.coords,5))];
   end
     
-  baseDims = size(baseCoords);
-  baseDims = baseDims ([1 2 4]);
+  volDims = size(baseCoords);
+  volDims = volDims ([1 2 4]);
   if baseType==1 && ~exportToFreesurferLabel
-    mrWarnDlg(sprintf('(mlrExportROI) Exporting ROI(s) to flat space (%d x %d x %d voxels). If you do not want this, load base volume or surface',baseDims(1),baseDims(2),baseDims(3)));
+    mrWarnDlg(sprintf('(mlrExportROI) Exporting ROI(s) to flat space (%d x %d x %d voxels). If you do not want this, load base volume or surface',volDims(1),volDims(2),volDims(3)));
   elseif exportToFreesurferLabel
     mrWarnDlg('(mlrExportROI) Vertex coordinates in label file might be incorrect.');
   end
   % make sure that baseCoords are rounded (they may not be if we are working with a baseCoordMap's flat map)
-  baseCoordsHomogeneous = reshape(baseCoordsHomogeneous,4,prod(baseDims));
+  baseCoordsHomogeneous = reshape(baseCoordsHomogeneous,4,prod(volDims));
   baseCoordsHomogeneous = round(baseCoordsHomogeneous);
   baseCoordsLinear = mrSub2ind(baseCoordMap.dims,baseCoordsHomogeneous(1,:),baseCoordsHomogeneous(2,:),baseCoordsHomogeneous(3,:));
 
   if baseType==1 && ~exportToFreesurferLabel
     % estimate voxel size (taken from getBaseOverlay, assuming mask is invariant to rotation, which it should be since it is a flat map)
-    oldBaseVoxelSize=viewGet(v,'basevoxelsize',viewGet(v,'curBase'));
+    oldBaseVoxelSize=viewGet(v,'basevoxelsize',baseNum);
     Xcoords0Mask = permute(baseCoords(:,:,1,:)==0,[1 2 4 3]);
     Xcoords0Mask = convn(Xcoords0Mask,ones(5,5,5),'same'); %expand the mask a bit to make sure we don't include any edge voxels
     XcoordsNaN = permute(baseCoords(:,:,1,:),[1 2 4 3]); 
@@ -98,24 +117,28 @@ if ~isempty(baseCoordMap) && (baseType==1 || exportToFreesurferLabel) %for flats
     end
   end
 else
-  baseDims = hdr.dim(2:4)';
+  volDims = hdr.dim(2:4)';
 end
 
 if ~passedInHeader && ~exportToFreesurferLabel
-  b = viewGet(v,'base');
+  b = viewGet(v,'base',baseNum);
 end
 
 for iRoi = 1:length(roiNum)
   roiName = viewGet(v,'roiName',roiNum(iRoi));
   % tell the user what is going on
-  fprintf('(mlrExportROI) Exporting ROI %s to %s with dimensions set to match base %s: [%i %i %i]\n',roiName, saveFilename{iRoi},viewGet(v,'baseName'),baseDims(1),baseDims(2),baseDims(3));
+  fprintf('(mlrExportROI) Exporting ROI %s to %s with dimensions set to match %s: [%i %i %i]\n',roiName, saveFilename{iRoi},exportVolumeString,volDims(1),volDims(2),volDims(3));
 
-  % get  roi coordinates in base coordinates
-  roiBaseCoords = getROICoordinates(v,roiNum(iRoi),0);
+  % get  roi coordinates in base/scan coordinates
+  if exportToScanSpace
+    roiBaseCoords = getROICoordinates(v,roiNum(iRoi),scanNum,groupNum);
+  else
+    roiBaseCoords = getROICoordinates(v,roiNum(iRoi),0,[],sprintf('baseNum=%d',baseNum));
+  end
 
   if ~exportToFreesurferLabel
     % create a data structure that has all 0's
-    d = zeros(baseDims);
+    d = zeros(volDims);
 
 
     % make sure we are inside the base dimensions
@@ -138,7 +161,7 @@ for iRoi = 1:length(roiNum)
 
   % check roiBaseCoords
   if isempty(roiBaseCoords) || ~nnz(roiBaseCoordsLinear)
-    mrWarnDlg(sprintf('(mlrExportROI) This ROI (%s) does not have any coordinates in the base',roiName));
+    mrWarnDlg(sprintf('(mlrExportROI) This ROI (%s) does not have any coordinates in the %s',roiName,exportVolumeString));
     
   else
     
@@ -177,9 +200,6 @@ for iRoi = 1:length(roiNum)
       d(roiBaseCoordsLinear) = 1;
 
       % if the orientation has been changed in loadAnat, undo that here.
-      if ~isempty(b.originalOrient)
-      end
-      % now save the nifti file
       if ~passedInHeader && ~isempty(b.originalOrient)
         % convert into mlrImage
         [d, h] = mlrImageLoad(d,hdr);
