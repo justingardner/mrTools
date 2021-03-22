75534bf8989bbc46d963449750d518787117dd05
Julien Besle <julienbesle@gmail.com>
2021-03-22 10:26:52 +0200

    applyInverseBaseCoordMap.m: can now be applied to 4D data

--- mrLoadRet/Plugin/GLM_v2/applyInverseBaseCoordMap.m
+++ mrLoadRet/Plugin/GLM_v2/applyInverseBaseCoordMap.m
@@ -12,30 +12,38 @@ function volumeData = applyInverseBaseCoordMap(surf2volumeMap,volumeDims,surfDat
 
 hWaitBar = mrWaitBar(-inf,'(applyInverseBaseCoordMap) Converting from surface to volume');
 
-volumeData = zeros(volumeDims);
-datapoints = zeros(volumeDims);
+nVolumes = size(surfData,4);
+volumeData = zeros([volumeDims nVolumes]);
 
-% first find the longest non-zero row of the sparse flat2volumeMap matrix,
-% as it is often much longer than the other rows and so time can be saved by treating it differently
-longestRow = find(surf2volumeMap(:,end));
-for iRow = longestRow % in case there are several such rows (unlikely)
-  volumeData(iRow) = volumeData(iRow) + sum(surfData(surf2volumeMap(iRow,:)));
-  datapoints(iRow) = size(surf2volumeMap,2);
-end
-surf2volumeMap(longestRow,:) = 0;
-surf2volumeMap(:,sum(surf2volumeMap>0)==0) = [];
+for iVolume = 1:nVolumes
+  thisSurfData = surfData(:,:,:,iVolume);
+  thisVolumeData = zeros(volumeDims);
+  datapoints = zeros(volumeDims);
+  thisSurf2volMap = surf2volumeMap;
 
-% now do the rest colum by column
-maxInstances = size(surf2volumeMap,2);
-for i=1:maxInstances
-  mrWaitBar( i/maxInstances, hWaitBar);
-  thisBaseCoordsMap = surf2volumeMap(:,i);
-  newData = surfData(thisBaseCoordsMap(logical(thisBaseCoordsMap)));
-  indices = find(thisBaseCoordsMap);
-  notNaN = ~isnan(newData);
-  volumeData(indices(notNaN)) = volumeData(indices(notNaN)) + newData(notNaN);
-  datapoints(indices(notNaN)) = datapoints(indices(notNaN)) + 1;
+  % first find the longest non-zero row of the sparse flat2volumeMap matrix,
+  % as it is often much longer than the other rows and so time can be saved by treating it differently
+  longestRow = find(thisSurf2volMap(:,end));
+  for iRow = longestRow % in case there are several such rows (unlikely)
+    thisVolumeData(iRow) = thisVolumeData(iRow) + sum(thisSurfData(thisSurf2volMap(iRow,:)));
+    datapoints(iRow) = size(thisSurf2volMap,2);
+  end
+  thisSurf2volMap(longestRow,:) = 0;
+  thisSurf2volMap(:,sum(thisSurf2volMap>0)==0) = [];
+
+  % now do the rest colum by column
+  maxInstances = size(thisSurf2volMap,2);
+  for i=1:maxInstances
+    mrWaitBar( ((iVolume-1)*maxInstances+i) / (maxInstances*nVolumes), hWaitBar);
+    thisBaseCoordsMap = thisSurf2volMap(:,i);
+    newData = thisSurfData(thisBaseCoordsMap(logical(thisBaseCoordsMap)));
+    indices = find(thisBaseCoordsMap);
+    notNaN = ~isnan(newData);
+    thisVolumeData(indices(notNaN)) = thisVolumeData(indices(notNaN)) + newData(notNaN);
+    datapoints(indices(notNaN)) = datapoints(indices(notNaN)) + 1;
+  end
+  datapoints = reshape(datapoints,volumeDims);
+  volumeData(:,:,:,iVolume) = thisVolumeData ./datapoints;
 end
-datapoints = reshape(datapoints,volumeDims);
-volumeData = volumeData ./datapoints;
+
 mrCloseDlg(hWaitBar);
