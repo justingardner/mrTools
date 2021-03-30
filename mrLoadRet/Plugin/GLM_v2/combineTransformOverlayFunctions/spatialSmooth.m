0d6bf226f525c87814c5c4aacf06d76ad631b954
Julien Besle <julienbesle@gmail.com>
2021-03-30 12:16:54 +0300

    spatialSmooth.m: handle cases where all kernel FWHM values are 0

--- mrLoadRet/Plugin/GLM_v2/combineTransformOverlayFunctions/spatialSmooth.m
+++ mrLoadRet/Plugin/GLM_v2/combineTransformOverlayFunctions/spatialSmooth.m
@@ -31,7 +31,9 @@ end
 kernelDims = 2*w+1;
 kernelCenter = ceil(kernelDims/2);
 [X,Y,Z] = meshgrid(1:kernelDims(1),1:kernelDims(2),1:kernelDims(3));
-if sigma_d(2) == 0 && sigma_d(3) == 0
+if all(sigma_d == 0)
+  kernel = 1; % no smoothing
+elseif sigma_d(2) == 0 && sigma_d(3) == 0
   kernel = exp(-((X-kernelCenter(1)).^2/(2*sigma_d(1)^2))); % 1D Gaussian function along X
 elseif sigma_d(1) == 0 && sigma_d(3) == 0
   kernel = exp(-((Y-kernelCenter(2)).^2/(2*sigma_d(2)^2))); % 1D Gaussian function along Y
