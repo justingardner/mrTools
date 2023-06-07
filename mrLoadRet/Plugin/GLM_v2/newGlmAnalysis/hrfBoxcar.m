justingardner/mrTools

commit 466be7e0eaf4fc6f79ad452d6bcf9e9ec73a46fe

glmAnalysis_v2: added estimation sampling period parameter to hrfDeconvolution.m., which required modifying other hrf functions (by adding an empty argument).
This change is required for cases where the acquisition supersampling is
less than the design supersampling (in which case deconvolution is not
very useful). This was needed to compare the estimation efficiency of different
sequences, some of which require design supersampling.

This change has not been tested in mrLoadRet

mrLoadRet/Plugin/GLM_v2/newGlmAnalysis/hrfBoxcar.m
@@ -6,7 +6,7 @@
 %       date: 13/04/2010
 %    purpose: returns a canonical hrf modeled as a boxcar function
 %
-function [params,hrf] = hrfBoxcar(params, sampleDuration, notUsed, defaultParams)
+function [params,hrf] = hrfBoxcar(params, sampleDuration, ~, defaultParams, ~)
 
 if ~any(nargin == [1 2 3 4])% 5])
   help hrfBoxcar

mrLoadRet/Plugin/GLM_v2/newGlmAnalysis/hrfCustom.m
@@ -8,7 +8,7 @@
 %             checks that times correspond to actual TR and acquistion time (sampleDuration and sampleDelay)
 %             otherwise, assumes that HRF sample times correspond to those TR and acquisition times
 %
-function [params,hrf] = hrfCustom(params, sampleDuration,sampleDelay, defaultParams)
+function [params,hrf] = hrfCustom(params, sampleDuration, sampleDelay, defaultParams, ~)
 
 if ~any(nargin == [1 2 3 4])% 5])
   help hrfCustom

mrLoadRet/Plugin/GLM_v2/newGlmAnalysis/hrfFslFlobs.m
@@ -6,7 +6,7 @@
 %       date: 14/06/07, 09/02/2010
 %    purpose: reads a basis set from a flobs file
 %
-function [params, hrf] = hrfFslFlobs(params, sampleDuration, sampleDelay, defaultParams)
+function [params, hrf] = hrfFslFlobs(params, sampleDuration, sampleDelay, defaultParams, ~)
 
 if ~any(nargin == [1 2 3 4])% 5])
   help hrfDoubleGamma

mrLoadRet/Plugin/GLM_v2/interrogatorFunctions/roisConfidenceInterval.m
@@ -184,7 +184,7 @@ for scanNum = analysisParams.scanNum
       fTests = analysisParams.testParams.fTests;
       contrasts = analysisParams.testParams.contrasts;
       %-------------- get the HRF model --------------
-      [analysisParams.hrfParams,d.hrf] = feval(analysisParams.hrfModel, analysisParams.hrfParams, d.tr/d.designSupersampling,0,1);
+      [analysisParams.hrfParams,d.hrf] = feval(analysisParams.hrfModel, analysisParams.hrfParams, d.tr/d.designSupersampling,d.tr/d.estimationSupersampling,1);
       nEstimates = size(d.hrf,2);
       
    case 'Deconvolution'

mrLoadRet/Plugin/GLM_v2/newGlmAnalysis/getGlmEVParamsGUI.m
@@ -360,7 +360,7 @@ for iScan = params.scanNum
   
   d = loadScan(thisView, iScan, viewGet(thisView,'groupNum',params.groupName), 0);
   d = getStimvol(d,params.scanParams{iScan});
-  [params.hrfParams,d.hrf] = feval(params.hrfModel, params.hrfParams, d.tr/d.designSupersampling,params.scanParams{iScan}.acquisitionDelay,1);
+  [params.hrfParams,d.hrf] = feval(params.hrfModel, params.hrfParams, d.tr/d.designSupersampling,params.scanParams{iScan}.acquisitionDelay,1,d.tr/d.estimationSupersampling);
   d = eventRelatedPreProcess(d,params.scanParams{iScan}.preprocess);
   d = makeDesignMatrix(d,params,1,iScan);
   if  strcmp(params.EVnames{end},'Not used')

mrLoadRet/Plugin/GLM_v2/newGlmAnalysis/getGlmTestParamsGUI.m
@@ -49,9 +49,9 @@ while keepAsking
   %here we assume that all scans in this group have the same sampling parameters 
   %(which are needed to determine the number of components in the deconvolution case) 
   framePeriod = viewGet(thisView,'framePeriod',params.scanNum(1),viewGet(thisView,'groupNum',params.groupName));
-  [hrfParams,hrf] = feval(params.hrfModel, params.hrfParams,framePeriod/params.scanParams{params.scanNum(1)}.estimationSupersampling,[],1);
+  [~,hrf] = feval(params.hrfModel, params.hrfParams,framePeriod/params.scanParams{params.scanNum(1)}.designSupersampling,[],1,framePeriod/params.scanParams{params.scanNum(1)}.estimationSupersampling);
   nComponents = size(hrf,2);
-  if fieldIsNotDefined(params, 'componentsToTest') || ~isequal(nComponents,length(params.componentsToTest));
+  if fieldIsNotDefined(params, 'componentsToTest') || ~isequal(nComponents,length(params.componentsToTest))
     params.componentsToTest = ones(1,nComponents);
   end
   if fieldIsNotDefined(params, 'componentsCombination')

mrLoadRet/Plugin/GLM_v2/newGlmAnalysis/glmAnalysis.m
@@ -334,7 +334,7 @@ for iScan = params.scanNum
       end
 
       %create model HRF
-      [params.hrfParams,d.hrf] = feval(params.hrfModel, params.hrfParams, d.tr/d.designSupersampling,scanParams{iScan}.acquisitionDelay,1);
+      [params.hrfParams,d.hrf] = feval(params.hrfModel, params.hrfParams, d.tr/d.designSupersampling,scanParams{iScan}.acquisitionDelay,1,d.tr/d.estimationSupersampling);
 
       d.volumes = 1:d.dim(4);
       %make a copy of d

mrLoadRet/Plugin/GLM_v2/newGlmAnalysis/hrfDeconvolution.m
@@ -6,9 +6,9 @@
 %       date: 13/04/2010
 %    purpose: returns a deconvolution matrix given the design sampling period and the estimation sampling period
 %
-function [params,hrf] = hrfDeconvolution(params, sampleDuration, notUsed, defaultParams)
+function [params,hrf] = hrfDeconvolution(params, designSamplingPeriod, ~, defaultParams, estimationSamplingPeriod)
 
-if ~any(nargin == [1 2 3 4])% 5])
+if ~any(nargin == [1 2 3 4 5])
   help hrfDeconvolution
   return
 end
@@ -16,6 +16,7 @@ end
 %estimationSampling = varargin{1};
 
 if ieNotDefined('defaultParams'),defaultParams = 0;end
+if ieNotDefined('estimationSamplingPeriod'),estimationSamplingPeriod = designSamplingPeriod;end
 
 if ieNotDefined('params')
   params = struct;
@@ -42,4 +43,8 @@ if nargout==1
    return
 end
 
-hrf = eye(round(params.hdrlenS/sampleDuration));
+hrf = eye(round(params.hdrlenS/designSamplingPeriod));
+
+% if the estimation sampling period is a multiple of the design sampling period
+downsamplingFactor = round(estimationSamplingPeriod/designSamplingPeriod); % downsampling factor from design to estimation sampling rates
+hrf = hrf(:,1:downsamplingFactor:end);                                     % (although this is rounded, we generally assume that the estimation sampling period is a multiple of the design sampling period)

commit b03f186b94c944f79e788c9cb1780643f18a9344

hrfBocar.m, hrfCustom.m,, hrfFslFlobs.m: fixed bugs introduced in 466be7e0

mrLoadRet/Plugin/GLM_v2/newGlmAnalysis/hrfBoxcar.m
@@ -8,7 +8,7 @@
 %
 function [params,hrf] = hrfBoxcar(params, sampleDuration, ~, defaultParams, ~)
 
-if ~any(nargin == [1 2 3 4])% 5])
+if ~any(nargin == [1 2 3 4 5])
   help hrfBoxcar
   return
 end

mrLoadRet/Plugin/GLM_v2/newGlmAnalysis/hrfCustom.m
@@ -10,7 +10,7 @@
 %
 function [params,hrf] = hrfCustom(params, sampleDuration, sampleDelay, defaultParams, ~)
 
-if ~any(nargin == [1 2 3 4])% 5])
+if ~any(nargin == [1 2 3 4 5])
   help hrfCustom
   return
 end

mrLoadRet/Plugin/GLM_v2/newGlmAnalysis/hrfFslFlobs.m
@@ -8,7 +8,7 @@
 %
 function [params, hrf] = hrfFslFlobs(params, sampleDuration, sampleDelay, defaultParams, ~)
 
-if ~any(nargin == [1 2 3 4])% 5])
+if ~any(nargin == [1 2 3 4 5])
   help hrfDoubleGamma
   return
 end