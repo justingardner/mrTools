dad9147d306c3eaafdaaad7f58b9776813b3388f
Julien Besle <julienbesle@gmail.com>
2024-12-11 14:32:25 +0000

    mrSub2ind.m: fixed for Matlab2024 and later, in which sub2ind does not accept NaNs anymore

--- mrUtilities/MatlabUtilities/mrSub2ind.m
+++ mrUtilities/MatlabUtilities/mrSub2ind.m
@@ -4,8 +4,7 @@
 %      usage: mrSub2ind(dims,x,y,z)
 %         by: justin gardner
 %       date: 10/15/07
-%    purpose: sub2ind that doesn't choke on coordinates
-%             outside of dims (just inserts nans for those)
+%    purpose: sub2ind that doesn't choke on coordinates outside of dims.
 %
 function linear = mrSub2ind(dims,x,y,z)
 
@@ -18,20 +17,35 @@ end
 if isempty(x),linear = [];return;end
 
 if nargin == 4
-  badCoords = find((x < 1) | (x > dims(1)) | ...
+  if verLessThan('matlab','24.1')
+    badCoords = find((x < 1) | (x > dims(1)) | ...
 		   (y < 1) | (y > dims(2)) | ...
 		   (z < 1) | (z > dims(3)));
-  x(badCoords) = nan;
-  y(badCoords) = nan;
-  z(badCoords) = nan;
-
-  linear = sub2ind(dims,x,y,z);
+    x(badCoords) = nan;
+    y(badCoords) = nan;
+    z(badCoords) = nan;
+    
+    linear = sub2ind(dims,x,y,z);
+  else   % As of Matlab 2024, nan coordinates in sub2ind give an error, these coordinates need to be removed completely
+    badCoords = (x < 1) | (x > dims(1)) | isnan(x) | ...
+		   (y < 1) | (y > dims(2)) | isnan(y) | ...
+		   (z < 1) | (z > dims(3)) | isnan(z);
+    linear = nan(size(x));
+    linear(~badCoords) = sub2ind(dims,x(~badCoords),y(~badCoords),z(~badCoords));
+  end
 else
-  badCoords = find((x < 1) | (x > dims(1)) | ...
-		   (y < 1) | (y > dims(2)));
-  x(badCoords) = nan;
-  y(badCoords) = nan;
-
-  linear = sub2ind(dims,x,y);
+  if verLessThan('matlab','24.1')
+    badCoords = find((x < 1) | (x > dims(1)) | ...
+		     (y < 1) | (y > dims(2)));
+    x(badCoords) = nan;
+    y(badCoords) = nan;
+    
+    linear = sub2ind(dims,x,y);
+  else   % As of Matlab 2024, nan coordinates in sub2ind give an error, these coordinates need to be removed completely
+    badCoords = (x < 1) | (x > dims(1)) | isnan(x) | ...
+		   (y < 1) | (y > dims(2)) | isnan(y);
+    linear = nan(size(x));
+    linear(~badCoords) = sub2ind(dims,x(~badCoords),y(~badCoords));
+  end
 end
 
